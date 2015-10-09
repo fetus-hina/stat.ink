e6e9dee830e14150c5a8748f4464de76c36ffdb0
AIZAWA, Hina <hina@bouhime.com>
2015-10-09 20:17:02 +0900

    oops

--- migrations/m151009_100616_kill_ratio.php
+++ migrations/m151009_100616_kill_ratio.php
@@ -10,8 +10,7 @@ class m151009_100616_kill_ratio extends Migration
             'CASE',
                 'WHEN {{battle}}.[[kill]] = 0 AND {{battle}}.[[death]] = 0 THEN 1.00',
                 'WHEN {{battle}}.[[kill]] > 0 AND {{battle}}.[[death]] = 0 THEN 99.99',
-                'WHEN {{battle}}.[[kill]] > 0 AND {{battle}}.[[death]] > 0 THEN ({{battle}}.[[kill]]::float / {{battle}}.[[death]]::float)::numeric(4,2)',
-                'ELSE NULL',
+                'ELSE ({{battle}}.[[kill]]::float / {{battle}}.[[death]]::float)::numeric(4,2)',
             'END'
         ]);
         $select = "SELECT {{battle}}.[[id]], {$selectRatio} AS [[kill_ratio]] FROM {{battle}} WHERE ({{battle}}.[[kill]] IS NOT NULL AND {{battle}}.[[death]] IS NOT NULL)";
