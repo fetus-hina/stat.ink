8df949d2544db90d71bbd9e0c819270a0aeeb030
AIZAWA, Hina <hina@bouhime.com>
2015-10-29 18:04:20 +0900

    ノヴァブラスターネオとH3リールガンDを追加

---
+++ migrations/m151029_085422_weapon.php
@@ -0,0 +1,46 @@
+<?php
+use yii\db\Migration;
+use app\models\Special;
+use app\models\Subweapon;
+use app\models\WeaponType;
+use app\models\DeathReasonType;
+
+class m151029_085422_weapon extends Migration
+{
+    public function safeUp()
+    {
+        $this->insert('weapon', [
+            'type_id' => WeaponType::findOne(['key' => 'shooter'])->id,
+            'key' => 'nova_neo',
+            'name' => 'Luna Blaster Neo',
+            'subweapon_id' => Subweapon::findOne(['key' => 'splashbomb'])->id,
+            'special_id' => Special::findOne(['key' => 'bombrush'])->id,
+        ]);
+
+        $this->insert('weapon', [
+            'type_id' => WeaponType::findOne(['key' => 'shooter'])->id,
+            'key' => 'h3reelgun_d',
+            'name' => 'H-3 Nozzlenose D',
+            'subweapon_id' => Subweapon::findOne(['key' => 'pointsensor'])->id,
+            'special_id' => Special::findOne(['key' => 'supershot'])->id,
+        ]);
+
+        $this->insert('death_reason', [
+            'type_id' => DeathReasonType::findOne(['key' => 'main'])->id,
+            'key' => 'nova_neo',
+            'name' => 'Luna Blaster Neo',
+        ]);
+
+        $this->insert('death_reason', [
+            'type_id' => DeathReasonType::findOne(['key' => 'main'])->id,
+            'key' => 'h3reelgun_d',
+            'name' => 'H-3 Nozzlenose D',
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->delete('death_reason', ['key' => ['nova_neo', 'h3reelgun_d']]);
+        $this->delete('weapon', ['key' => ['nova_neo', 'h3reelgun_d']]);
+    }
+}
