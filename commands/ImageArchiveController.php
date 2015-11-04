797a5c206b789bde9c0c4e8d6ec6dfe70f9501e1
AIZAWA, Hina <hina@bouhime.com>
2015-11-04 19:44:55 +0900

    画像アーカイブ処理変更 step2

--- commands/ImageArchiveController.php
+++ commands/ImageArchiveController.php
@@ -17,6 +17,7 @@ use app\components\helpers\Resource;
 class ImageArchiveController extends Controller
 {
     const MAX_OPTIMIZE_PROC_COUNT = 3;
+    const ARCHIVE_SPLIT_AIM = 524288000; // 500 MiB
 
     public function init()
     {
@@ -24,13 +25,17 @@ class ImageArchiveController extends Controller
         return parent::init();
     }
 
+    // optimize {{{
     public function actionOptimize()
     {
         $inDir  = Yii::getAlias('@archive/queue');
         $outDir = Yii::getAlias('@archive/interim');
 
         if (!file_exists($inDir)) {
-            $this->stderr("Optimize queue directory not found. It will be created on post a battle.\n", Console::FG_RED);
+            $this->stderr(
+                "Optimize queue directory not found. It will be created on post a battle.\n",
+                Console::FG_RED
+            );
             return 1;
         }
 
@@ -204,6 +209,219 @@ class ImageArchiveController extends Controller
         }
         return 'running';
     }
+    // }}}
+
+    // archive {{{
+    public function actionArchive($date = null)
+    {
+        if ($date === null) {
+            $tmp = time() + 9 * 3600;
+            $tmp = gmmktime(0, 0, 0, gmdate('n', $tmp), gmdate('j', $tmp) - 1, gmdate('Y', $tmp));
+            $date = gmdate('Ymd', $tmp);
+        }
+
+        if (!preg_match('/^\d{8}$/', $date)) {
+            $this->stderr("Invalid date given. YYYYMMDD required.\n", Console::FG_RED);
+            return 1;
+        }
+
+        $inDirs = [
+            'judge'  => Yii::getAlias('@archive/interim') . '/' . $date . '-judge',
+            'result' => Yii::getAlias('@archive/interim') . '/' . $date . '-result',
+        ];
+        $outDir = Yii::getAlias('@archive/archive');
+
+        $inDirExists = false;
+        foreach ($inDirs as $tmp) {
+            if (!file_exists($tmp)) {
+                $this->stderr("Input directory not found. {$tmp}\n", Console::FG_RED);
+            } else {
+                $inDirExists = true;
+            }
+        }
+        if (!$inDirExists) {
+            return 1;
+        }
+
+        if (!$lock = $this->getLock(__METHOD__)) {
+            $this->stderr("Another process is running.\n");
+            return 0;
+        }
+
+        $plans = $this->createArchivePlan($inDirs, $outDir, $date);
+        if (empty($plans)) {
+            $this->stderr("[archive] No plans created.\n");
+            return 0;
+        }
+
+        $this->stdout(sprintf("[archive] %d plans created.\n", count($plans)), Console::FG_GREEN);
+
+        $error = false;
+        foreach ($plans as $plan) {
+            if (!$this->executeArchivePlan($plan)) {
+                $error = true;
+            }
+        }
+
+        if (!$error) {
+            foreach ($inDirs as $dir) {
+                $this->rmdirRecursive($dir);
+            }
+        }
+
+        return $error ? 1 : 0;
+    }
+
+    private function createArchivePlan(array $dirs, $outDir, $date)
+    {
+        $this->stdout("[archive] Creating plan...\n");
+        // $fileLists = [
+        //     'result' => [
+        //         (object)[
+        //             'path' => '/path/to/file.png',
+        //             'size' => 42,
+        //         ],
+        //         ...
+        //     ],
+        //     'judge' => [ ... ],
+        // ]
+        $fileLists = array_map(
+            function ($dir) {
+                return $this->getFileInfoListForArchivePlan($dir);
+            },
+            $dirs
+        );
+
+        $ret = [];
+
+        // 走査開始番号（一番若いバトルID）
+        $startId = min(
+            array_map(
+                function (array $files) {
+                    return count($files) ? $files[0]->battle : PHP_INT_MAX;
+                },
+                $fileLists
+            )
+        );
+        $part = 1;
+        while ($startId < PHP_INT_MAX) {
+            // 規定容量程度になるように分割するときの最終バトルID
+            $endId = min(
+                array_map(
+                    function (array $files) use ($startId) {
+                        $files = array_filter(
+                            $files,
+                            function ($file) use ($startId) {
+                                return $file->battle >= $startId;
+                            }
+                        );
+                        $total = 0;
+                        foreach ($files as $file) {
+                            $total += $file->size;
+                            if ($total >= self::ARCHIVE_SPLIT_AIM) {
+                                return $file->battle;
+                            }
+                        }
+                        return PHP_INT_MAX - 1; // 後で + 1 するので INT を超えないように
+                    },
+                    $fileLists
+                )
+            );
+
+            $this->stdout(sprintf("    Archive #%d : battle %d - %d\n", $part, $startId, $endId));
+            foreach ($dirs as $key => $dir) {
+                $files = array_filter(
+                    $fileLists[$key],
+                    function ($file) use ($startId, $endId) {
+                        return $file->battle >= $startId && $file->battle <= $endId;
+                    }
+                );
+                if (!empty($files)) {
+                    $ret[] = (object)[
+                        'dst'    => sprintf('%s/%s-%02d-%s.tar', $outDir, $date, $part, $key),
+                        'srcDir' => $dir,
+                        'srcFiles' => array_values(
+                            array_map(
+                                function ($info) {
+                                    return basename($info->path);
+                                },
+                                $files
+                            )
+                        ),
+                    ];
+                }
+            }
+
+            ++$part;
+            $startId = $endId + 1;
+        }
+        return $ret;
+    }
+
+    private function getFileInfoListForArchivePlan($dir)
+    {
+        $ret = [];
+        foreach (new DirectoryIterator($dir) as $entry) {
+            if ($entry->isFile() && preg_match('/^(\d+)-\w+\.png$/', $entry->getBasename(), $match)) {
+                $ret[] = (object)[
+                    'battle' => (int)$match[1],
+                    'path' => $entry->getPathname(),
+                    'size' => $entry->getSize(),
+                ];
+            }
+        }
+        usort(
+            $ret,
+            function ($a, $b) {
+                return strnatcmp($a->path, $b->path);
+            }
+        );
+        return $ret;
+    }
+
+    private function executeArchivePlan(\stdClass $plan)
+    {
+        $this->stdout("[archive] Creating archive " . basename($plan->dst) . " ... ", Console::FG_YELLOW);
+
+        if (!file_exists(dirname($plan->dst))) {
+            mkdir(dirname($plan->dst), 0755, true);
+        }
+        if (file_exists($plan->dst)) {
+            $this->stdout("ALREADY EXISTS\n", Console::FG_YELLOW);
+            return true;
+        }
+
+        // tar に喰わせるためのファイルリストを作成
+        $tmpFile = new Resource(
+            tempnam(Yii::getAlias('@archive'), 'tmp-'),
+            'unlink'
+        );
+        file_put_contents($tmpFile->get(), implode("\n", $plan->srcFiles) . "\n");
+
+        // tar の実行
+        $chdir = new Resource(
+            getcwd(),
+            function ($dir) {
+                chdir($dir);
+            }
+        );
+        chdir($plan->srcDir);
+        $cmdline = sprintf(
+            '/usr/bin/env %s -c -T %s -f %s',
+            escapeshellarg('tar'),
+            escapeshellarg($tmpFile->get()),
+            escapeshellarg($plan->dst)
+        );
+        $lines = $status = null;
+        exec($cmdline, $lines, $status);
+        if ($status === 0) {
+            $this->stdout("SUCCESS\n", Console::FG_GREEN);
+            return true;
+        }
+        $this->stderr("FAILED with code={$status}\n", Console::FG_RED);
+        return false;
+    }
+    // }}}
 
     protected function getLock($ident)
     {
@@ -225,5 +443,33 @@ class ImageArchiveController extends Controller
                 fclose($fh);
             }
         );
+    }
+
+    protected function rmdirRecursive($dir)
+    {
+        $files = [];
+        $subs = [];
+        foreach (new DirectoryIterator($dir) as $entry) {
+            if ($entry->isDot()) {
+                continue;
+            }
+            if ($entry->isDir()) {
+                $subs[] = $dir . '/' . $entry->getBasename();
+            } else {
+                $files[] = $dir . '/' . $entry->getBasename();
+            }
+        }
+
+        foreach ($files as $path) {
+            $this->stdout("delete file: " . $path . "\n");
+            unlink($path);
+        }
+
+        foreach ($subs as $path) {
+            $this->rmdirRecursive($path);
+        }
+
+        $this->stdout("delete directory: " . $dir . "\n");
+        rmdir($dir);
     }
 }
