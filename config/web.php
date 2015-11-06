901015ccb4238aa625d31074369e6c4648d7cd54
AIZAWA, Hina <hina@bouhime.com>
2015-11-06 23:06:38 +0900

    bump version

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 $params = require(__DIR__ . '/params.php');
 $config = [
     'name' => 'stat.ink',
-    'version' => '1.2.2',
+    'version' => '1.2.3',
     'id' => 'statink',
     'language' => 'ja-JP',
     'timeZone' => 'Asia/Tokyo',
