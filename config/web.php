5f59b4e4b9541d41704e1703776c54bda76a592b
AIZAWA, Hina <hina@bouhime.com>
2015-12-01 19:17:28 +0900

    v1.7.0-dev

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 $params = require(__DIR__ . '/params.php');
 $config = [
     'name' => 'stat.ink',
-    'version' => '1.6.2',
+    'version' => '1.7.0-dev',
     'id' => 'statink',
     'language' => 'ja-JP',
     'timeZone' => 'Asia/Tokyo',
