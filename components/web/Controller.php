f426f03a65cec8b14ed66aa0c90a7cc1c66eb7d1
AIZAWA, Hina <hina@bouhime.com>
2016-01-07 23:26:08 +0900

    初期表示言語の自動設定（テキトー）

--- components/web/Controller.php
+++ components/web/Controller.php
@@ -9,6 +9,7 @@ namespace app\components\web;
 
 use Yii;
 use yii\web\Controller as Base;
+use yii\web\Cookie;
 use app\models\Language;
 use app\models\Timezone;
 
@@ -23,14 +24,83 @@ class Controller extends Base
 
     private function setLanguage()
     {
-        $request = Yii::$app->request;
-        $cookie = $request->cookies->get('language');
-        if ($cookie) {
-            $lang = Language::findOne(['lang' => $cookie->value]);
-            if ($lang) {
-                Yii::$app->language = $lang->lang;
-                return;
+        $lang = (function () {
+            if ($val = $this->detectLanguageByParam()) {
+                return $val;
             }
+            if ($val = $this->detectLanguageByCookie()) {
+                return $val;
+            }
+            if ($val = $this->detectLanguageByHeader()) {
+                return $val;
+            }
+            return null;
+        })();
+        if ($lang) {
+            Yii::$app->language = $lang;
+        }
+    }
+
+    private function detectLanguageByParam()
+    {
+        $param = Yii::$app->request->get('_lang_');
+        if (!is_scalar($param)) {
+            return false;
+        }
+        if (!$lang = Language::findOne(['lang' => (string)$param])) {
+            return false;
+        }
+        if ($this->detectLanguageByCookie() === false) {
+            Yii::$app->response->cookies->add(
+                new Cookie([
+                    'name' => 'language',
+                    'value' => $lang->lang,
+                    'expire' => time() + 86400 * 366,
+                ])
+            );
+        }
+        return $lang->lang;
+    }
+
+    private function detectLanguageByCookie()
+    {
+        $cookie = Yii::$app->request->cookies->get('language');
+        if (!$cookie) {
+            return false;
+        }
+        if (!$lang = Language::findOne(['lang' => $cookie->value])) {
+            return false;
+        }
+        return $lang->lang;
+    }
+
+    private function detectLanguageByHeader()
+    {
+        $request = Yii::$app->request;
+        $userAgent   = trim($request->userAgent);
+        $acceptLangs = $request->acceptableLanguages;
+        if (empty($acceptLangs) ||
+                $userAgent == '' ||
+                stripos($userAgent, 'bot') !== false ||
+                stripos($userAgent, 'spider') !== false ||
+                stripos($userAgent, 'google') !== false ||
+                stripos($userAgent, 'http://') !== false ||
+                stripos($userAgent, 'https://') !== false
+        ) {
+            return false;
+        }
+
+        //FIXME
+        $firstLang = strtolower(substr(array_shift($acceptLangs), 0, 2));
+        switch ($firstLang) {
+            case 'ja':
+                return 'ja-JP';
+
+            case 'en':
+                return 'en-US';
+
+            default:
+                return false;
         }
     }
 
