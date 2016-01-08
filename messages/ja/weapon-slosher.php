f8002e964fdad7951c65658dbcac8c8556129668
AIZAWA, Hina <hina@bouhime.com>
2016-01-08 18:05:31 +0900

    スクリュースロッシャーネオを追加

--- messages/ja/weapon-slosher.php
+++ messages/ja/weapon-slosher.php
@@ -3,6 +3,7 @@ return [
     'Slosher' => 'バケットスロッシャー',
     'Slosher Deco' => 'バケットスロッシャーデコ',
     'Sloshing Machine' => 'スクリュースロッシャー',
+    'Sloshing Machine Neo' => 'スクリュースロッシャーネオ',
     'Tri-Slosher' => 'ヒッセン',
     'Tri-Slosher Nouveau' => 'ヒッセン・ヒュー',
 ];
---
+++ migrations/m160108_090118_weapon.php
@@ -0,0 +1,35 @@
+<?php
+use yii\db\Migration;
+use app\models\Special;
+use app\models\Subweapon;
+use app\models\WeaponType;
+use app\models\DeathReasonType;
+
+class m160108_090118_weapon extends Migration
+{
+    public function safeUp()
+    {
+        $this->insert('weapon', [
+            'type_id' => WeaponType::findOne(['key' => 'slosher'])->id,
+            'key' => 'screwslosher_neo',
+            'name' => 'Sloshing Machine Neo',
+            'subweapon_id' => Subweapon::findOne(['key' => 'pointsensor'])->id,
+            'special_id' => Special::findOne(['key' => 'supershot'])->id,
+        ]);
+
+        $this->insert('death_reason', [
+            'type_id' => DeathReasonType::findOne(['key' => 'main'])->id,
+            'key' => 'screwslosher_neo',
+            'name' => 'Sloshing Machine Neo',
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $keys = [
+            'screwslosher_neo',
+        ];
+        $this->delete('death_reason', ['key' => $keys]);
+        $this->delete('weapon', ['key' => $keys]);
+    }
+}
