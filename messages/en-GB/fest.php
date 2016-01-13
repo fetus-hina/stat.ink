42322957a908443f79f186a9bd1cd940f3b6e083
AIZAWA, Hina <hina@bouhime.com>
2016-01-14 03:46:16 +0900

    update en-GB translation

--- messages/en-GB/fest.php
+++ messages/en-GB/fest.php
@@ -1,18 +1,3 @@
 <?php
 return [
-    'Champion' => 'Charismatic',
-    'Defender' => 'Super',
-    'Fanboy/Fangirl' => 'Normal',
-    'Fiend' => 'Full-on',
-    'King/Queen' => 'Eternal',
-    '{0} Champion' => 'Charismatic {0} Boy',
-    '{0} Defender' => 'Super {0} Boy',
-    '{0} Fanboy' => 'Normal {0} Boy',
-    '{0} Fiend' => 'Full-on {0} Boy',
-    '{0} King' => 'Eternal {0} Boy',
-    '{1} Champion' => 'Charismatic {1} Girl',
-    '{1} Defender' => 'Super {1} Girl',
-    '{1} Fangirl' => 'Normal {1} Girl',
-    '{1} Fiend' => 'Full-on {1} Girl',
-    '{1} Queen' => 'Eternal {1} Girl',
 ];
