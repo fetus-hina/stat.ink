6bc08137bd7478f516f19ca6e43403171e0568bb
AIZAWA Hina <hina@bouhime.com>
2016-02-20 12:15:02 +0900

    JP-Splatfest #11
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160220_030838_jp11th_splatfest.php
@@ -0,0 +1,60 @@
+<?php
+use yii\db\Migration;
+use app\models\Region;
+use app\models\Splatfest;
+use app\models\SplatfestTeam;
+
+class m160220_030838_jp11th_splatfest extends Migration
+{
+    public function safeUp()
+    {
+        $model = new Splatfest();
+        $model->attributes = [
+            'region_id' => Region::findOne(['key' => 'jp'])->id,
+            'name'      => 'ポケットモンスター 赤vs緑',
+            'start_at'  => '2016-02-20 06:00:00+09',
+            'end_at'    => '2016-02-21 06:00:00+09',
+            'order'     => 11,
+        ];
+        if (!$model->save()) {
+            return false;
+        }
+
+        $team = new SplatfestTeam();
+        $team->attributes = [
+            'fest_id' => $model->id,
+            'team_id' => 1,
+            'name'    => 'ポケットモンスター赤',
+            'color_hue' => 348,
+        ];
+        if (!$team->save()) {
+            return false;
+        }
+
+        $team = new SplatfestTeam();
+        $team->attributes = [
+            'fest_id' => $model->id,
+            'team_id' => 2,
+            'name'    => 'ポケットモンスター緑',
+            'color_hue' => 156,
+        ];
+        if (!$team->save()) {
+            return false;
+        }
+    }
+
+    public function safeDown()
+    {
+        $fest = Splatfest::findOne([
+            'region_id' => Region::findOne(['key' => 'jp'])->id,
+            'order' => 11,
+        ]);
+        if (!$fest) {
+            return false;
+        }
+        SplatfestTeam::deleteAll(['fest_id' => $fest->id]);
+        if (!$fest->delete()) {
+            return false;
+        }
+    }
+}
