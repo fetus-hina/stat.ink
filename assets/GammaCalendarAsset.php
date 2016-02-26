55d3eea56692cb8deb60a62161e9d54b18b4055e
AIZAWA Hina <hina@bouhime.com>
2016-02-26 20:37:22 +0900

    refs #75, GitHubのContributes風のものを実現するためのライブラリ
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ assets/GammaCalendarAsset.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (C) 2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class GammaCalendarAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/gammacalendar/dist';
+    public $js = [
+        'gammacalendar-v0.0.1.min.js',
+    ];
+    public $css = [
+        'gammacalendar-v0.0.1.min.css',
+    ];
+}
