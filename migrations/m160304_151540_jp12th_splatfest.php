4377ee395b6992d7ad2170e08e92e16e83930435
AIZAWA Hina <hina@bouhime.com>
2016-03-05 00:24:22 +0900

    日本フェス12回分の準備（色は適当に愛vsお金のデータ）
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160304_151540_jp12th_splatfest.php
@@ -0,0 +1,46 @@
+<?php
+use yii\db\Migration;
+use app\models\Region;
+use app\models\Splatfest;
+
+class m160304_151540_jp12th_splatfest extends Migration
+{
+    public function safeUp()
+    {
+        $this->insert('splatfest', [
+            'region_id' => Region::findOne(['key' => 'jp'])->id,
+            'name'      => 'ガンガンいこうぜ vs いのちだいじに',
+            'start_at'  => '2016-03-12 09:00:00+09',
+            'end_at'    => '2016-03-13 09:00:00+09',
+            'order'     => 12,
+        ]);
+        $festId = Splatfest::find()
+            ->joinWith(['region'])
+            ->andWhere([
+                '{{region}}.[[key]]' => 'jp',
+                '{{splatfest}}.[[order]]' => 12,
+            ])
+            ->one()->id;
+
+        $this->batchInsert('splatfest_team', ['fest_id', 'team_id', 'name', 'color_hue'], [
+            [ $festId, 1, 'ガンガンいこうぜ', 332],
+            [ $festId, 2, 'いのちだいじに', 34],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $fest = Splatfest::find()
+            ->joinWith(['region'])
+            ->andWhere([
+                '{{region}}.[[key]]' => 'jp',
+                '{{splatfest}}.[[order]]' => 12,
+            ])
+            ->one();
+        if (!$fest) {
+            return false;
+        }
+        $this->delete('splatfest_team', ['fest_id' => $fest->id]);
+        $this->delete('splatfest', ['fest_id' => $fest->id]);
+    }
+}
