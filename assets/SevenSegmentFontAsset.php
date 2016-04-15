ff99c49a08085608dd2532b92b811128a1bfb6a4
AIZAWA Hina <hina@bouhime.com>
2016-04-15 18:28:41 +0900

    コミット漏れ #125
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ assets/SevenSegmentFontAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class SevenSegmentFontAsset extends AssetBundle
+{
+    public $sourcePath = '@app/resources/.compiled/dseg';
+    public $css = [
+        'dseg14.css',
+    ];
+}
