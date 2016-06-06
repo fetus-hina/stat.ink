dcb0e81a894652bd68a7e7108ddfc1375b42fc5c
AIZAWA Hina <hina@bouhime.com>
2016-06-06 23:29:08 +0900

    IkaLogの最小対応バージョンを定義
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160606_142124_ikalog_requirements.php
@@ -0,0 +1,22 @@
+<?php
+use yii\db\Migration;
+
+class m160606_142124_ikalog_requirements extends Migration
+{
+    public function up()
+    {
+        $this->createTable('ikalog_requirement', [
+            'id'            => $this->primaryKey(),
+            'from'          => 'TIMESTAMP(0) WITH TIME ZONE NOT NULL',
+            'version_date'  => 'VARCHAR(64) NOT NULL',
+        ]);
+        $this->batchInsert('ikalog_requirement', [ 'from', 'version_date' ], [
+            [ '2016-06-08 10:00:00+09:00', '2016-06-08_00' ],
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('ikalog_requirement');
+    }
+}
