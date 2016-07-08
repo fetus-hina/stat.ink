0b06ce7a2b931a9edb5e707063d0ebf7e6f85299
AIZAWA Hina <hina@bouhime.com>
2016-07-08 16:54:57 +0900

    :bug: Fix support Splatoon 2.10.0
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160708_074713_fix_version_2_10.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use yii\db\Migration;
+use app\models\SplatoonVersion;
+
+class m160708_074713_fix_version_2_10 extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'splatoon_version',
+            ['released_at' => '2016-07-08T14:00:00+09:00'],
+            ['tag' => '2.10.0']
+        );
+        $this->update(
+            'battle',
+            ['version_id' => SplatoonVersion::findOne(['tag' => '2.10.0'])->id],
+            ['>=', 'end_at', '2016-07-08T14:00:00+09:00']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'splatoon_version',
+            ['released_at' => '2016-07-06T14:00:00+09:00'],
+            ['tag' => '2.10.0']
+        );
+    }
+}
