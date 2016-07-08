3eac58a527d54357656ec0ca6a9ddc73af339e7b
AIZAWA Hina <hina@bouhime.com>
2016-07-08 14:08:00 +0900

    :up: Support Splatoon v2.10.0
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160708_050431_version_2_10.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+use yii\db\Migration;
+
+class m160708_050431_version_2_10 extends Migration
+{
+    public function safeUp()
+    {
+        $this->batchInsert('splatoon_version', [ 'tag', 'name', 'released_at' ], [
+            [ '2.10.0', '2.10.0', '2016-07-06T14:00:00+09:00' ],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->delete('splatoon_version', ['tag' => '2.10.0']);
+    }
+}
