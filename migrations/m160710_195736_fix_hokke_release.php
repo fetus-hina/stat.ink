54805061a2f06d462568ee9bc68c1ccbb8187eac
AIZAWA Hina <hina@bouhime.com>
2016-07-11 05:03:31 +0900

    :bug: ホッケふ頭のリリース日を修正
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160710_195736_fix_hokke_release.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use yii\db\Migration;
+
+class m160710_195736_fix_hokke_release extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'map',
+            ['release_at' => '2015-06-02 11:00:00+09'],
+            ['key' => 'hokke']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'map',
+            ['release_at' => '2015-05-28 00:00:00+09'],
+            ['key' => 'hokke']
+        );
+    }
+}
