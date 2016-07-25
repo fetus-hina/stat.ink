2f38f75374381b957b0a5a7b71649f407680d7bc
AIZAWA Hina <hina@bouhime.com>
2016-07-25 16:00:52 +0900

    :+1: Add UTF-16LE
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160725_063530_utf16le.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\models\Charset;
+use app\models\Language;
+use yii\db\Migration;
+
+class m160725_063530_utf16le extends Migration
+{
+    public function safeUp()
+    {
+        $this->insert('charset', [
+            'name'          => 'UTF-16LE',
+            'php_name'      => 'UTF-16LE',
+            'substitute'    => ord('?'),
+        ]);
+        $id = Charset::findOne(['php_name' => 'UTF-16LE'])->id;
+        $this->batchInsert('language_charset', [ 'language_id', 'charset_id', 'is_win_acp' ], [
+            [ Language::findOne(['lang' => 'en-US'])->id, $id, false ],
+            [ Language::findOne(['lang' => 'en-GB'])->id, $id, false ],
+            [ Language::findOne(['lang' => 'es-ES'])->id, $id, false ],
+            [ Language::findOne(['lang' => 'es-MX'])->id, $id, false ],
+            [ Language::findOne(['lang' => 'ja-JP'])->id, $id, false ],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $id = Charset::findOne(['php_name' => 'UTF-16LE'])->id;
+        $this->delete('language_charset', ['charset_id' => $id]);
+        $this->delete('charset', ['id' => $id]);
+    }
+}
