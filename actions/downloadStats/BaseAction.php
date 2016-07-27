eb77c28c2e663b5ea1198ee9ef13d62e863ab279
AIZAWA Hina <hina@bouhime.com>
2016-07-27 19:44:40 +0900

    :recycle: ダウンロードアクションの共通になりそうな部分を切り離し
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ actions/downloadStats/BaseAction.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @copyright Copyright (C) 2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+namespace app\actions\downloadStats;
+
+use Yii;
+use app\models\Charset;
+use app\models\Language;
+use app\models\Map;
+use app\models\Rule;
+use app\models\Weapon;
+use yii\base\DynamicModel;
+use yii\web\BadRequestHttpException;
+use yii\web\ViewAction;
+
+abstract class BaseAction extends ViewAction
+{
+    public $config;
+
+    public function init()
+    {
+        parent::init();
+        $req = Yii::$app->request;
+        $config = DynamicModel::validateData(
+            [
+                'lang'      => $req->get('lang'),
+                'charset'   => $req->get('charset'),
+                'bom'       => $req->get('bom'),
+                'tsv'       => $req->get('tsv'),
+            ],
+            [
+                [['lang', 'charset'], 'string'],
+                [['bom', 'tsv'], 'boolean'],
+                [['lang'], 'exist', 'skipOnError' => true,
+                    'targetClass' => Language::class,
+                    'targetAttribute' => 'lang'],
+                [['charset'], 'exist', 'skipOnError' => true,
+                    'targetClass' => Charset::class,
+                    'targetAttribute' => ['charset' => 'php_name']],
+            ]
+        );
+        if ($config->hasErrors()) {
+            throw new BadRequestHttpException('Bad parameters');
+        }
+        $this->config = $config;
+    }
+
+    public function getWeapons()
+    {
+        $ret = [];
+        foreach (Weapon::find()->all() as $a) {
+            $ret[$a->key] = Yii::t('app-weapon', $a->name);
+        }
+        return $ret;
+    }
+
+    public function getRules()
+    {
+        $ret = [];
+        foreach (Rule::find()->all() as $a) {
+            $ret[$a->key] = Yii::t('app-rule', $a->name);
+        }
+        return $ret;
+    }
+
+    public function getMaps()
+    {
+        $ret = [];
+        foreach (Map::find()->all() as $a) {
+            $ret[$a->key] = Yii::t('app-map', $a->name);
+        }
+        return $ret;
+    }
+}
--- actions/downloadStats/WeaponRuleMapAction.php
+++ actions/downloadStats/WeaponRuleMapAction.php
@@ -10,47 +10,11 @@ namespace app\actions\downloadStats;
 use Yii;
 use app\components\web\CsvResponseFormatter;
 use app\models\Charset;
-use app\models\Language;
-use app\models\Map;
-use app\models\Rule;
 use app\models\StatWeaponKDWinRate;
-use app\models\Weapon;
-use yii\base\DynamicModel;
 use yii\db\Query;
-use yii\web\BadRequestHttpException;
-use yii\web\ViewAction;
 
-class WeaponRuleMapAction extends ViewAction
+class WeaponRuleMapAction extends BaseAction
 {
-    public $config;
-
-    public function init()
-    {
-        $req = Yii::$app->request;
-        $config = DynamicModel::validateData(
-            [
-                'lang'      => $req->get('lang'),
-                'charset'   => $req->get('charset'),
-                'bom'       => $req->get('bom'),
-                'tsv'       => $req->get('tsv'),
-            ],
-            [
-                [['lang', 'charset'], 'string'],
-                [['bom', 'tsv'], 'boolean'],
-                [['lang'], 'exist', 'skipOnError' => true,
-                    'targetClass' => Language::class,
-                    'targetAttribute' => 'lang'],
-                [['charset'], 'exist', 'skipOnError' => true,
-                    'targetClass' => Charset::class,
-                    'targetAttribute' => ['charset' => 'php_name']],
-            ]
-        );
-        if ($config->hasErrors()) {
-            throw new BadRequestHttpException('Bad parameters');
-        }
-        $this->config = $config;
-    }
-
     public function run()
     {
         if ($this->config->lang) {
@@ -139,32 +103,5 @@ class WeaponRuleMapAction extends ViewAction
                 ];
             }
         })($query->createCommand()->queryAll());
-    }
-
-    public function getWeapons()
-    {
-        $ret = [];
-        foreach (Weapon::find()->all() as $a) {
-            $ret[$a->key] = Yii::t('app-weapon', $a->name);
-        }
-        return $ret;
-    }
-
-    public function getRules()
-    {
-        $ret = [];
-        foreach (Rule::find()->all() as $a) {
-            $ret[$a->key] = Yii::t('app-rule', $a->name);
-        }
-        return $ret;
-    }
-
-    public function getMaps()
-    {
-        $ret = [];
-        foreach (Map::find()->all() as $a) {
-            $ret[$a->key] = Yii::t('app-map', $a->name);
-        }
-        return $ret;
     }
 }
