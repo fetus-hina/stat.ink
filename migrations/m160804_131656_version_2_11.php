b9d5804d76dd2004eb738558c5f189b3305ffcda
AIZAWA Hina <hina@bouhime.com>
2016-08-04 22:22:39 +0900

    :sparkles: Splatoon v2.11.0 対応
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160804_131656_version_2_11.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\models\SplatoonVersion;
+use yii\db\Migration;
+
+class m160804_131656_version_2_11 extends Migration
+{
+    public function safeUp()
+    {
+        $this->batchInsert('splatoon_version', [ 'tag', 'name', 'released_at' ], [
+            [ '2.11.0', '2.11.0', '2016-08-03T10:00:00+09:00' ],
+        ]);
+        $this->update(
+            'battle',
+            ['version_id' => SplatoonVersion::findOne(['tag' => '2.11.0'])->id],
+            ['>=', 'end_at', '2016-08-03T10:00:00+09:00']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'battle',
+            ['version_id' => SplatoonVersion::findOne(['tag' => '2.10.0'])->id],
+            ['version_id' => SplatoonVersion::findOne(['tag' => '2.11.0'])->id]
+        );
+        $this->delete('splatoon_version', ['tag' => '2.11.0']);
+    }
+}
