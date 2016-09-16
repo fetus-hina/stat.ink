04c5092df524bd0e792bd12bb32f764ee97f3846
AIZAWA Hina <hina@bouhime.com>
2016-09-16 19:42:38 +0900

    :+1: Splatoon version 2.12
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m160916_103807_version_2_12.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2016 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\models\SplatoonVersion;
+use yii\db\Migration;
+
+class m160916_103807_version_2_12 extends Migration
+{
+    public function safeUp()
+    {
+        $this->batchInsert('splatoon_version', [ 'tag', 'name', 'released_at' ], [
+            [ '2.12.0', '2.12.0', '2016-09-14T10:00:00+09:00' ],
+        ]);
+        $this->update(
+            'battle',
+            ['version_id' => SplatoonVersion::findOne(['tag' => '2.12.0'])->id],
+            ['>=', 'end_at', '2016-09-14T10:00:00+09:00']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'battle',
+            ['version_id' => SplatoonVersion::findOne(['tag' => '2.11.0'])->id],
+            ['version_id' => SplatoonVersion::findOne(['tag' => '2.12.0'])->id]
+        );
+        $this->delete('splatoon_version', ['tag' => '2.12.0']);
+    }
+}
