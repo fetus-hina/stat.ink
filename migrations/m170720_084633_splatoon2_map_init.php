cceb3d290dc525526940865f16b272d37899b50e
AIZAWA Hina <hina@bouhime.com>
2017-07-20 17:54:22 +0900

    ステージリリース日
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m170720_084633_splatoon2_map_init.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m170720_084633_splatoon2_map_init extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'map2',
+            ['release_at' => '2017-07-15 17:00:00+09'],
+            ['key' => ['combu', 'gangaze', 'ama', 'tachiuo']]
+        );
+        $this->update(
+            'map2',
+            ['release_at' => '2017-07-21 00:00:00+09'],
+            ['key' => ['chozame', 'hokke']]
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'map2',
+            ['release_at' => null],
+            ['key' => [
+                'combu', 'gangaze', 'ama', 'tachiuo', 'chozame', 'hokke',
+            ]]
+        );
+    }
+}
