7dbd81df1b95e8dbbe4c4ec85b143d1a7f67dfab
AIZAWA Hina <hina@bouhime.com>
2017-07-27 13:32:55 +0000

    Apply fixes from StyleCI

--- views/show-v2/_battle_details.php
+++ views/show-v2/_battle_details.php
@@ -21,17 +21,17 @@ $battle->his_team_count = 100;
     'class' => ['table', 'table-striped'],
   ],
   'template' => function ($attribute, $index, $widget) {
-    // {{{
+      // {{{
     if ($attribute['value'] === null) {
-      return;
+        return;
     }
-    $captionOptions = Html::renderTagAttributes(
+      $captionOptions = Html::renderTagAttributes(
       ArrayHelper::getValue($attribute, 'captionOptions', [])
     );
-    $contentOptions = Html::renderTagAttributes(
+      $contentOptions = Html::renderTagAttributes(
       ArrayHelper::getValue($attribute, 'contentOptions', [])
     );
-    return strtr(
+      return strtr(
       '<tr><th{captionOptions}>{label}</th><td{contentOptions}>{value}</td></tr>',
       [
         '{label}' => $attribute['label'],
@@ -46,10 +46,10 @@ $battle->his_team_count = 100;
     [
       'label' => Yii::t('app', 'Mode'),
       'value' => function ($model) {
-        if ($text = $model->getPrettyMode()) {
-          return $text;
-        }
-        return sprintf(
+          if ($text = $model->getPrettyMode()) {
+              return $text;
+          }
+          return sprintf(
           '%s %s',
           Yii::t('app', '(incomplete)'),
           implode(' / ', [
@@ -63,15 +63,15 @@ $battle->his_team_count = 100;
     [
       'attribute' => 'map_id', // {{{
       'value' => function ($model) {
-        return Yii::t('app-map2', $model->map->name ?? '?');
+          return Yii::t('app-map2', $model->map->name ?? '?');
       },
       // }}}
     ],
     [
       'attribute' => 'weapon_id', // {{{
       'value' => function ($model) {
-        $weapon = $model->weapon;
-        return sprintf(
+          $weapon = $model->weapon;
+          return sprintf(
           '%s (%s / %s)',
           Yii::t('app-weapon2', $weapon->name ?? '?'),
           Yii::t('app-subweapon2', $weapon->subweapon->name ?? '?'),
@@ -83,10 +83,10 @@ $battle->his_team_count = 100;
     [
       'attribute' => 'rank_id', // {{{
       'value' => function ($model) : ?string {
-        if ($model->rank_id === null && $model->rank_after_id === null) {
-          return null;
-        }
-        return sprintf(
+          if ($model->rank_id === null && $model->rank_after_id === null) {
+              return null;
+          }
+          return sprintf(
           '%s → %s',
           Yii::t('app-rank2', $model->rank->name ?? '?'),
           Yii::t('app-rank2', $model->rankAfter->name ?? '?')
@@ -97,12 +97,12 @@ $battle->his_team_count = 100;
     [
       'attribute' => 'level', // {{{
       'value' => function ($model) : ?string {
-        $model->level = 10;
-        $model->level_after = 42;
-        if ($model->level === null && $model->level_after === null) {
-          return null;
-        }
-        return sprintf(
+          $model->level = 10;
+          $model->level_after = 42;
+          if ($model->level === null && $model->level_after === null) {
+              return null;
+          }
+          return sprintf(
           '%s → %s',
           $model->level ?? '?',
           $model->level_after ?? '?'
@@ -116,24 +116,24 @@ $battle->his_team_count = 100;
       'label' => Yii::t('app', 'Result'), // {{{
       'format' => 'raw',
       'value' => function ($model) : ?string {
-        $parts = [];
-        if ($model->isGachi && $model->is_knockout !== null) {
-          if ($model->is_knockout) {
-            $parts[] = Html::tag(
+          $parts = [];
+          if ($model->isGachi && $model->is_knockout !== null) {
+              if ($model->is_knockout) {
+                  $parts[] = Html::tag(
               'span',
               Html::encode(Yii::t('app', 'Knockout')),
               ['class' => 'label label-info']
             );
-          } else {
-            $parts[] = Html::tag(
+              } else {
+                  $parts[] = Html::tag(
               'span',
               Html::encode(Yii::t('app', 'Time was up')),
               ['class' => 'label-label-warning']
             );
+              }
           }
-        }
-        if ($model->is_win !== null) {
-          $parts[] = ($model->is_win)
+          if ($model->is_win !== null) {
+              $parts[] = ($model->is_win)
             ? Html::tag(
               'span',
               Html::encode(Yii::t('app', 'Won')),
@@ -144,10 +144,10 @@ $battle->his_team_count = 100;
               Html::encode(Yii::t('app', 'Lost')),
               ['class' => 'label label-success']
             );
-        } else {
-          $parts[] = Html::encode('?');
-        }
-        return implode(' ', $parts);
+          } else {
+              $parts[] = Html::encode('?');
+          }
+          return implode(' ', $parts);
       },
       // }}}
     ],
@@ -155,17 +155,17 @@ $battle->his_team_count = 100;
       'label' => Yii::t('app', 'Team Inked'), // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if ($model->my_team_percent !== null && $model->his_team_percent !== null) {
-          $myPct = (float)$model->my_team_percent;
-          $hisPct = (float)$model->his_team_percent;
-          $myDrawPct = round($myPct * 100 / ($myPct + $hisPct) * 100) / 100;
-          $myPoint = null;
-          $hisPoint = null;
-          if ($model->my_team_point !== null && $model->his_team_point !== null) {
-            $myPoint = Yii::$app->formatter->asInteger($model->my_team_point) . 'P';
-            $hisPoint = Yii::$app->formatter->asInteger($model->his_team_point) . 'P';
-          }
-          return Html::tag(
+          if ($model->my_team_percent !== null && $model->his_team_percent !== null) {
+              $myPct = (float)$model->my_team_percent;
+              $hisPct = (float)$model->his_team_percent;
+              $myDrawPct = round($myPct * 100 / ($myPct + $hisPct) * 100) / 100;
+              $myPoint = null;
+              $hisPoint = null;
+              if ($model->my_team_point !== null && $model->his_team_point !== null) {
+                  $myPoint = Yii::$app->formatter->asInteger($model->my_team_point) . 'P';
+                  $hisPoint = Yii::$app->formatter->asInteger($model->his_team_point) . 'P';
+              }
+              return Html::tag(
             'div',
             implode('', [
               Html::tag(
@@ -189,13 +189,13 @@ $battle->his_team_count = 100;
             ]),
             ['class' => 'progress', 'style' => 'width:100%;max-width:400px']
           );
-        }
+          }
 
-        if ($model->my_team_point !== null && $model->his_team_point !== null) {
-          $myPoint = (int)$model->my_team_point;
-          $hisPoint = (int)$model->his_team_point;
-          $myDrawPct = round($myPoint * 100 / ($myPoint + $hisPoint) * 100) / 100;
-          return Html::tag(
+          if ($model->my_team_point !== null && $model->his_team_point !== null) {
+              $myPoint = (int)$model->my_team_point;
+              $hisPoint = (int)$model->his_team_point;
+              $myDrawPct = round($myPoint * 100 / ($myPoint + $hisPoint) * 100) / 100;
+              return Html::tag(
             'div',
             implode('', [
               Html::tag(
@@ -218,8 +218,8 @@ $battle->his_team_count = 100;
             ]),
             ['class' => 'progress', 'style' => 'width:100%;max-width:400px']
           );
-        }
-        return null;
+          }
+          return null;
       },
       // }}}
     ],
@@ -227,11 +227,11 @@ $battle->his_team_count = 100;
       'label' => Yii::t('app', 'Final Count'), // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if ($model->my_team_count !== null && $model->his_team_count !== null) {
-          $myCount = (int)$model->my_team_count;
-          $hisCount = (int)$model->his_team_count;
-          $myDrawPct = round($myCount * 100 / ($myCount + $hisCount) * 100) / 100;
-          return Html::tag(
+          if ($model->my_team_count !== null && $model->his_team_count !== null) {
+              $myCount = (int)$model->my_team_count;
+              $hisCount = (int)$model->his_team_count;
+              $myDrawPct = round($myCount * 100 / ($myCount + $hisCount) * 100) / 100;
+              return Html::tag(
             'div',
             implode('', [
               Html::tag(
@@ -254,8 +254,8 @@ $battle->his_team_count = 100;
             ]),
             ['class' => 'progress', 'style' => 'width:100%;max-width:400px']
           );
-        }
-        return null;
+          }
+          return null;
       },
       // }}}
     ],
@@ -264,22 +264,22 @@ $battle->his_team_count = 100;
       'label' => Yii::t('app', 'Kills / Deaths'), // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        $parts = [];
-        $parts[] = Html::encode(sprintf(
+          $parts = [];
+          $parts[] = Html::encode(sprintf(
           '%s / %s',
           $model->kill === null ? '?' : $model->kill,
           $model->death === null ? '?' : $model->death
         ));
-        if ($model->kill !== null && $model->death !== null) {
-          if ($model->kill > $model->death) {
-            $parts[] = Html::tag('span', Html::encode('>'), ['class' => 'label label-success']);
-          } elseif ($model->kill < $model->death) {
-            $parts[] = Html::tag('span', Html::encode('<'), ['class' => 'label label-danger']);
-          } else {
-            $parts[] = Html::tag('span', Html::encode('='), ['class' => 'label label-default']);
+          if ($model->kill !== null && $model->death !== null) {
+              if ($model->kill > $model->death) {
+                  $parts[] = Html::tag('span', Html::encode('>'), ['class' => 'label label-success']);
+              } elseif ($model->kill < $model->death) {
+                  $parts[] = Html::tag('span', Html::encode('<'), ['class' => 'label label-danger']);
+              } else {
+                  $parts[] = Html::tag('span', Html::encode('='), ['class' => 'label label-default']);
+              }
           }
-        }
-        return implode(' ', $parts);
+          return implode(' ', $parts);
       },
       // }}}
     ],
@@ -287,10 +287,10 @@ $battle->his_team_count = 100;
       'label' => Yii::t('app', 'Kills+Assist / Specials'), // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if ($model->kill_or_assist === null && $model->special === null) {
-          return null;
-        }
-        return sprintf(
+          if ($model->kill_or_assist === null && $model->special === null) {
+              return null;
+          }
+          return sprintf(
           '%s / %s',
           $model->kill_or_assist === null ? '?' : $model->kill_or_assist,
           $model->special === null ? '?' : $model->special
@@ -301,10 +301,10 @@ $battle->his_team_count = 100;
     [
       'attribute' => 'kill_ratio', // {{{
       'value' => function ($model) {
-        if ($model->kill === null || $model->death === null) {
-          return null;
-        }
-        return $model->kill_ratio === null
+          if ($model->kill === null || $model->death === null) {
+              return null;
+          }
+          return $model->kill_ratio === null
           ? Yii::t('app', 'N/A')
           : Yii::$app->formatter->asDecimal($model->kill_ratio, 2);
       },
@@ -313,10 +313,10 @@ $battle->his_team_count = 100;
     [
       'attribute' => 'kill_rate', // {{{
       'value' => function ($model) {
-        if ($model->kill === null || $model->death === null) {
-          return null;
-        }
-        return $model->kill_rate === null
+          if ($model->kill === null || $model->death === null) {
+              return null;
+          }
+          return $model->kill_rate === null
           ? Yii::t('app', 'N/A')
           : Yii::$app->formatter->asPercent($model->kill_rate / 100, 1);
       },
@@ -327,13 +327,13 @@ $battle->his_team_count = 100;
     [
       'label' => Yii::t('app', 'Cause of Death'), // FIXME {{{
       'value' => function ($model) {
-        $reasons = $model->getBattleDeathReasons()
+          $reasons = $model->getBattleDeathReasons()
           ->orderBy('{{battle_death_reason2}}.[[count]] DESC')
           ->all();
-        if (!$reasons) {
+          if (!$reasons) {
+              return null;
+          }
           return null;
-        }
-        return null;
         // <tr>
         //   <th>{{'Cause of Death'|translate:'app'|escape}}</th>
         //   <td>
@@ -359,23 +359,23 @@ $battle->his_team_count = 100;
     [
       'label' => Yii::t('app', 'Turf Inked + Bonus'), // {{{
       'value' => function ($model) {
-        $inked = $model->inked;
-        if ($model->my_point === null) {
-          return null;
-        }
-        if ($inked === null) {
-          return Yii::$app->formatter->asInteger($model->my_point);
-        }
-        $bonus = $model->my_point - $inked;
-        if ($bonus > 0) {
-          return sprintf(
+          $inked = $model->inked;
+          if ($model->my_point === null) {
+              return null;
+          }
+          if ($inked === null) {
+              return Yii::$app->formatter->asInteger($model->my_point);
+          }
+          $bonus = $model->my_point - $inked;
+          if ($bonus > 0) {
+              return sprintf(
             '%sP + %sP',
             Yii::$app->formatter->asInteger($inked),
             Yii::$app->formatter->asInteger($bonus)
           );
-        } else {
-          return Yii::$app->formatter->asInteger($inked) . 'P';
-        }
+          } else {
+              return Yii::$app->formatter->asInteger($inked) . 'P';
+          }
       },
       // }}}
     ],
@@ -386,10 +386,10 @@ $battle->his_team_count = 100;
       'attribute' => 'start_at', // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if ($model->start_at === null) {
-          return null;
-        }
-        return sprintf(
+          if ($model->start_at === null) {
+              return null;
+          }
+          return sprintf(
           '%s (%s)',
           Yii::$app->formatter->asHtmlDatetime($model->start_at),
           Html::encode(Yii::$app->formatter->asRelativeTime($model->start_at))
@@ -401,10 +401,10 @@ $battle->his_team_count = 100;
       'attribute' => 'end_at', // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if ($model->end_at === null) {
-          return null;
-        }
-        return sprintf(
+          if ($model->end_at === null) {
+              return null;
+          }
+          return sprintf(
           '%s (%s)',
           Yii::$app->formatter->asHtmlDatetime($model->end_at),
           Html::encode(Yii::$app->formatter->asRelativeTime($model->end_at))
@@ -416,10 +416,10 @@ $battle->his_team_count = 100;
       'attribute' => 'created_at', // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if ($model->created_at === null) {
-          return null;
-        }
-        return sprintf(
+          if ($model->created_at === null) {
+              return null;
+          }
+          return sprintf(
           '%s (%s)',
           Yii::$app->formatter->asHtmlDatetime($model->created_at),
           Html::encode(Yii::$app->formatter->asRelativeTime($model->created_at))
@@ -431,10 +431,10 @@ $battle->his_team_count = 100;
       'label' => Yii::t('app', 'User Agent'), // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if (!$model->agent) {
-          return null;
-        }
-        return implode(' / ', [
+          if (!$model->agent) {
+              return null;
+          }
+          return implode(' / ', [
           $model->agent->productUrl
             ? Html::a(
               Html::encode($model->agent->name),
@@ -457,25 +457,25 @@ $battle->his_team_count = 100;
       'attribute' => 'ua_variables', // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if ($model->ua_variables === null) {
-          return null;
-        }
-        return Html::tag(
+          if ($model->ua_variables === null) {
+              return null;
+          }
+          return Html::tag(
           'table',
           Html::tag(
             'tbody',
             (function (array $rows) {
-              $ret = [];
-              foreach ($rows as $k => $v) {
-                $ret[] = Html::tag(
+                $ret = [];
+                foreach ($rows as $k => $v) {
+                    $ret[] = Html::tag(
                   'tr',
                   implode('', [
                     Html::tag('th', Html::encode(Yii::t('app-ua-vars', $k))),
                     Html::tag('td', Html::encode(Yii::t('app-ua-vars-v', $v))),
                   ])
                 );
-              }
-              return implode('', $ret);
+                }
+                return implode('', $ret);
             })($model->extraData)
           ),
           ['class' => 'table', 'style' => 'margin-bottom:0']
@@ -488,19 +488,19 @@ $battle->his_team_count = 100;
       'attribute' => 'private_note', // {{{
       'format' => 'raw',
       'value' => function ($model) {
-        if ($model->private_note == '') {
-          return null;
-        }
-        if (Yii::$app->user->isGuest || Yii::$app->user->identity->id != $model->user_id) {
-          return null;
-        }
-        $this->registerCss('#private-note{display:none}');
-        $this->registerJs(
+          if ($model->private_note == '') {
+              return null;
+          }
+          if (Yii::$app->user->isGuest || Yii::$app->user->identity->id != $model->user_id) {
+              return null;
+          }
+          $this->registerCss('#private-note{display:none}');
+          $this->registerJs(
             '!function(a){"use strict";var o=a("#private-note-show"),e=a("#private-note"),n=a(".fa",o);' .
             'o.hover(function(){n.removeClass("fa-lock").addClass("fa-unlock-alt")},function(){' .
             'n.removeClass("fa-unlock-alt").addClass("fa-lock")}).click(function(){o.hide(),e.show()})}(jQuery);'
         );
-        return implode('', [
+          return implode('', [
           Html::button(
             Html::tag('span', '', ['class' => 'fa fa-lock fa-fw']),
             ['class' => 'btn btn-default', 'id' => 'private-note-show']
@@ -517,7 +517,7 @@ $battle->his_team_count = 100;
     [
       'label' => Yii::t('app', 'Game Version'), // {{{
       'value' => function ($model) {
-        return $model->splatoonVersion->name ?? Yii::t('app', 'Unknown');
+          return $model->splatoonVersion->name ?? Yii::t('app', 'Unknown');
       },
       // }}}
     ],
