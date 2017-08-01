cbe5704f587212ba6fc318e71e6ce5603a2befa2
AIZAWA Hina <hina@bouhime.com>
2017-08-01 20:34:11 +0900

    :two: fix #232 ミステリーゾーンの追加
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m170801_112751_mystery_zone.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m170801_112751_mystery_zone extends Migration
+{
+    public function safeUp()
+    {
+        $this->insert('{{map2}}', [
+            'key'        => 'mystery',
+            'name'       => 'Shifty Station',
+            'short_name' => 'Shifty',
+            'release_at' => '2017-08-04T15:00:00+09',
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->delete('{{map2}}', ['key' => 'mystery']);
+    }
+}
