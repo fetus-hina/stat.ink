697df785c9a29c75b8f7480170cb9dd3a64dcd9d
AIZAWA Hina <hina@bouhime.com>
2017-08-08 20:11:18 +0900

    Update translation
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ messages/fr/map2.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+return [
+    'Any Stage' => '',
+
+    'Humpback Pump Track' => 'Piste Méroule',
+    'Inkblot Art Academy' => 'Institut Calam\'art',
+    'Moray Towers' => 'Tours Girelle',
+    'Musselforge Fitness' => 'Gymnase Ancrage',
+    'Port Mackerel' => 'Docks Haddock',
+    'Shifty Station' => 'Plateforme polymorphe',
+    'Starfish Mainstage' => 'Scène Sirène',
+    'Sturgeon Shipyard' => 'Chantier Narval',
+    'The Reef' => 'Allées salées',
+
+    'Track' => 'Piste Méroule',
+    'Academy' => 'Institut Calam\'art',
+    'Towers' => 'Tours Girelle',
+    'Fitness' => 'Gymnase Ancrage',
+    'Port' => 'Docks Haddock',
+    'Shifty' => 'Plateforme polymorphe',
+    'Mainstage' => 'Scène Sirène',
+    'Shipyard' => 'Chantier Narval',
+    'Reef' => 'Allées salées',
+];
