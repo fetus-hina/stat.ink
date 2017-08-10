11b12473e16d87d6efb58616b0a4e1e68c507c5c
AIZAWA Hina <hina@bouhime.com>
2017-08-10 13:18:46 +0900

    :sparkles: Add some known user-agents (fix #244)

---
+++ migrations/m170810_035817_add_ua_attrs.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m170810_035817_add_ua_attrs extends Migration
+{
+    public function safeUp()
+    {
+        $this->batchInsert('agent_attribute', ['name', 'is_automated', 'link_url'], [
+            ['IkaRec-en', false, 'https://play.google.com/store/apps/details?id=ink.pocketgopher.ikarec'],
+            ['stat.ink web client', false, 'https://stat.ink/'],
+            ['IkaRec2', false, 'https://play.google.com/store/apps/details?id=com.syanari.merluza.ikarec2'],
+            ['splatnet2statink', true, 'https://github.com/frozenpandaman/splatnet2statink'],
+            ['SquidTracks', true, 'https://github.com/hymm/squid-tracks/'],
+            ['SplatTrack', true, 'https://github.com/hymm/squid-tracks/'],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->delete('agent_attribute', ['name' => [
+            'IkaRec-en',
+            'stat.ink web client',
+            'IkaRec2',
+            'splatnet2statink',
+            'SquidTracks',
+            'SplatTrack',
+        ]]);
+    }
+}
---
+++ migrations/m170810_040928_ua_attr_groups.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use yii\db\Query;
+
+class m170810_040928_ua_attr_groups extends Migration
+{
+    public function safeUp()
+    {
+        $this->insert('agent_group', ['name' => 'SquidTracks']);
+
+        $ikaRec = $this->getGroupId('IkaRec');
+        $squidTracks = $this->getGroupId('SquidTracks');
+
+        $this->batchInsert('agent_group_map', ['group_id', 'agent_name'], [
+            [$ikaRec, 'IkaRec2'],
+            [$squidTracks, 'SquidTracks'],
+            [$squidTracks, 'SplatTrack'],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->delete('agent_group_map', ['name' => [
+            'IkaRec2',
+            'SquidTracks',
+            'SplatTrack',
+        ]]);
+        $this->delete('agent_group', ['name' => 'SquidTracks']);
+    }
+
+    private function getGroupId(string $name) : int
+    {
+        $ret = (new Query())
+            ->select('id')
+            ->from('agent_group')
+            ->where(['name' => $name])
+            ->scalar();
+        if (!$ret) {
+            throw new \Exception('Invalid UA group name: ' . $name);
+        }
+        return (int)$ret;
+    }
+}
