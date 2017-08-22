124a63a4013390202fd3ec7427dc146062331f56
AIZAWA Hina <hina@bouhime.com>
2017-08-22 18:17:08 +0900

    :two: Splatoon 2 1.2.0
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m170822_090200_splatoon2_120.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m170822_090200_splatoon2_120 extends Migration
+{
+    public function safeUp()
+    {
+        $this->batchInsert('splatoon_version2', [ 'tag', 'name', 'released_at' ], [
+            ['1.2.0', '1.2.0', '2017-08-23T11:00:00+09:00'],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'battle2',
+            ['version_id' => $this->getId('1.1.2')],
+            ['version_id' => $this->getId('1.2.0')]
+        );
+        $this->delete('splatoon_version2', ['tag' => '1.2.0']);
+    }
+
+    private function getid(string $tag) : int
+    {
+        return (new Query())
+            ->select('id')
+            ->from('splatoon_version2')
+            ->where(['tag' => $tag])
+            ->limit(1)
+            ->scalar();
+    }
+}
