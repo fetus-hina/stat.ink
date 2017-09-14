3efafa178ea997502143950998662bacc6473a73
AIZAWA Hina <hina@bouhime.com>
2017-09-14 15:40:06 +0900

    Fix weapon data of Custom Jr.

---
+++ migrations/m170914_062301_fix_momiji.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use app\components\db\WeaponMigration;
+
+class m170914_062301_fix_momiji extends Migration
+{
+    use WeaponMigration;
+
+    public function safeUp()
+    {
+        $this->update('weapon2', [
+            'main_group_id' => $this->findWeaponId('wakaba'),
+        ], [
+            'key' => 'momiji',
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->update('weapon2', [
+            'main_group_id' => $this->findWeaponId('momiji'),
+        ], [
+            'key' => 'momiji',
+        ]);
+    }
+}
