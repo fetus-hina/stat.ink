a3d0d423c6ba38fe26f6a6f55d0f74f7803d85ec
AIZAWA Hina <hina@bouhime.com>
2017-09-17 00:41:41 +0900

    First commit for German Language
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ messages/de/map2.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+return [
+    'Academy' => '',
+    'Any Stage' => '',
+    'Canal' => '',
+    'Dome' => '',
+    'Fitness' => '',
+    'Humpback Pump Track' => '',
+    'Inkblot Art Academy' => '',
+    'Kelp Dome' => '',
+    'Mainstage' => '',
+    'Manta' => '',
+    'Manta Maria' => '',
+    'Moray Towers' => '',
+    'Musselforge Fitness' => '',
+    'Port' => '',
+    'Port Mackerel' => '',
+    'Reef' => '',
+    'Shifty' => '',
+    'Shifty Station' => '',
+    'Shipyard' => '',
+    'Snapper Canal' => '',
+    'Starfish Mainstage' => '',
+    'Sturgeon Shipyard' => '',
+    'The Reef' => '',
+    'Towers' => '',
+    'Track' => '',
+];
---
+++ migrations/m170916_153557_i18n_de.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use app\models\Charset;
+use app\models\Language;
+
+class m170916_153557_i18n_de extends Migration
+{
+    public function safeUp()
+    {
+        $this->batchInsert('language', ['lang', 'name', 'name_en'], [
+            ['de-DE', 'Deutsch', 'German'],
+        ]);
+
+        $deDe = Language::findOne(['lang' => 'de-DE'])->id;
+        $utf8 = Charset::findOne(['php_name' => 'UTF-8'])->id;
+        $utf16 = Charset::findOne(['php_name' => 'UTF-16LE'])->id;
+        $ansi = Charset::findOne(['php_name' => 'CP1252'])->id;
+
+        $this->batchInsert('language_charset', ['language_id', 'charset_id', 'is_win_acp'], [
+            [ $deDe, $utf8, false ],
+            [ $deDe, $utf16, false ],
+            [ $deDe, $ansi, true ],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $deDe = Language::findOne(['name' => 'de-DE'])->id;
+        $this->delete('language_charset', ['language_id' => $deDe]);
+        $this->delete('language', ['id' => $deDe]);
+    }
+}
