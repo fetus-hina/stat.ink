cd310a02fb73eb8523665af79eff45c29a438c39
AIZAWA Hina <hina@bouhime.com>
2017-09-17 00:24:22 +0900

    Update SplatNet ID
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m170916_152136_update_splatnet_id.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m170916_152136_update_splatnet_id extends Migration
+{
+    public function safeUp()
+    {
+        $this->update('map2', ['splatnet' => 10], ['key' => 'mozuku']);
+        $this->update('weapon2', ['splatnet' => 4011], ['key' => 'barrelspinner_deco']);
+    }
+
+    public function safeDown()
+    {
+        $this->update('map2', ['splatnet' => null], ['key' => 'mozuku']);
+        $this->update('weapon2', ['splatnet' => null], ['key' => 'barrelspinner_deco']);
+    }
+}
