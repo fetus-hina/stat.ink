094874ce741e94891c37bb3ba281c47dee7a335c
AIZAWA Hina <hina@bouhime.com>
2017-09-30 14:13:26 +0900

    oops

---
+++ migrations/m170930_050801_campingshelter_id.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m170930_050801_campingshelter_id extends Migration
+{
+    public function safeUp()
+    {
+        $this->update('weapon2', ['splatnet' => 6010], ['key' => 'campingshelter']);
+    }
+
+    public function safeDown()
+    {
+        $this->update('weapon2', ['splatnet' => null], ['key' => 'campingshelter']);
+    }
+}
