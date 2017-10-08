eba3f6f1f94a7165f6228222cb7f26c98cfbd9b5
AIZAWA Hina <hina@bouhime.com>
2017-10-09 05:02:23 +0900

    Maintenance mode

--- web/index.php
+++ web/index.php
@@ -11,6 +11,16 @@ if (preg_match(
 ini_set('display_errors', 1);
 ini_set('serialize_precision', -1);
 
+if (@file_exists(__DIR__ . '/../.maintenance')) {
+    header('HTTP/1.1 503 Service Unavailable');
+    header('Content-Type: text/plain; charset=UTF-8');
+    echo "This website is under maintenance.\n";
+    echo "It will be back soon.\n";
+    echo "\n";
+    echo "メンテナンス中です。\n";
+    exit(0);
+}
+
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
