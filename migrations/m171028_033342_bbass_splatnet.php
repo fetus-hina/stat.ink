16f732e4138ce6088cb9a7109fa9c576990cbceb
AIZAWA Hina <hina@bouhime.com>
2017-10-28 12:35:44 +0900

    Set SplatNet ID for bbass
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m171028_033342_bbass_splatnet.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m171028_033342_bbass_splatnet extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'map2',
+            ['splatnet' => 11],
+            ['key' => 'bbass']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'map2',
+            ['splatnet' => null],
+            ['key' => 'bbass']
+        );
+    }
+}
