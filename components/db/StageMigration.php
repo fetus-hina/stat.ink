521028e8677b58196396e0b86b10be99b6929791
AIZAWA Hina <hina@bouhime.com>
2017-10-29 22:32:16 +0900

    Set area sizes of mozuku, engawa, bbass
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ components/db/StageMigration.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+namespace app\components\db;
+
+use Yii;
+use yii\db\Expression;
+use yii\db\Query;
+
+trait StageMigration
+{
+    Protected function setArea(array $list) : void
+    {
+        $db = Yii::$app->db;
+        $value = new Expression(vsprintf('(CASE %s %s END)', [
+            $db->quoteColumnName('key'),
+            (function () use ($list, $db) : string {
+                return implode(' ', array_map(
+                    function (string $key, ?int $area) use ($db) : string {
+                        return vsprintf('WHEN %s THEN %s', [
+                            $db->quoteValue($key),
+                            $area === null ? 'NULL' : $db->quoteValue($area),
+                        ]);
+                    },
+                    array_keys($list),
+                    array_values($list)
+                ));
+            })(),
+        ]));
+        $this->execute(
+            $db->createCommand()
+                ->update('map2', ['area' => $value], ['key' => array_keys($list)])
+                ->rawSql
+        );
+    }
+}
---
+++ migrations/m171029_122851_map2_area.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use app\components\db\StageMigration;
+
+class m171029_122851_map2_area extends Migration
+{
+    use StageMigration;
+
+    public function safeUp()
+    {
+        $this->setArea($this->getList());
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'map2',
+            ['area' => null],
+            ['key' => array_keys($this->getList())]
+        );
+    }
+
+    public function getList()
+    {
+        return [
+            'mozuku'    => 2315,
+            'engawa'    => 2250,
+            'bbass'     => 1947,
+        ];
+    }
+}
