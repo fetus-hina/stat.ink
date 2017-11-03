b6dfd141f095d3e2b0c8b560eb40433e9f4aeb35
AIZAWA Hina <hina@bouhime.com>
2017-11-03 20:39:02 +0900

    fix style
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- messages/ja/ability2.php
+++ messages/ja/ability2.php
@@ -3,7 +3,6 @@
  * @copyright Copyright (C) 2015-2017 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@bouhime.com>
- * @author mntone <sd8@live.jp>
  */
 
 return [
