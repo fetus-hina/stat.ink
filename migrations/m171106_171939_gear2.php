6a642955e84311a4e0de45e53e74593cea3daace
AIZAWA Hina <hina@bouhime.com>
2017-11-07 02:24:35 +0900

    Update Gear2
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m171106_171939_gear2.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m171106_171939_gear2 extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'gear2',
+            ['key' => 'painters_mask', 'splatnet' => 8004],
+            ['key' => 'painter_s_mask']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'gear2',
+            ['key' => 'painter_s_mask', 'splatnet' => null],
+            ['key' => 'painters_mask']
+        );
+    }
+}
