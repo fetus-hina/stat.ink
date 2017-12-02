e5255172e35772bc8e599b4368b268dc8e18e6e4
AIZAWA Hina <hina@bouhime.com>
2017-12-02 14:15:31 +0900

    Update SplatNet ID for Glooga Dualies
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m171202_051349_kelvin525_splatnet.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m171202_051349_kelvin525_splatnet extends Migration
+{
+    public function safeUp()
+    {
+        $this->update('weapon2', ['splatnet' => 5020], ['key' => 'kelvin525']);
+    }
+
+    public function safeDown()
+    {
+        $this->update('weapon2', ['splatnet' => null], ['key' => 'kelvin525']);
+    }
+}
