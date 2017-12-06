b9ad50bf6a89252eeed99d3ee901a85ad453798a
AIZAWA Hina <hina@bouhime.com>
2017-12-07 01:16:43 +0900

    Update gear DB (sync with https://github.com/frozenpandaman/splatnet2statink/commit/a8e1a384334e69710378bf3e04d6ccede68c1d3c)
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- data/gear_id/run.sh
+++ data/gear_id/run.sh
@@ -3,14 +3,6 @@
 set -eu
 cd $(cd $(dirname $0); pwd)
 
-echo "Downloading Eli's DB..."
-
 rm -f dbs.py dbs.pyc
 curl -sSL -o dbs.py https://github.com/frozenpandaman/splatnet2statink/raw/master/dbs.py
-
-echo "Downloaded the DB."
-echo "Converting to JSON..."
-
 ./db2json
-
-
---
+++ migrations/m171206_160848_gear2.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m171206_160848_gear2 extends Migration
+{
+    public function safeUp()
+    {
+        $data = $this->getUpdateData();
+        $updateCase = new \yii\db\Expression(sprintf(
+            '(CASE %s %s END)',
+            $this->db->quoteColumnName('key'),
+            implode(' ', array_map(
+                function (string $key, int $value) : string {
+                    return sprintf(
+                        'WHEN %s THEN %s',
+                        $this->db->quoteValue($key),
+                        $this->db->quoteValue($value)
+                    );
+                },
+                array_keys($data),
+                array_values($data)
+            ))
+        ));
+        $this->update(
+            'gear2',
+            ['splatnet' => $updateCase],
+            ['key' => array_keys($data)]
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'gear2',
+            ['splatnet' => null],
+            ['key' => array_keys($this->getUpdateData())]
+        );
+    }
+
+    public function getUpdateData() : array
+    {
+        return [
+            'red_slip_ons' => 7001,
+            'squid_stitch_slip_ons' => 7002,
+            'tulip_parasol' => 4010,
+            'varsity_baseball_ls' => 2005,
+        ];
+    }
+}
