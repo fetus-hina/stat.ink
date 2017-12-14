c3a19321d0a65d89bec17d9930b4eabf630f22d2
AIZAWA Hina <hina@bouhime.com>
2017-12-14 16:56:57 +0900

    Update stage data
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m171214_073702_stage2_meta.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m171214_073702_stage2_meta extends Migration
+{
+    public function safeUp()
+    {
+        $this->update('map2', ['release_at' => '2017-12-15T11:00:00+09:00'], ['key' => 'hakofugu']);
+        $this->update('map2', ['area' => 2166], ['key' => 'zatou']);
+    }
+
+    public function safeDown()
+    {
+        $this->update('map2', ['area' => null], ['key' => 'zatou']);
+        $this->update('map2', ['release_at' => null], ['key' => 'hakofugu']);
+    }
+}
