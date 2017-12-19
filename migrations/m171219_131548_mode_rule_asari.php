7d98802292d3f38754f497f993139191e408381b
AIZAWA Hina <hina@bouhime.com>
2017-12-19 22:23:03 +0900

    Fix api/v2/rule for Clam Blitz
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m171219_131548_mode_rule_asari.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use yii\db\Query;
+use yii\helpers\ArrayHelper;
+
+class m171219_131548_mode_rule_asari extends Migration
+{
+    public function safeUp()
+    {
+        $modes = $this->getModes();
+        $rules = $this->getRules();
+
+        $this->batchInsert('mode_rule2', ['mode_id', 'rule_id'], [
+            [$modes['gachi'], $rules['asari']],
+            [$modes['private'], $rules['asari']],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $rules = $this->getRules();
+        $this->delete('mode_rule2', ['rule_id' => $rules['asari']]);
+    }
+
+    private function getModes() : array
+    {
+        return ArrayHelper::map(
+            (new Query())->select(['key', 'id'])->from('mode2')->all(),
+            'key',
+            'id'
+        );
+    }
+
+    private function getRules() : array
+    {
+        return ArrayHelper::map(
+            (new Query())->select(['key', 'id'])->from('rule2')->all(),
+            'key',
+            'id'
+        );
+    }
+}
