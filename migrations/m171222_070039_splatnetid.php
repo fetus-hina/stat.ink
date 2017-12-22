2c058a4b0702ea2dad04a4c528505b7c6b96618d
AIZAWA Hina <hina@bouhime.com>
2017-12-22 16:02:57 +0900

    Update SplatNet ID for Squeezer
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m171222_070039_splatnetid.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m171222_070039_splatnetid extends Migration
+{
+    public function safeUp()
+    {
+        $this->update('weapon2', ['splatnet' => 400], ['key' => 'bottlegeyser']);
+    }
+
+    public function safeDown()
+    {
+        $this->update('weapon2', ['splatnet' => null], ['key' => 'bottlegeyser']);
+    }
+}
