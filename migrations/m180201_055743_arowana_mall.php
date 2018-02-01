2fd7813586a128da0b7b710070e6a23e811e2e0c
AIZAWA Hina <hina@bouhime.com>
2018-02-01 18:56:46 +0900

    Set Arowana Mall's SplatNet ID
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180201_055743_arowana_mall.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m180201_055743_arowana_mall extends Migration
+{
+    public function safeUp()
+    {
+        $this->update('map2', ['splatnet' => 15], ['key' => 'arowana']);
+    }
+
+    public function safeDown()
+    {
+        $this->update('map2', ['splatnet' => null], ['key' => 'arowana']);
+    }
+}
