2434bfdcc4fab70bca587c546c8c9c6d71c2ab64
AIZAWA Hina <hina@bouhime.com>
2018-02-01 14:54:59 +0900

    Add Welding Mask
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180201_053837_add_welding_mask.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\GearMigration;
+use app\components\db\Migration;
+
+class m180201_053837_add_welding_mask extends Migration
+{
+    use GearMigration;
+
+    public function safeUp()
+    {
+        $this->upGear2(
+            static::name2key('Welding Mask'),
+            'Welding Mask',
+            'headgear',
+            static::name2key('Grizzco'),
+            null,
+            21002
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downGear2(
+            static::name2key('Welding Mask')
+        );
+    }
+}
