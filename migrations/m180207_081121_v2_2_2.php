6fab129955791976eb475c3ff9a16603bdc67c48
AIZAWA Hina <hina@bouhime.com>
2018-02-07 17:13:29 +0900

    Splatoon 2 v2.2.2
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180207_081121_v2_2_2.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m180207_081121_v2_2_2 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '2.2',
+            '2.2.x',
+            '2.2.2',
+            '2.2.2',
+            new DateTimeImmutable('2018-02-07T17:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('2.2.2', '2.2.1');
+    }
+}
