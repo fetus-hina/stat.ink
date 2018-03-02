45b05f0d631a5d490af75018fd105960fb39b1ef
AIZAWA Hina <hina@bouhime.com>
2018-03-02 14:18:21 +0900

    Set SplatNet ID of Dark Tetra Dualies
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180302_051616_quadhopper_id.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m180302_051616_quadhopper_id extends Migration
+{
+    public function safeUp()
+    {
+        $this->update('weapon2', ['splatnet' => 5040], ['key' => 'quadhopper_black']);
+    }
+
+    public function safeDown()
+    {
+        $this->update('weapon2', ['splatnet' => null], ['key' => 'quadhopper_black']);
+    }
+}
