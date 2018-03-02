9d36b81b3bf7f3a5a4c4f12c64eec54bf1d79200
AIZAWA Hina <hina@bouhime.com>
2018-03-02 13:46:57 +0900

    OMG
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180301_103246_quadhopper_black.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use app\components\db\WeaponMigration;
+
+class m180301_103246_quadhopper_black extends Migration
+{
+    use WeaponMigration;
+
+    public function safeUp()
+    {
+        $this->upWeapon(
+            'quadhopper_black',
+            'Dark Tetra Dualies',
+            'maneuver',
+            'robotbomb',
+            'chakuchi'
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downWeapon('quadhopper_black');
+    }
+}
