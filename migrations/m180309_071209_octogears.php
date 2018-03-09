a2ea5180bd3f6ac1ad93c3f48a6c25c4e6072778
AIZAWA Hina <hina@bouhime.com>
2018-03-09 16:23:31 +0900

    Add Octo gears (expansion)
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180309_071209_octogears.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\GearMigration;
+use app\components\db\Migration;
+
+class m180309_071209_octogears extends Migration
+{
+    use GearMigration;
+
+    public function safeUp()
+    {
+        $this->upGear2(
+            static::name2key('Studio Octophones'),
+            'Studio Octophones',
+            'headgear',
+            'cuttlegear',
+            'ink_recovery_up',
+            5005
+        );
+        $this->upGear2(
+            static::name2key('Octo Layered LS'),
+            'Octo Layered LS',
+            'clothing',
+            'cuttlegear',
+            'ink_saver_main',
+            3012
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downGear2(static::name2key('Studio Octophones'));
+        $this->downGear2(static::name2key('Octo Layered LS'));
+    }
+}
