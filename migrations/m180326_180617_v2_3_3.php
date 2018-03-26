317010c07ef939a876f80852618590df63c2702a
AIZAWA Hina <hina@bouhime.com>
2018-03-27 03:07:47 +0900

    Splatoon 2 v2.3.3

---
+++ migrations/m180326_180617_v2_3_3.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m180326_180617_v2_3_3 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '2.3',
+            '2.3.x',
+            '2.3.3',
+            '2.3.3',
+            new DateTimeImmutable('2018-03-26T11:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('2.3.3', '2.3.2');
+    }
+}
