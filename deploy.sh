61f53ab6000dc31b26e843aa79df8ce998e062ff
AIZAWA Hina <hina@bouhime.com>
2018-04-18 19:05:15 +0900

    Update deploy script
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,17 @@
 
 set -eu
 
-git push origin master
+if [ $# -ne 1 ]; then
+  echo "USAGE: $0 vA.B.C"
+  exit 1
+fi
+
+if [[ $1 =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  git tag -s $1 -m $1
+else
+  echo "Invalid version tag $1. Should be like v1.2.3"
+  exit 1
+fi
+
+git push origin master $1
 ssh statink@app1.stat.ink 'pushd stat.ink && git fetch origin && git merge --ff-only origin/master && scl enable php71 make && rm -rfv web/assets/* runtime/Smarty/compile/*'
