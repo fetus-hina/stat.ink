15eedc2a620f93282e0192f6f83e4261bec46894
AIZAWA Hina <hina@bouhime.com>
2018-04-25 03:14:54 +0900

    Update stages data
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180424_175751_update_map.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m180424_175751_update_map extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'map2',
+            [
+                'splatnet' => 16,
+                'release_at' => '2018-04-25T11:00:00+09:00',
+            ],
+            ['key' => 'mongara']
+        );
+        $this->update(
+            'map2',
+            [
+                'area' => 3081,
+                'release_at' => '2018-03-31T11:00:00+09:00',
+            ],
+            ['key' => 'shottsuru']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'map2',
+            ['splatnet' => null, 'release_at' => null],
+            ['key' => 'mongara']
+        );
+        $this->update(
+            'map2',
+            ['area' => null, 'release_at' => null],
+            ['key' => 'shottsuru']
+        );
+    }
+}
