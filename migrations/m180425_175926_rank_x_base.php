e2f6a2cb1866a81cd8ada90d52c04d5bdb10ec18
AIZAWA Hina <hina@bouhime.com>
2018-04-26 03:04:45 +0900

    update int_base for Rank X (fix sidebar "peak")
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180425_175926_rank_x_base.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m180425_175926_rank_x_base extends Migration
+{
+    public function safeUp()
+    {
+        $this->update('rank2', ['int_base' => 1100], ['key' => 'x']); 
+    }
+
+    public function safeDown()
+    {
+        $this->update('rank2', ['int_base' => 1010], ['key' => 'x']); 
+    }
+}
