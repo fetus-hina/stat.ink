0b985852833109363e08764d31ad67bee3908eef
AIZAWA Hina <hina@bouhime.com>
2018-05-09 13:18:37 +0900

    Add key-string fixer for https://github.com/frozenpandaman/splatnet2statink/commit/d6c6826e685ce2ccdedf4f941ba5b069d38df8d4
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- models/api/v2/PostClothingForm.php
+++ models/api/v2/PostClothingForm.php
@@ -7,8 +7,25 @@
 
 namespace app\models\api\v2;
 
+use app\components\behaviors\FixAttributesBehavior;
+
 class PostClothingForm extends BaseGearForm
 {
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            [
+                'class' => FixAttributesBehavior::class,
+                'attributes' => [
+                    'gear' => [
+                        // https://github.com/frozenpandaman/splatnet2statink/commit/d6c6826e685ce2ccdedf4f941ba5b069d38df8d4
+                        'kung-fu_zip_up' => 'kung_fu_zip_up',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
     public function getType()
     {
         return 'clothing';
