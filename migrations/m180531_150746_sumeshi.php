846679967469412ab0b521261ea104629f2d2070
AIZAWA Hina <hina@bouhime.com>
2018-06-01 00:19:36 +0900

    Update Splatnet # of Wahoo World
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180531_150746_sumeshi.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+
+class m180531_150746_sumeshi extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'map2',
+            [
+                'splatnet' => 20,
+                'release_at' => '2018-06-01T09:00:00+09:00',
+            ],
+            ['key' => 'sumeshi']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'map2',
+            [
+                'splatnet' => null,
+                'release_at' => null,
+            ],
+            ['key' => 'sumeshi']
+        );
+    }
+}
