feceaffcb0059a201b710ac694c65545e800a37e
AIZAWA Hina <hina@bouhime.com>
2018-06-15 00:38:20 +0900

    v3.1.0
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180614_153323_v3_1_0.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m180614_153323_v3_1_0 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '3.1',
+            '3.1.x',
+            '3.1.0',
+            '3.1.0',
+            new DateTimeImmutable('2018-06-14T11:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('3.1.0', '3.0.1');
+    }
+}
