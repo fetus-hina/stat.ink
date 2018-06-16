cf764c74dd356167d6f55c497010c192e954579c
AIZAWA Hina <hina@bouhime.com>
2018-06-16 20:48:34 +0900

    Add missing OCTO gears
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180616_111627_missing_octo_gears.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\GearMigration;
+use app\components\db\Migration;
+use yii\db\Query;
+
+class m180616_111627_missing_octo_gears extends Migration
+{
+    use GearMigration;
+
+    public function safeUp()
+    {
+        $this->upGear2(
+            static::name2key('Octoleet Goggles'),
+            'Octoleet Goggles',
+            'headgear',
+            static::name2key('Grizzco'),
+            null,
+            21004
+        );
+        $this->upGear2(
+            static::name2key('Fresh Octo Tee'),
+            'Fresh Octo Tee',
+            'clothing',
+            static::name2key('Cuttlegear'),
+            static::name2key('Ink Saver (Sub)'),
+            3
+        );
+
+        $brand = (new Query())
+            ->select(['id'])
+            ->from('brand2')
+            ->where(['key' => static::name2key('Grizzco')])
+            ->scalar();
+
+        $this->update(
+            'gear2',
+            ['brand_id' => $brand],
+            ['key' => [
+                static::name2key('Octoleet Armor'),
+                static::name2key('Octoleet Boots'),
+            ]]
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downGear2(static::name2key('Octoleet Goggles'));
+        $this->downGear2(static::name2key('Fresh Octo Tee'));
+
+        $brand = (new Query())
+            ->select(['id'])
+            ->from('brand2')
+            ->where(['key' => static::name2key('Cuttlegear')])
+            ->scalar();
+
+        $this->update(
+            'gear2',
+            ['brand_id' => $brand],
+            ['key' => [
+                static::name2key('Octoleet Armor'),
+                static::name2key('Octoleet Boots'),
+            ]]
+        );
+    }
+}
