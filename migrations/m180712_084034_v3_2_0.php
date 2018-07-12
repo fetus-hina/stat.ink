62285bca70e13bf6349f85af0245946d7d45106b
AIZAWA Hina <hina@bouhime.com>
2018-07-12 17:42:32 +0900

    v3.2.0
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m180712_084034_v3_2_0.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m180712_084034_v3_2_0 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '3.2',
+            '3.2.x',
+            '3.2.0',
+            '3.2.0',
+            new DateTimeImmutable('2018-07-13T11:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('3.2.0', '3.1.0');
+    }
+}
