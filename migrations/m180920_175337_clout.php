5734c3187e643e1bdf3f9f8e685cb9c4f69ef96b
AIZAWA Hina <hina@bouhime.com>
2018-09-20 19:20:59 +0000

    Apply fixes from StyleCI

--- migrations/m180920_175337_clout.php
+++ migrations/m180920_175337_clout.php
@@ -26,7 +26,7 @@ class m180920_175337_clout extends Migration
             'clout'                 => $this->integer()->null(),
             'total_clout'           => $this->integer()->null(),
             'total_clout_after'     => $this->integer()->null(),
-            'synergy_bonus'         => $this->decimal(2,1)->null(),
+            'synergy_bonus'         => $this->decimal(2, 1)->null(),
             'my_team_win_streak'    => $this->integer()->null(),
             'his_team_win_streak'   => $this->integer()->null(),
         ];
