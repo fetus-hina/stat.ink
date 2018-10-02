77aae034bd2083544dd5c9e2af9adbd4abea150e
AIZAWA Hina <hina@bouhime.com>
2018-10-02 17:54:22 +0900

    Add Kensa weapons vol.2
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m181002_083734_becchu.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\WeaponMigration;
+
+class m181002_083734_becchu extends Migration
+{
+    use WeaponMigration;
+
+    public function safeUp()
+    {
+        foreach ($this->getWeapons() as $weaponData) {
+            call_user_func_array([$this, 'upWeapon'], $weaponData);
+        }
+    }
+
+    public function safeDown()
+    {
+        foreach ($this->getWeapons() as $weaponData) {
+            $this->downWeapon($weaponData[0]);
+        }
+    }
+
+    public function getWeapons(): array
+    {
+        return [
+            ['prime_becchu', 'Kensa Splattershot Pro', 'shooter', 'splashbomb', 'nicedama', 'prime', null, 72],
+            ['nova_becchu', 'Kensa Luna Blaster', 'blaster', 'tansanbomb', 'amefurashi', 'nova', null, 202],
+            ['dynamo_becchu', 'Kensa Dynamo Roller', 'roller', 'sprinkler', 'nicedama', 'dynamo', null, 1022],
+            ['screwslosher_becchu', 'Kensa Sloshing Machine', 'slosher', 'tansanbomb', 'chakuchi', 'screwslosher', null, 3022],
+        ];
+    }
+}
