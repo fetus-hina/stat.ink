2eb650ff51aa1b94b4fe932f9edb6b295d8e392d
AIZAWA Hina <hina@bouhime.com>
2018-10-05 19:18:28 +0900

    Update weapon migration (salmon run support, #363)
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- components/db/WeaponMigration.php
+++ components/db/WeaponMigration.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * @copyright Copyright (C) 2015-2017 AIZAWA Hina
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@bouhime.com>
  */
+declare(strict_types=1);
+
 namespace app\components\db;
 
 use yii\db\Expression;
@@ -20,42 +22,65 @@ trait WeaponMigration
         ?string $main = null,
         ?string $canonical = null,
         ?int $splatnet = null
-    ) : void {
-        $this->insert('weapon2', [
-            'key'           => $key,
-            'name'          => $name,
-            'type_id'       => $this->findId('weapon_type2', $type),
-            'subweapon_id'  => $this->findId('subweapon2', $sub),
-            'special_id'    => $this->findId('special2', $special),
-            'main_group_id' => ($main !== null)
-                ? $this->findId('weapon2', $main)
-                : new Expression("currval('weapon2_id_seq'::regclass)"),
-            'canonical_id'  => ($canonical !== null)
-                ? $this->findId('weapon2', $canonical)
-                : new Expression("currval('weapon2_id_seq'::regclass)"),
-            'splatnet'      => $splatnet,
-        ]);
+    ): void {
+        $this->db->transaction(function () use (
+            $key,
+            $name,
+            $type,
+            $sub,
+            $special,
+            $main,
+            $canonical,
+            $splatnet
+        ): void {
+            $this->insert('weapon2', [
+                'key' => $key,
+                'name' => $name,
+                'type_id' => $this->findId('weapon_type2', $type),
+                'subweapon_id' => $this->findId('subweapon2', $sub),
+                'special_id' => $this->findId('special2', $special),
+                'main_group_id' => ($main !== null)
+                    ? $this->findId('weapon2', $main)
+                    : new Expression("currval('weapon2_id_seq'::regclass)"),
+                'canonical_id' => ($canonical !== null)
+                    ? $this->findId('weapon2', $canonical)
+                    : new Expression("currval('weapon2_id_seq'::regclass)"),
+                'splatnet' => $splatnet,
+            ]);
 
-        $this->insert('death_reason2', [
-            'key'           => $key,
-            'name'          => $name,
-            'type_id'       => $this->findId('death_reason_type2', 'main'),
-            'weapon_id'     => $this->findId('weapon2', $key),
-        ]);
+            $this->insert('death_reason2', [
+                'key' => $key,
+                'name' => $name,
+                'type_id' => $this->findId('death_reason_type2', 'main'),
+                'weapon_id' => $this->findId('weapon2', $key),
+            ]);
+
+            if ($main === null) {
+                $this->insert('salmon_main_weapon2', [
+                    'key' => $key,
+                    'name' => $name,
+                    'splatnet' => $splatnet,
+                    'weapon_id' => $this->findId('weapon2', $key),
+                ]);
+            }
+        });
     }
 
-    protected function downWeapon(string $key)
+    protected function downWeapon(string $key): void
     {
-        $this->delete('death_reason2', ['key' => $key]);
-        $this->delete('weapon2', ['key' => $key]);
+        $this->db->transaction(function () use ($key): void {
+            $this->delete('salmon_main_weapon2', ['key' => $key]);
+            $this->delete('death_reason2', ['key' => $key]);
+            $this->delete('weapon2', ['key' => $key]);
+        });
     }
 
-    protected function findWeaponId(string $key) : int
+    protected function findWeaponId(string $key): int
     {
         return $this->findId('weapon2', $key);
     }
 
-    private function findId(string $table, string $key) : int
+    private function findId(string $table, string $key): int
     {
         $id = (new Query())
             ->select('id')
@@ -66,6 +91,7 @@ trait WeaponMigration
         if (!$id) {
             throw new \Exception("Could not found {$key} in {$table}");
         }
+
         return (int)$id;
     }
 }
