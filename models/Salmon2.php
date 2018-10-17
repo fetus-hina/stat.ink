d5cc147f4647cd5f1586811163d63f8fa5226b00
AIZAWA Hina <hina@bouhime.com>
2018-10-17 21:22:56 +0900

    fix style
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- models/Salmon2.php
+++ models/Salmon2.php
@@ -272,7 +272,7 @@ class Salmon2 extends ActiveRecord
         ];
 
         foreach ($data as $_) {
-            list ($minDanger, $quota) = $_;
+            list($minDanger, $quota) = $_;
             if ($minDanger <= $danger) {
                 return $quota;
             }
