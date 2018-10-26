00de90670558c905d418d7488c54eb0d962b40ba
AIZAWA Hina <hina@bouhime.com>
2018-10-26 16:17:58 +0900

    add index #363
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m181025_141312_index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+
+class m181025_141312_index extends Migration
+{
+    public function up()
+    {
+        $this->createIndex('ix_salmon_player2_work_id', 'salmon_player2', 'work_id');
+    }
+
+    public function down()
+    {
+        $this->dropIndex('ix_salmon_player2_work_id', 'salmon_player2');
+    }
+}
