fbeb3fc2c8b1516acd580406c82742a699bc6a93
AIZAWA Hina <hina@bouhime.com>
2018-10-31 20:42:05 +0900

    GET /api/v2/salmon/:id #363
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ actions/api/v2/salmon/ViewAction.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+declare(strict_types=1);
+
+namespace app\actions\api\v2\salmon;
+
+use Yii;
+use app\models\Salmon2;
+use yii\web\NotFoundHttpException;
+
+class ViewAction extends \yii\web\ViewAction
+{
+    public function init()
+    {
+        parent::init();
+
+        Yii::$app->language = 'en-US';
+        Yii::$app->timeZone = 'Etc/UTC';
+    }
+
+    public function run()
+    {
+        $resp = Yii::$app->getResponse();
+        $resp->format = 'compact-json';
+
+        $id = filter_var(Yii::$app->getRequest()->get('id'), FILTER_VALIDATE_INT);
+        if ($id === false) {
+            throw new NotFoundHttpException('Not found');
+        }
+
+        if (!$model = Salmon2::findOne(['id' => $id])) {
+            throw new NotFoundHttpException('Not found');
+        }
+
+        return $model->toJsonArray();
+    }
+}
