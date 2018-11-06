85b574d3ad10fd0aa92b92a1762b90d226db1f0d
ShingoMisawa <33051481+shngmsw@users.noreply.github.com>
2018-11-06 17:43:20 +0900

    Fix Jet Squelcher's special weapon
    fix #377, #378
    
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m181106_083644_fix_jetsweeper_special.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ * @author ShingoMisawa <33051481+shngmsw@users.noreply.github.com>
+ */
+
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use yii\db\Query;
+
+class m181106_083644_fix_jetsweeper_special extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'weapon2',
+            ['special_id' => $this->getSpecialId('missile')],
+            ['key' => 'jetsweeper']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'weapon2',
+            ['special_id' => $this->getSpecialId('jetpack')],
+            ['key' => 'jetsweeper']
+        );
+    }
+
+    private function getSpecialId(string $key): int
+    {
+        $query = (new Query())
+            ->select(['id'])
+            ->from('special2')
+            ->where(['key' => $key])
+            ->limit(1);
+
+        if (!$value = $query->scalar()) {
+            throw new \Exception("Unknown special: {$key}");
+        }
+
+        return (int)$value;
+    }
+}
