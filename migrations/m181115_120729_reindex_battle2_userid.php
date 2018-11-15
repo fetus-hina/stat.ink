c09050e3c22bf17a7aae7818ce62bf4eed1133a7
AIZAWA Hina <hina@bouhime.com>
2018-11-15 21:11:19 +0900

    Fix performance issue on profile page #384
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m181115_120729_reindex_battle2_userid.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+
+class m181115_120729_reindex_battle2_userid extends Migration
+{
+    public function up()
+    {
+        $this->createIndex('ix_battle2_userid_id', 'battle2', ['user_id', 'id'], true);
+        $this->execute('VACUUM ANALYZE {{battle2}}');
+    }
+
+    public function down()
+    {
+        $this->dropIndex('ix_battle2_userid_id');
+    }
+}
