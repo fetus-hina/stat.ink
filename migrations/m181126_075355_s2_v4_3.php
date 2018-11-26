87f668b49826928688deb638347afe8d1ba8efef
AIZAWA Hina <hina@bouhime.com>
2018-11-26 17:38:31 +0900

    Add Splatoon 2 version 4.3 data
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m181126_075355_s2_v4_3.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m181126_075355_s2_v4_3 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '4.3',
+            '4.3.x',
+            '4.3.0',
+            '4.3.0',
+            new DateTimeImmutable('2018-12-05T11:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('4.3.0', '4.2.0');
+    }
+}
