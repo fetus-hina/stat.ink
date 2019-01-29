d99e00dbb209e4136afd8b3320a95477b8dfd376
AIZAWA Hina <hina@bouhime.com>
2019-01-29 19:03:03 +0900

    Splatoon 2 v4.4.0
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m190129_100138_splatoon2_v4_4_0.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m190129_100138_splatoon2_v4_4_0 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '4.4',
+            '4.4.x',
+            '4.4.0',
+            '4.4.0',
+            new DateTimeImmutable('2019-01-30T11:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('4.4.0', '4.3.1');
+    }
+}
