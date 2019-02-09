2f7a0d5a2a5029a7dfd1265e5d7872fc2ca2529b
AIZAWA Hina <hina@bouhime.com>
2019-02-10 03:39:59 +0900

    Split Latin America to Central/South America
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m190209_183030_latin_america.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+
+class m190209_183030_latin_america extends Migration
+{
+    public function safeUp()
+    {
+        $this->delete('timezone_group', ['name' => 'Latin America']);
+        $this->batchInsert('timezone_group', ['order', 'name'], [
+            [60, 'Central America'],
+            [70, 'South America'],
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->delete('timezone_group', ['name' => [
+            'Central America',
+            'South America',
+        ]]);
+        $this->insert('timezone_group', [
+            'order' => 60,
+            'name' => 'Latin America',
+        ]);
+    }
+}
