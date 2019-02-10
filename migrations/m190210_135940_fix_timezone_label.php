a1f5a7cd76921978236b3be559addf046b0a4cf7
AIZAWA Hina <hina@bouhime.com>
2019-02-10 23:02:09 +0900

    Fix time zone label (#398)
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ migrations/m190210_135940_fix_timezone_label.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+
+class m190210_135940_fix_timezone_label extends Migration
+{
+    public function safeUp()
+    {
+        $this->update(
+            'timezone',
+            ['name' => 'Brazil (Western Amazonas)'],
+            ['identifier' => 'America/Eirunepe']
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->update(
+            'timezone',
+            ['name' => 'Brazil (Weastern Amazonas)'],
+            ['identifier' => 'America/Eirunepe']
+        );
+    }
+}
