74a1014d6d92fa17fffc3c98b511414ddc5be0a2
AIZAWA Hina <hina@bouhime.com>
2019-04-01 12:37:44 +0900

    Cleanup

--- migrations/m190401_032933_sailor_cap.php
+++ migrations/m190401_032933_sailor_cap.php
@@ -29,9 +29,6 @@ class m190401_032933_sailor_cap extends Migration
 
     public function getGears(): array
     {
-        // types: headgear, clothing, shoes
-        // brands: amiibo, annaki, cuttlegear, enperry, firefin, forge, grizzco, inkline, krak_on, rockenberg, skalop, splash_mob, squidforce, takoroka, tentatek, toni_kensa, zekko, zink
-        // abilities: ability_doubler, bomb_defense_up, bomb_defense_up_dx, cold_blooded, comeback, drop_roller, haunt, ink_recovery_up, ink_resistance_up, ink_saver_main, ink_saver_sub, last_ditch_effort, main_power_up, ninja_squid, object_shredder, opening_gambit, quick_respawn, quick_super_jump, respawn_punisher, run_speed_up, special_charge_up, special_power_up, special_saver, stealth_jump, sub_power_up, swim_speed_up, tenacity, thermal_ink
         return [
             [
                 static::name2key('Sailor Cap'),
