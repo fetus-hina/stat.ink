b10fd8388c979f9a9c244e04909f5b3854a6379b
AIZAWA Hina <hina@bouhime.com>
2019-04-12 11:39:24 +0900

    Splatoon 2 v4.6.1

---
+++ migrations/m190412_023707_s2_4_6_1.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m190412_023707_s2_4_6_1 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '4.6',
+            '4.6.x',
+            '4.6.1',
+            '4.6.1',
+            new DateTimeImmutable('2019-04-12T11:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('4.6.1', '4.6.0');
+    }
+}
