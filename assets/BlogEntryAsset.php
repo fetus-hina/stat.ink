d98b8c217894810dbd663cbfc7c49b4d34435d15
AIZAWA Hina <hina@bouhime.com>
2019-04-18 16:29:27 +0900

    Split view to widget (Blog entries on index page)

---
+++ assets/BlogEntryAsset.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+declare(strict_types=1);
+
+namespace app\assets;
+
+use Yii;
+use yii\web\AssetBundle;
+use yii\bootstrap\BootstrapAsset;
+
+class BlogEntryAsset extends AssetBundle
+{
+    public $sourcePath = '@app/resources/.compiled/stat.ink';
+    public $css = [
+        'blog-entries.css',
+    ];
+    public $depends = [
+        AppAsset::class,
+        BootstrapAsset::class,
+    ];
+}
