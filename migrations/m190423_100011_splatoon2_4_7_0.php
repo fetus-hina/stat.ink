e893a45b24737e6a61ce8caf9b3de2e30b3f743f
AIZAWA Hina <hina@bouhime.com>
2019-04-23 19:01:59 +0900

    Splatoon 2 v4.7.0
    https://twitter.com/SplatoonJP/status/1120616339012341760
    https://support.nintendo.co.jp/app/answers/detail/a_id/34680

---
+++ migrations/m190423_100011_splatoon2_4_7_0.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m190423_100011_splatoon2_4_7_0 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '4.7',
+            '4.7.x',
+            '4.7.0',
+            '4.7.0',
+            new DateTimeImmutable('2019-04-24T11:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('4.7.0', '4.6.1');
+    }
+}
