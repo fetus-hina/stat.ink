d74e44e7750d3d1c5b2d79d404b52c28fe54f78f
AIZAWA Hina <hina@bouhime.com>
2019-05-04 02:33:21 +0900

    Update User Language Detector

--- components/web/Controller.php
+++ components/web/Controller.php
@@ -8,7 +8,7 @@
 namespace app\components\web;
 
 use Yii;
-use app\models\Language;
+use app\components\helpers\UserLanguage;
 use app\models\Timezone;
 use yii\base\Event;
 use yii\base\View;
@@ -28,117 +28,15 @@ class Controller extends Base
 
     private function setLanguage(): void
     {
-        $lang = (function (): ?Language {
-            if ($val = $this->detectLanguageByParam()) {
-                return $val;
-            }
-
-            if ($val = $this->detectLanguageByCookie()) {
-                return $val;
-            }
-
-            if ($val = $this->detectLanguageByHeader()) {
-                return $val;
-            }
-
-            return null;
-        })();
-
+        $lang = UserLanguage::guess();
         if ($lang) {
             Yii::$app->language = $lang->getLanguageId();
             Yii::$app->locale = $lang->lang;
-        }
-    }
-
-    private function detectLanguageByParam(): ?Language
-    {
-        $param = Yii::$app->request->get('_lang_');
-        if (!is_scalar($param)) {
-            return null;
-        }
-
-        if (!$lang = Language::findOne(['lang' => (string)$param])) {
-            return null;
-        }
-
-        if ($this->detectLanguageByCookie() === false) {
-            Yii::$app->response->cookies->add(
-                new Cookie([
-                    'name' => 'language',
-                    'value' => $lang->lang,
-                    'expire' => time() + 86400 * 366,
-                ])
-            );
-        }
-
-        return $lang;
-    }
-
-    private function detectLanguageByCookie(): ?Language
-    {
-        $cookie = Yii::$app->request->cookies->get('language');
-        if (!$cookie) {
-            return null;
-        }
-
-        if (!$lang = Language::findOne(['lang' => $cookie->value])) {
-            return null;
-        }
-
-        return $lang;
-    }
-
-    private function detectLanguageByHeader(): ?Language
-    {
-        $request = Yii::$app->request;
-        $userAgent   = trim($request->userAgent);
-        $acceptLangs = $request->acceptableLanguages;
-        if (empty($acceptLangs) ||
-                $userAgent == '' ||
-                stripos($userAgent, 'bot') !== false ||
-                stripos($userAgent, 'spider') !== false ||
-                stripos($userAgent, 'google') !== false ||
-                stripos($userAgent, 'http://') !== false ||
-                stripos($userAgent, 'https://') !== false
-        ) {
-            return null;
-        }
-
-        //FIXME
-        $firstLang = strtolower(array_shift($acceptLangs));
-        $firstLangShort = substr($firstLang, 0, 2);
-        switch ($firstLangShort) {
-            case 'ja':
-                return Language::findOne(['lang' => 'ja-JP']);
-
-            case 'en':
-                return Language::findOne([
-                    'lang' => ($firstLang === 'en-gb' || $firstLang === 'en-au')
-                        ? 'en-GB'
-                        : 'en-US',
-                ]);
-
-            case 'es':
-                return Language::findOne([
-                    'lang' => $firstLang === 'es-es' ? 'es-ES' : 'es-MX',
-                ]);
-
-            case 'fr':
-                return Language::findOne([
-                    'lang' => $firstLang === 'fr-ca' ? 'fr-CA' : 'fr-FR',
-                ]);
-
-            case 'it':
-                return Language::findOne(['lang' => 'it-IT']);
-
-            case 'ru':
-                return Language::findOne(['lang' => 'ru-RU']);
-
-            case 'nl':
-                return Language::findOne(['lang' => 'nl-NL']);
-
-            default:
-                return null;
+            Yii::$app->response->cookies->add(new Cookie([
+                'name' => UserLanguage::COOKIE_KEY,
+                'value' => $lang->lang,
+                'expire' => time() + 86400 * 366,
+            ]));
         }
     }
 
