727cd14554b47e363fe94f062d19103b0dbd0adf
AIZAWA Hina <hina@bouhime.com>
2019-05-04 05:20:42 +0900

    Add new time zone detector with GeoIP

--- components/helpers/UserTimeZone.php
+++ components/helpers/UserTimeZone.php
@@ -9,7 +9,10 @@ declare(strict_types=1);
 
 namespace app\components\helpers;
 
+use DateTimeImmutable;
+use DateTimeZone;
 use Yii;
+use app\components\GeoIP;
 use app\models\Timezone;
 use yii\helpers\StringHelper;
 
@@ -23,6 +26,7 @@ class UserTimeZone
             Yii::beginProfile(__FUNCTION__, __METHOD__);
             $methods = [
                 [static::class, 'guessByCookie'],
+                [static::class, 'guessByGeoIP'],
                 [static::class, 'guessByAppLanguage'],
             ];
 
@@ -108,5 +112,158 @@ class UserTimeZone
         } finally {
             Yii::endProfile(__FUNCTION__, __METHOD__);
         }
+    }
+
+    public static function guessByGeoIP(): ?Timezone
+    {
+        try {
+            Yii::beginProfile(__FUNCTION__, __METHOD__);
+            $ipAddr = Yii::$app->request->getUserIP();
+            if (!$ipAddr) {
+                return null;
+            }
+
+            if (!$identifier = static::consultGeoIPDB($ipAddr)) {
+                return null;
+            }
+
+            $tz = Timezone::findOne(['identifier' => $identifier]);
+            if ($tz) {
+                Yii::info(
+                    "Detected timezone by geoip, " . $tz->identifier,
+                    __METHOD__
+                );
+                return $tz;
+            }
+
+            Yii::info(
+                'There was no exact time zone match: ' . $identifier,
+                __METHOD__
+            );
+            $tz = static::guessTimezoneByIdentifier($identifier);
+            if ($tz) {
+                Yii::info(
+                    sprintf(
+                        'Detected timezone by geoip %s, guessed our timezone %s',
+                        $identifier,
+                        $tz->identifier
+                    ),
+                    __METHOD__
+                );
+            }
+
+            return $tz;
+        } finally {
+            Yii::endProfile(__FUNCTION__, __METHOD__);
+        }
+    }
+
+    private static function consultGeoIPDB(string $ipAddr): ?string
+    {
+        try {
+            Yii::beginProfile(__FUNCTION__, __METHOD__);
+
+            if (!$city = Yii::$app->geoip->city($ipAddr)) {
+                Yii::warning('Could not determinate user\'s city from IP ' . $ipAddr, __METHOD__);
+                return null;
+            }
+
+            Yii::info(
+                sprintf(
+                    'GeoIP: country=%s, city=%s, geo=(%f, %f), timezone=%s',
+                    $city->country->name ?? '?',
+                    $city->city->name ?? '?',
+                    $city->location->latitude ?? 0,
+                    $city->location->longitude ?? 0,
+                    $city->location->timeZone ?? '?',
+                ),
+                __METHOD__
+            );
+
+            if (!$location = $city->location) {
+                Yii::warning('Could not determinate user\'s city from IP ' . $ipAddr, __METHOD__);
+                return null;
+            }
+
+            $tz = trim((string)$location->timeZone);
+            if ($tz === '') {
+                Yii::warning(
+                    'GeoIP\'s record has not a time zone information, IP ' . $ipAddr,
+                    __METHOD__
+                );
+                return null;
+            }
+
+            return $tz;
+        } catch (\Exception $e) {
+            Yii::warning(
+                'Catch an exception: ' . $e->getMessage(),
+                __METHOD__
+            );
+            return null;
+        } finally {
+            Yii::endProfile(__FUNCTION__, __METHOD__);
+        }
+    }
+
+    private static function guessTimezoneByIdentifier(string $identifier): ?Timezone
+    {
+        try {
+            Yii::beginProfile(__FUNCTION__, __METHOD__);
+            try {
+                $tz = new DateTimeZone($identifier);
+            } catch (\Exception $e) {
+                return null;
+            }
+            if ($guessed = static::guessTimezoneByTimezone($tz)) {
+                return $guessed;
+            }
+
+            return static::createUTCOffsetTimezone($tz);
+        } finally {
+            Yii::endProfile(__FUNCTION__, __METHOD__);
+        }
+    }
+
+    private static function guessTimezoneByTimezone(DateTimeZone $tz1): ?Timezone
+    {
+        try {
+            Yii::beginProfile(__FUNCTION__, __METHOD__);
+            $t1 = new DateTimeImmutable('2019-01-01T00:00:00+00:00');
+            $t2 = new DateTimeImmutable('2019-07-01T00:00:00+00:00');
+            $timezones = Timezone::find()
+                ->andWhere(['not like', 'timezone.identifier', 'Etc/%', false])
+                ->orderBy(['timezone.order' => SORT_ASC])
+                ->all();
+            foreach ($timezones as $timezone) {
+                $tz2 = new DateTimeZone($timezone->identifier);
+
+                $val1 = $t1->setTimezone($tz1)->format('TP');
+                $val2 = $t1->setTimezone($tz2)->format('TP');
+                if ($val1 !== $val2) {
+                    continue;
+                }
+
+                $val1 = $t2->setTimezone($tz1)->format('TP');
+                $val2 = $t2->setTimezone($tz2)->format('TP');
+                if ($val1 !== $val2) {
+                    continue;
+                }
+
+                return $timezone;
+            }
+
+            return null;
+        } finally {
+            Yii::endProfile(__FUNCTION__, __METHOD__);
+        }
+    }
+
+    private static function createUTCOffsetTimezone(DateTimeZone $tz): ?Timezone
+    {
+        $offsetSec = (new DateTimeImmutable('now', $tz))->format('Z');
+        $offsetHours = (int)floor($offsetSec / 3600);
+        $tzName = sprintf('Etc/GMT%+d', -$offsetHours);
+        return Timezone::findOne(['identifier' => $tzName]);
     }
 }
