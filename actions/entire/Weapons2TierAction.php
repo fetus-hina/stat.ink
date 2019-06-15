66bf141fa73d146af45c5ccf92d2c0da0e6744ad
AIZAWA Hina <hina@bouhime.com>
2019-06-16 07:33:40 +0900

    weapons2-tier: Link to other rules

---
+++ actions/entire/Weapons2TierAction.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+declare(strict_types=1);
+
+namespace app\actions\entire;
+
+use Yii;
+use app\models\Rule2;
+use app\models\SplatoonVersionGroup2;
+use app\models\StatWeapon2Tier;
+use yii\db\Query;
+use yii\web\ViewAction;
+
+class Weapons2TierAction extends ViewAction
+{
+    public $input;
+
+    public function init()
+    {
+        parent::init();
+
+        $request = Yii::$app->request;
+        $this->input = [
+            'version' => (string)$request->get('version'),
+            'month' => (string)$request->get('month'),
+            'rule' => (string)$request->get('rule'),
+        ];
+    }
+
+    public function run()
+    {
+        // redirect to default page if input is empty
+        if (!$this->input['version'] || !$this->input['month'] || !$this->input['rule']) {
+            $latest = StatWeapon2Tier::find()
+                ->orderBy(['id' => SORT_DESC])
+                ->limit(1)
+                ->one();
+            if (!$latest) {
+                $this->controller->error404();
+                return;
+            }
+
+            $this->controller->redirect(['entire/weapons2-tier',
+                'version' => $latest->versionGroup->tag,
+                'month' => substr($latest->month, 0, 7), // "YYYY-MM"-DD
+                'rule' => 'area',
+            ]);
+            return;
+        }
+
+        if (!preg_match('/^\d{4}-\d{2}$/', $this->input['month'])) {
+            $this->controller->error404();
+            return;
+        }
+
+        $rule = Rule2::find()
+            ->andWhere(['key' => (string)$this->input['rule']])
+            ->andWhere(['<>', 'key', 'nawabari'])
+            ->limit(1)
+            ->one();
+        if (!$rule) {
+            $this->controller->error404();
+            return;
+        }
+
+        $vGroup = SplatoonVersionGroup2::find()
+            ->andWhere(['tag' => (string)$this->input['version']])
+            ->limit(1)
+            ->one();
+        if (!$vGroup) {
+            $this->controller->error404();
+            return;
+        }
+
+        $data = StatWeapon2Tier::find()
+            ->andWhere([
+                'version_group_id' => $vGroup->id,
+                'month' => $this->input['month'] . '-01',
+                'rule_id' => $rule->id,
+            ])
+            ->orderBy(['id' => SORT_ASC])
+            ->with(['weapon'])
+            ->all();
+        if (!$data) {
+            $this->controller->error404();
+            return;
+        }
+
+        return $this->controller->render('weapons2-tier', [
+            'data' => $data,
+            'versionGroup' => $vGroup,
+            'month' => $this->input['month'],
+            'rule' => $rule,
+            'rules' => $this->getRules($vGroup),
+        ]);
+    }
+
+    private function getRules(SplatoonVersionGroup2 $version): array
+    {
+        return Rule2::getSortedAll('gachi', null, function (Rule2 $rule) use ($version): array {
+            return [
+                'name' => $rule->name,
+                'enabled' => StatWeapon2Tier::find()
+                    ->andWhere([
+                        'version_group_id' => $version->id,
+                        'month' => $this->input['month'] . '-01',
+                        'rule_id' => $rule->id,
+                    ])
+                    ->limit(1)
+                    ->exists(),
+            ];
+        });
+    }
+}
--- views/entire/weapons2-tier.php
+++ views/entire/weapons2-tier.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 use app\assets\Spl2WeaponAsset;
 use app\components\widgets\AdWidget;
 use app\components\widgets\SnsWidget;
+use yii\bootstrap\Nav;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\helpers\Json;
@@ -40,119 +41,189 @@ $weaponIcons = Spl2WeaponAsset::register($this);
   <?= AdWidget::widget() . "\n" ?>
   <?= SnsWidget::widget() . "\n" ?>
 
-  <p class="m-0">野良ガチマのランクXのみ集計。投稿者は除外済</p>
-  <table class="table">
-    <thead>
-      <tr>
-        <th style="width:calc(3em + 16px)"></th>
-        <th>Win %</th>
-        <th style="width:calc(8em + 16px)">K</th>
-        <th style="width:calc(8em + 16px)">D</th>
-        <th style="width:calc(4em + 16px)">KR</th>
-        <th style="width:calc(2em + 16px)">n</th>
-    </thead>
-    <tbody>
+  <nav class="mb-3"><?= Nav::widget([
+    'options' => ['class' => 'nav-tabs'],
+    'items' => array_map(
+        function (string $key, array $data) use ($versionGroup, $month, $rule): array {
+            return [
+                'label' => Yii::t('app-rule2', $data['name']),
+                'url' => ['entire/weapons2-tier',
+                    'version' => $versionGroup->tag,
+                    'month' => $month,
+                    'rule' => $key,
+                ],
+                'active' => $key === $rule->key,
+                'options' => [
+                    'class' => array_filter([
+                        $data['enabled'] ? null : 'disabled',
+                    ]),
+                ],
+            ];
+        },
+        array_keys($rules),
+        array_values($rules),
+    ),
+  ]) ?></nav>
+
+  <ul class="m-0">
+    <li>
+      Targets:
+      <ul>
+        <li>Ranked battles (not including League battles)</li>
+        <li>Rank X(v3.0-)/S+(-v2.x) only</li>
+        <li>Excluded the uploader (stat.ink's user)</li>
+      </ul>
+    </li>
+    <li>
+      Kills and deaths:
+      <ul>
+        <li>Normalized to 5 minutes (even KO or overtimed)</li>
+      </ul>
+    </li>
+    <li>
+      ±:
+      <ul>
+        <li>
+          Perhaps "the true value" is somewhere in this range.
+          Don't too believe the representative (average) value.
+        </li>
+      </ul>
+    </li>
+  </ul>
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th style="width:calc(3em + 16px)"></th>
+          <th style="min-width:300px"><?= Html::encode(Yii::t('app', 'Win %')) ?></th>
+          <th style="width:calc(8em + 16px)"><?= Html::encode(Yii::t('app', 'Kills')) ?></th>
+          <th style="width:calc(8em + 16px)"><?= Html::encode(Yii::t('app', 'Deaths')) ?></th>
+          <th style="width:calc(4em + 16px)"><?= Html::encode(Yii::t('app', 'Ratio')) ?></th>
+          <th style="width:calc(2em + 16px)">n</th>
+      </thead>
+      <tbody>
 <?php foreach ($data as $model) { ?>
-      <tr>
-        <td><?= Html::img($weaponIcons->getIconUrl($model->weapon->key), [
-          'title' => Yii::t('app-weapon2', $model->weapon->name),
-          'class' => 'auto-tooltip',
-          'style' => [
-            'width' => '3em',
-            'height' => 'auto',
-          ],
-        ]) ?></td>
-        <td><?php
-          if ($_rate = $model->getWinRates()) {
-            if ($_rate[0] === null) {
-              echo Html::tag(
-                'div',
-                implode('', [
-                  Html::tag(
-                    'div',
-                    Yii::$app->formatter->asPercent($_rate[1], 2),
-                    [
-                      'class' => 'progress-bar progress-bar-primary',
-                      'style' => [
-                        'width' => sprintf('%f%%', $_rate[1] * 100),
-                      ],
-                    ]
-                  ),
-                ]),
-                ['class' => 'progress']
-              );
-            } else {
-              echo Html::tag(
-                'div',
-                implode('', [
-                  Html::tag(
-                    'div',
+        <tr>
+          <td><?= Html::img($weaponIcons->getIconUrl($model->weapon->key), [
+            'title' => Yii::t('app-weapon2', $model->weapon->name),
+            'class' => 'auto-tooltip',
+            'style' => [
+              'width' => '3em',
+              'height' => 'auto',
+            ],
+          ]) ?></td>
+          <td><?php
+            if ($_rate = $model->getWinRates()) {
+              if ($_rate[0] === null) {
+                echo Html::tag(
+                  'div',
+                  implode('', [
                     Html::tag(
-                      'span',
-                      vsprintf('%s±%s%%', [
-                        Yii::$app->formatter->asDecimal($_rate[1] * 100, 2),
-                        Yii::$app->formatter->asDecimal(($_rate[2] - $_rate[0]) * 100 / 2, 2),
-                      ]),
-                      ['class' => 'pl-2']
+                      'div',
+                      Yii::$app->formatter->asPercent($_rate[1], 2),
+                      [
+                        'class' => 'progress-bar progress-bar-primary',
+                        'style' => [
+                          'width' => sprintf('%f%%', $_rate[1] * 100),
+                        ],
+                      ]
                     ),
-                    [
-                      'class' => 'progress-bar progress-bar-primary text-left-important',
-                      'style' => [
-                        'width' => sprintf('%f%%', $_rate[0] * 100),
-                      ],
-                    ]
-                  ),
-                  Html::tag(
-                    'div',
-                    '',
-                    [
-                      'class' => 'progress-bar progress-bar-primary',
-                      'style' => [
-                        'width' => sprintf('%f%%', ($_rate[1] - $_rate[0]) * 100),
-                        'opacity' => '0.65',
-                      ],
-                    ]
-                  ),
-                  Html::tag(
-                    'div',
-                    '',
-                    [
-                      'class' => 'progress-bar progress-bar-primary',
-                      'style' => [
-                        'width' => sprintf('%f%%', ($_rate[2] - $_rate[1]) * 100),
-                        'opacity' => '0.3',
-                      ],
-                    ]
-                  ),
-                ]),
-                ['class' => 'progress']
-              );
+                  ]),
+                  ['class' => 'progress']
+                );
+              } else {
+                echo Html::tag(
+                  'div',
+                  implode('', [
+                    Html::tag(
+                      'div',
+                      Html::tag(
+                        'span',
+                        vsprintf('%s±%s%%', [
+                          Yii::$app->formatter->asDecimal($_rate[1] * 100, 2),
+                          Yii::$app->formatter->asDecimal(($_rate[2] - $_rate[0]) * 100 / 2, 2),
+                        ]),
+                        ['class' => 'pl-2']
+                      ),
+                      [
+                        'class' => 'progress-bar progress-bar-primary text-left-important',
+                        'style' => [
+                          'width' => sprintf('%f%%', $_rate[0] * 100),
+                        ],
+                      ]
+                    ),
+                    Html::tag(
+                      'div',
+                      '',
+                      [
+                        'class' => 'progress-bar progress-bar-primary',
+                        'style' => [
+                          'width' => sprintf('%f%%', ($_rate[1] - $_rate[0]) * 100),
+                          'opacity' => '0.65',
+                        ],
+                      ]
+                    ),
+                    Html::tag(
+                      'div',
+                      '',
+                      [
+                        'class' => 'progress-bar progress-bar-primary',
+                        'style' => [
+                          'width' => sprintf('%f%%', ($_rate[2] - $_rate[1]) * 100),
+                          'opacity' => '0.3',
+                        ],
+                      ]
+                    ),
+                  ]),
+                  ['class' => 'progress']
+                );
+              }
             }
-          }
-        ?></td>
-        <td>
-          <?= vsprintf('%s±%s', [
-            Yii::$app->formatter->asDecimal($model->avg_kill, 2),
-            Yii::$app->formatter->asDecimal($model->stderr_kill * 2, 2),
-          ]) ?><br>
-          median=<?= Yii::$app->formatter->asDecimal($model->med_kill, 2) ?>
-        </td>
-        <td>
-          <?= vsprintf('%s±%s', [
-            Yii::$app->formatter->asDecimal($model->avg_death, 2),
-            Yii::$app->formatter->asDecimal($model->stderr_death * 2, 2),
-          ]) ?><br>
-          median=<?= Yii::$app->formatter->asDecimal($model->med_death, 2) ?>
-        </td>
-        <td>
-          <?= $model->avg_death > 0
-            ? Yii::$app->formatter->asDecimal($model->avg_kill / $model->avg_death, 3)
-            : '-'
-          ?>
-        </td>
-        <td><?= Yii::$app->formatter->asInteger($model->players_count) ?></td>
-      </tr>
+          ?></td>
+          <td>
+            <?= vsprintf('%s=%s±%s', [
+              Html::tag('span', Html::encode('μ'), [
+                'title' => Yii::t('app', 'Average'),
+                'class' => 'auto-tooltip',
+              ]),
+              Yii::$app->formatter->asDecimal($model->avg_kill, 2),
+              Yii::$app->formatter->asDecimal($model->stderr_kill * 2, 2),
+            ]) ?><br>
+            <?= vsprintf('%s=%s', [
+              Html::tag('span', Html::encode('Med'), [
+                'title' => Yii::t('app', 'Median'),
+                'class' => 'auto-tooltip',
+              ]),
+              Yii::$app->formatter->asDecimal($model->med_kill, 1),
+            ]) . "\n" ?>
+          </td>
+          <td>
+            <?= vsprintf('%s=%s±%s', [
+              Html::tag('span', Html::encode('μ'), [
+                'title' => Yii::t('app', 'Average'),
+                'class' => 'auto-tooltip',
+              ]),
+              Yii::$app->formatter->asDecimal($model->avg_death, 2),
+              Yii::$app->formatter->asDecimal($model->stderr_death * 2, 2),
+            ]) ?><br>
+            <?= vsprintf('%s=%s', [
+              Html::tag('span', Html::encode('Med'), [
+                'title' => Yii::t('app', 'Median'),
+                'class' => 'auto-tooltip',
+              ]),
+              Yii::$app->formatter->asDecimal($model->med_death, 1),
+            ]) . "\n" ?>
+          </td>
+          <td>
+            <?= $model->avg_death > 0
+              ? Yii::$app->formatter->asDecimal($model->avg_kill / $model->avg_death, 3)
+              : '-'
+            ?>
+          </td>
+          <td><?= Yii::$app->formatter->asInteger($model->players_count) ?></td>
+        </tr>
 <?php } ?>
-    </tbody>
-  </table>
+      </tbody>
+    </table>
+  </div>
 </div>
