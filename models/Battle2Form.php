099db5671a1e64ed90af407d301a133ebb2a199a
AIZAWA Hina <hina@fetus.jp>
2019-07-19 18:49:15 +0900

    Fix updating battle mode

--- models/Battle2Form.php
+++ models/Battle2Form.php
@@ -219,23 +219,25 @@ class Battle2Form extends Model
         return null;
     }
 
-    public function setXMode(string $str) : void
+    public function setXMode(string $str): void
     {
         $this->lobby_id = null;
         $this->mode_id = null;
         $this->rule_id = null;
-        $s = preg_match(
-            '/^(standard|squad_[24]|private)-(regular|gachi|fest|private)-(nawabari|area|yagura|hoko|asari)$/',
-            $str,
-            $match
-        );
-        if ($s) {
+
+        $lobbies = '(standard|fest_normal|squad_[24]|private)';
+        $modes = '(regular|gachi|fest|private)';
+        $rules = '(nawabari|area|yagura|hoko|asari)';
+
+        if (preg_match("/^{$lobbies}-{$modes}-{$rules}\$/", $str, $match)) {
             if ($_ = Lobby2::findOne(['key' => $match[1]])) {
                 $this->lobby_id = (int)$_->id;
             }
+
             if ($_ = Mode2::findOne(['key' => $match[2]])) {
                 $this->mode_id = (int)$_->id;
             }
+
             if ($_ = Rule2::findOne(['key' => $match[3]])) {
                 $this->rule_id = (int)$_->id;
             }
