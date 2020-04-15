3bad0fe8b5c1a189b6cdefc0af2b68281fd180ec
AIZAWA Hina <hina@fetus.jp>
2020-04-15 18:16:45 +0900

    Splatoon 2 5.2.0

---
+++ migrations/m200415_091509_s2_5_2_0.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2020 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+class m200415_091509_s2_5_2_0 extends Migration
+{
+    use VersionMigration;
+
+    public function safeUp()
+    {
+        $this->upVersion2(
+            '5.2',
+            '5.2.x',
+            '5.2.0',
+            '5.2.0',
+            new DateTimeImmutable('2020-04-22T11:00:00+09:00')
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->downVersion2('5.2.0', '5.1.0');
+    }
+}
