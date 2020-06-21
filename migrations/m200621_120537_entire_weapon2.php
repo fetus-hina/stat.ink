27fcf36f52cd340e6cbfa27298c0dd46e0c0bb1a
AIZAWA Hina <hina@fetus.jp>
2020-06-21 21:58:38 +0900

    Add table definitions for rewrite entire/weapons2

---
+++ migrations/m200621_120537_entire_weapon2.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2020 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+use app\components\db\Migration;
+
+class m200621_120537_entire_weapon2 extends Migration
+{
+    public function safeUp()
+    {
+        $tables = $this->tables;
+        array_walk($tables, fn ($def, $name) => $this->createTable($name, $def));
+
+        if (true) {
+            echo "\n";
+            foreach (array_keys($tables) as $name) {
+                vprintf("./yii gii/model --tableName=%s --modelClass=%s --interactive=0 --overwrite\n", [
+                    escapeshellarg($name),
+                    escapeshellarg(implode('', array_map('ucfirst', explode('_', $name)))),
+                ]);
+            }
+        }
+    }
+
+    public function safeDown()
+    {
+        $this->dropTables(array_keys($this->tables));
+    }
+
+    public function getTables(): array
+    {
+        $tables = [];
+
+        foreach ($this->versionMap as $vTableSuffix => $vRefs) {
+            foreach ($this->weaponMap as $wTableSuffix => $wRefs) {
+                $tableName = ($vTableSuffix === '')
+                    ? sprintf('stat_weapon2_entire_%s', $wTableSuffix)
+                    : sprintf('stat_weapon2_entire_%s_by_%s', $wTableSuffix, $vTableSuffix);
+                $def = array_merge(
+                    ['rule_id' => $this->pkRef('rule2')->notNull()],
+                    $vRefs
+                        ? array_map(fn ($_) => $this->pkRef($_[0], $_[1])->notNull(), $vRefs)
+                        : [],
+                    $wRefs
+                        ? array_map(fn ($_) => $this->pkRef($_[0], $_[1])->notNull(), $wRefs)
+                        : [],
+                    [
+                        'battles' => $this->bigInteger()->notNull(),
+                        'wins' => $this->bigInteger()->notNull(),
+                        'avg_kill' => $this->double()->notNull(),
+                        'med_kill' => $this->double()->notNull(),
+                        'stddev_kill' => $this->double()->null(),
+                        'avg_death' => $this->double()->notNull(),
+                        'med_death' => $this->double()->notNull(),
+                        'stddev_death' => $this->double()->null(),
+                        'avg_special' => $this->double()->notNull(),
+                        'med_special' => $this->double()->notNull(),
+                        'stddev_special' => $this->double()->null(),
+                        'avg_point' => $this->double()->notNull(),
+                        'med_point' => $this->double()->notNull(),
+                        'stddev_point' => $this->double()->null(),
+                        'avg_time' => $this->double()->notNull(),
+                        'updated_at' => $this->timestampTZ()->notNull(),
+                    ]
+                );
+                $tables[$tableName] = $def;
+            }
+        }
+
+        return $tables;
+    }
+
+    public function getVersionMap(): array
+    {
+        return [
+            '' => null,
+            'version' => [
+                'version_id' => ['splatoon_version2', 'id'],
+            ],
+            'vgroup' => [
+                'version_group_id' => ['splatoon_version_group2', 'id'],
+            ],
+        ];
+    }
+
+    public function getWeaponMap(): array
+    {
+        return [
+            'main' => [
+                'weapon_id' => ['weapon2', 'id'],
+            ],
+            'sub' => [
+                'subweapon_id' => ['subweapon2', 'id'],
+            ],
+            'special' => [
+                'special_id' => ['special2', 'id'],
+            ],
+        ];
+    }
+}
---
+++ models/StatWeapon2EntireMain.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2020 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "stat_weapon2_entire_main".
+ *
+ * @property integer $rule_id
+ * @property integer $weapon_id
+ * @property integer $battles
+ * @property integer $wins
+ * @property double $avg_kill
+ * @property double $med_kill
+ * @property double $stddev_kill
+ * @property double $avg_death
+ * @property double $med_death
+ * @property double $stddev_death
+ * @property double $avg_special
+ * @property double $med_special
+ * @property double $stddev_special
+ * @property double $avg_point
+ * @property double $med_point
+ * @property double $stddev_point
+ * @property double $avg_time
+ * @property string $updated_at
+ *
+ * @property Rule2 $rule
+ * @property Weapon2 $weapon
+ */
+class StatWeapon2EntireMain extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'stat_weapon2_entire_main';
+    }
+
+    public function rules()
+    {
+        return [
+            [['rule_id', 'weapon_id', 'battles', 'wins', 'avg_kill', 'med_kill', 'avg_death', 'med_death', 'avg_special', 'med_special', 'avg_point', 'med_point', 'avg_time', 'updated_at'], 'required'],
+            [['rule_id', 'weapon_id', 'battles', 'wins'], 'default', 'value' => null],
+            [['rule_id', 'weapon_id', 'battles', 'wins'], 'integer'],
+            [['avg_kill', 'med_kill', 'stddev_kill', 'avg_death', 'med_death', 'stddev_death', 'avg_special', 'med_special', 'stddev_special', 'avg_point', 'med_point', 'stddev_point', 'avg_time'], 'number'],
+            [['updated_at'], 'safe'],
+            [['rule_id'], 'exist', 'skipOnError' => true, 'targetClass' => Rule2::class, 'targetAttribute' => ['rule_id' => 'id']],
+            [['weapon_id'], 'exist', 'skipOnError' => true, 'targetClass' => Weapon2::class, 'targetAttribute' => ['weapon_id' => 'id']],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'rule_id' => 'Rule ID',
+            'weapon_id' => 'Weapon ID',
+            'battles' => 'Battles',
+            'wins' => 'Wins',
+            'avg_kill' => 'Avg Kill',
+            'med_kill' => 'Med Kill',
+            'stddev_kill' => 'Stddev Kill',
+            'avg_death' => 'Avg Death',
+            'med_death' => 'Med Death',
+            'stddev_death' => 'Stddev Death',
+            'avg_special' => 'Avg Special',
+            'med_special' => 'Med Special',
+            'stddev_special' => 'Stddev Special',
+            'avg_point' => 'Avg Point',
+            'med_point' => 'Med Point',
+            'stddev_point' => 'Stddev Point',
+            'avg_time' => 'Avg Time',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    public function getRule(): ActiveQuery
+    {
+        return $this->hasOne(Rule2::class, ['id' => 'rule_id']);
+    }
+
+    public function getWeapon(): ActiveQuery
+    {
+        return $this->hasOne(Weapon2::class, ['id' => 'weapon_id']);
+    }
+}
