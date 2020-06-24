d1f235809b4e00dc047f9bcdca754683930eecf3
AIZAWA Hina <hina@fetus.jp>
2020-06-24 12:05:17 +0900

    Update copyright comment

--- components/gii/generators/model/Generator.php
+++ components/gii/generators/model/Generator.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @copyright Copyright (C) 2008 Yii Software LLC
+ * @copyright Copyright (C) 2015-2020 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
 declare(strict_types=1);
 
 namespace app\components\gii\generators\model;
