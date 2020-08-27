18764f5c71f87df8e07573eab0e1c245e0249d4e
AIZAWA Hina <hina@fetus.jp>
2020-08-28 05:50:19 +0900

    Add machine-translation enable/disable button to alert box

--- components/widgets/alerts/LanguageSupportLevelWarning.php
+++ components/widgets/alerts/LanguageSupportLevelWarning.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
  */
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace app\components\widgets\alerts;
 
 use Yii;
+use app\assets\LanguageDialogAsset;
 use app\components\widgets\Alert;
 use app\components\widgets\FA;
 use app\components\widgets\FlagIcon;
@@ -63,7 +64,7 @@ class LanguageSupportLevelWarning extends Widget
                 ),
                 Html::tag(
                     'p',
-                    Html::encode('Only proper nouns translated. (e.g. weapons, stages)')
+                    Html::encode('Only proper nouns (e.g., weapons, stages) translated.')
                 ),
                 Html::tag(
                     'p',
@@ -73,6 +74,7 @@ class LanguageSupportLevelWarning extends Widget
                         ['class' => 'alert-link']
                     )
                 ),
+                $this->renderMachineTranslate(),
             ]),
         ]);
     }
@@ -106,5 +108,46 @@ class LanguageSupportLevelWarning extends Widget
                 ]
             ),
         ]));
+    }
+
+    protected function renderMachineTranslate(): string
+    {
+        if (!$this->language) {
+            return '';
+        }
+
+        if (Yii::$app->isEnabledMachineTranslation) {
+            return Html::tag('p', implode(' ', [
+                Html::encode('Currently displayed using machine translation (Powered by DeepL).'),
+                $this->renderMachineTranslateSwitch(false),
+            ]));
+        } else {
+            return Html::tag('p', implode(' ', [
+                Html::encode('The machine translation option is currently disabled.'),
+                $this->renderMachineTranslateSwitch(true),
+            ]));
+        }
+    }
+
+    protected function renderMachineTranslateSwitch(bool $toEnable): string
+    {
+        LanguageDialogAsset::register($this->view);
+
+        return Html::tag(
+            'a',
+            Html::encode($toEnable ? 'Enable machine-translation' : 'Disable machine-translation'),
+            [
+                'class' => [
+                    'btn',
+                    'btn-default',
+                    'btn-xs',
+                    'language-change-machine-translation',
+                ],
+                'data' => [
+                    'direction' => $toEnable ? 'enable' : 'disable',
+                ],
+                'aria-role' => 'button',
+            ]
+        );
     }
 }
