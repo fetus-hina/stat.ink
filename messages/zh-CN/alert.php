b3b7162e8b88020faf171680397609a8a70f5646
Gosin <canling0@gmail.com>
2020-09-02 16:01:46 +0100

    Finish translation of alert.php to Chinese

--- messages/zh-CN/alert.php
+++ messages/zh-CN/alert.php
@@ -9,13 +9,13 @@
 declare(strict_types=1);
 
 return [
-    'Due to: {reason}' => '',
-    'For SquidTracks or splatnet2statink users:' => '',
-    'Incorrect data will be registered if you do not use the latest version.' => '',
-    'Please update your client software and use always latest version (they will be updated to the latest version when restarted).' => '',
-    'Sorry for inconvenience.' => '',
-    'Term: {startDate} - {endDate}' => '',
-    'We\'ll perform maintenance on the schedule below:' => '',
-    'We won\'t implement automatic importing to {appName} for security reasons.' => '',
-    'You can import automatically from SplatNet 2, use these apps: (USE AT YOUR OWN RISK)' => '',
+    'Due to: {reason}' => '由于该原因：{reason}',
+    'For SquidTracks or splatnet2statink users:' => '针对SquidTracks或splatnet2statink的用户',
+    'Incorrect data will be registered if you do not use the latest version.' => '不使用最新版本会导致数据记录错误。',
+    'Please update your client software and use always latest version (they will be updated to the latest version when restarted).' => '请确保一直使用最新的客户端软件（重启可以使它们更新到最新版本）。',
+    'Sorry for inconvenience.' => '对可能的不便表示抱歉。',
+    'Term: {startDate} - {endDate}' => '时间：{startDate} - {endDate}',
+    'We\'ll perform maintenance on the schedule below:' => '我们会在以下时间进行服务器维护：',
+    'We won\'t implement automatic importing to {appName} for security reasons.' => '出于安全原因，我们不支持使用 {appName} 来自动导入。',
+    'You can import automatically from SplatNet 2, use these apps: (USE AT YOUR OWN RISK)' => '你可以使用这些软件来从SplatNet 2自动导入数据（风险自负）',
 ];
