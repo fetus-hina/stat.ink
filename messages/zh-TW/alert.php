7bb84025177a0def4c26a1ba2fda37aab93f4333
AIZAWA Hina <hina@fetus.jp>
2020-09-03 03:28:05 +0900

    Simplified Chinese to Traditional Chinese (using OpenCC) #743

--- messages/zh-TW/alert.php
+++ messages/zh-TW/alert.php
@@ -4,18 +4,19 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
 
 return [
-    'Due to: {reason}' => '',
-    'For SquidTracks or splatnet2statink users:' => '',
-    'Incorrect data will be registered if you do not use the latest version.' => '',
-    'Please update your client software and use always latest version (they will be updated to the latest version when restarted).' => '',
-    'Sorry for inconvenience.' => '',
-    'Term: {startDate} - {endDate}' => '',
-    'We\'ll perform maintenance on the schedule below:' => '',
-    'We won\'t implement automatic importing to {appName} for security reasons.' => '',
-    'You can import automatically from SplatNet 2, use these apps: (USE AT YOUR OWN RISK)' => '',
+    'Due to: {reason}' => '由於該原因：{reason}',
+    'For SquidTracks or splatnet2statink users:' => '針對SquidTracks或splatnet2statink的用戶',
+    'Incorrect data will be registered if you do not use the latest version.' => '不使用最新版本會導致數據記錄錯誤。',
+    'Please update your client software and use always latest version (they will be updated to the latest version when restarted).' => '請確保一直使用最新的客戶端軟件（重啓可以使它們更新到最新版本）。',
+    'Sorry for inconvenience.' => '對可能的不便表示抱歉。',
+    'Term: {startDate} - {endDate}' => '時間：{startDate} - {endDate}',
+    'We\'ll perform maintenance on the schedule below:' => '我們會在以下時間進行服務器維護：',
+    'We won\'t implement automatic importing to {appName} for security reasons.' => '出於安全原因，我們不支持使用 {appName} 來自動導入。',
+    'You can import automatically from SplatNet 2, use these apps: (USE AT YOUR OWN RISK)' => '你可以使用這些軟件來從SplatNet 2自動導入數據（風險自負）',
 ];
