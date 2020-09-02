d0b48e76a14bed168724c5ce932ee4847670f502
AIZAWA Hina <hina@fetus.jp>
2020-09-03 03:22:54 +0900

    add author (#743)

--- messages/zh-CN/ability2.php
+++ messages/zh-CN/ability2.php
@@ -4,6 +4,7 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
