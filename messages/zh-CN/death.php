2497127815d2c25b658575b7d8dda79f87859595
Gosin <canling0@gmail.com>
2020-09-03 21:07:46 +0100

    Finish translation of death.php to Chinese

--- messages/zh-CN/death.php
+++ messages/zh-CN/death.php
@@ -4,22 +4,23 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
 
 return [
-    'Drowning' => '',
-    'Fall' => '',
-    'Gadget' => '',
-    'Ink from a propeller' => '',
-    'Main Weapon' => '',
-    'Out of Bounds' => '',
-    'Rainmaker' => '',
-    'Rainmaker Inksplosion' => '',
-    'Rainmaker Shield' => '',
-    'Rainmaker Shot' => '',
-    'Special' => '',
-    'Sub Weapon' => '',
-    'Unknown' => '',
+    'Drowning' => '淹死',
+    'Fall' => '摔死',
+    'Gadget' => '物件',
+    'Ink from a propeller' => '来自扇页的墨水',
+    'Main Weapon' => '主武器',
+    'Out of Bounds' => '越界',
+    'Rainmaker' => '鱼炮',
+    'Rainmaker Inksplosion' => '鱼炮爆炸',
+    'Rainmaker Shield' => '鱼炮护盾',
+    'Rainmaker Shot' => '鱼炮射击',
+    'Special' => '大招',
+    'Sub Weapon' => '副武器',
+    'Unknown' => '未知',
 ];
