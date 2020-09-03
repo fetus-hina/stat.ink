6ec262a981833c4c94766184735e6acb2d9293f5
Gosin <canling0@gmail.com>
2020-09-03 20:43:40 +0100

    Finish translation of brand.php to Chinese

--- messages/zh-CN/brand.php
+++ messages/zh-CN/brand.php
@@ -4,26 +4,27 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
 
 return [
-    'Amiibo' => '',
-    'Cuttlegear' => '',
-    'Famitsu' => '',
-    'Firefin' => '',
-    'Forge' => '',
-    'Inkline' => '',
-    'KOG' => '',
-    'Krak-On' => '',
-    'Rockenberg' => '',
-    'Skalop' => '',
-    'Splash Mob' => '',
-    'Squidforce' => '',
-    'SQUID GIRL' => '',
-    'Takoroka' => '',
-    'Tentatek' => '',
-    'Zekko' => '',
-    'Zink' => '',
+    'Amiibo' => 'Amiibo',
+    'Cuttlegear' => '乌贼牌',
+    'Famitsu' => '电玩通',
+    'Firefin' => '火河豚',
+    'Forge' => '锻造',
+    'Inkline' => '墨线',
+    'KOG' => '游戏王',
+    'Krak-On' => '海蜇',
+    'Rockenberg' => '罗肯堡',
+    'Skalop' => '仙贝',
+    'Splash Mob' => '喷涂帮',
+    'Squidforce' => '乌贼力量',
+    'SQUID GIRL' => '乌贼女孩',
+    'Takoroka' => '乌贼人',
+    'Tentatek' => '东海堂',
+    'Zekko' => '贼扣',
+    'Zink' => '真可',
 ];
