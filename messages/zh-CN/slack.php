c2d29736af1dc5cf3a6a09b8c4a017fa2ac1a387
Gosin <canling0@gmail.com>
2020-09-04 19:18:54 +0100

    Finish translation of slack.php to Chinese

--- messages/zh-CN/slack.php
+++ messages/zh-CN/slack.php
@@ -4,16 +4,17 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
 
 return [
-    'lost' => '',
-    'Staaaay Fresh!' => '',
-    'unknown mode' => '',
-    'unknown stage' => '',
-    'won' => '',
-    '{name}: Just {winlose} {rule} at {stage}. <{url}|Detail>' => '',
-    '{name}: Just {winlose} {rule} at {stage}. {url}' => '',
+    'lost' => '失利',
+    'Staaaay Fresh!' => '保持持持新鲜',
+    'unknown mode' => '未知模式',
+    'unknown stage' => '未知场地',
+    'won' => '胜利',
+    '{name}: Just {winlose} {rule} at {stage}. <{url}|Detail>' => '{name}: 刚刚 在{stage} {winlose} {rule}。 <{url}|细节>',
+    '{name}: Just {winlose} {rule} at {stage}. {url}' => '{name}: 刚刚在{stage} {winlose} {rule}。 {url}',
 ];
