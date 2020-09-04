e6b9afe292da83194abecd858e11478e3d312d4b
Gosin <canling0@gmail.com>
2020-09-04 19:02:05 +0100

    Finish translation of salmon-title2.php to Chinese

--- messages/zh-CN/salmon-title2.php
+++ messages/zh-CN/salmon-title2.php
@@ -4,30 +4,31 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
 
 return [
-    'Any Title' => '',
-    'Apprentice' => '',
-    'Go-Getter' => '',
-    'Hazard Level MAX!!' => '',
-    'Intern' => '',
-    'Overachiever' => '',
-    'Part-Timer' => '',
-    'Profreshional' => '',
-    'Profreshional (Avg. {from}-{to})' => '',
-    '{boy}Apprentice' => '',
-    '{boy}Go-Getter' => '',
-    '{boy}Intern' => '',
-    '{boy}Overachiever' => '',
-    '{boy}Part-Timer' => '',
-    '{boy}Profreshional' => '',
-    '{girl}Apprentice' => '',
-    '{girl}Go-Getter' => '',
-    '{girl}Intern' => '',
-    '{girl}Overachiever' => '',
-    '{girl}Part-Timer' => '',
-    '{girl}Profreshional' => '',
+    'Any Title' => '所有称号',
+    'Apprentice' => '学徒',
+    'Go-Getter' => '潜力股',
+    'Hazard Level MAX!!' => '最大危险悉数',
+    'Intern' => '实习生',
+    'Overachiever' => '绩优生',
+    'Part-Timer' => '兼职员工',
+    'Profreshional' => '专业人士',
+    'Profreshional (Avg. {from}-{to})' => '专业水平（平均{from}到{to}）',
+    '{boy}Apprentice' => '{boy}学徒',
+    '{boy}Go-Getter' => '{boy}潜力股',
+    '{boy}Intern' => '{boy}实习生',
+    '{boy}Overachiever' => '{boy}绩优生',
+    '{boy}Part-Timer' => '{boy}兼职员工',
+    '{boy}Profreshional' => '{boy}专业人士',
+    '{girl}Apprentice' => '{girl}学徒',
+    '{girl}Go-Getter' => '{girl}潜力股',
+    '{girl}Intern' => '{girl}实习生',
+    '{girl}Overachiever' => '{girl}绩优生',
+    '{girl}Part-Timer' => '{girl}兼职员工',
+    '{girl}Profreshional' => '{girl}专业人士',
 ];
