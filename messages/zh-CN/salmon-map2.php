198c879f12fdf2f844287f07b9509c807fd383fc
Gosin <canling0@gmail.com>
2020-09-04 18:54:44 +0100

    Finish translation of salmon-map2.php to Chinese

--- messages/zh-CN/salmon-map2.php
+++ messages/zh-CN/salmon-map2.php
@@ -4,19 +4,20 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
 
 return [
-    'Bay' => '',
-    'Grounds' => '',
-    'Lost Outpost' => '',
-    'Marooner\'s Bay' => '',
-    'Outpost' => '',
-    'Polaris' => '',
-    'Ruins of Ark Polaris' => '',
-    'Salmonid Smokeyard' => '',
-    'Smokeyard' => '',
-    'Spawning Grounds' => '',
+    'Bay' => '破船湾',
+    'Grounds' => '养殖场',
+    'Lost Outpost' => '废弃前哨站',
+    'Marooner\'s Bay' => '破船湾',
+    'Outpost' => '前哨站',
+    'Polaris' => '北极星',
+    'Ruins of Ark Polaris' => '北极星方舟废墟',
+    'Salmonid Smokeyard' => '三文鱼烟熏厂',
+    'Smokeyard' => '烟熏厂',
+    'Spawning Grounds' => '养殖场',
 ];
