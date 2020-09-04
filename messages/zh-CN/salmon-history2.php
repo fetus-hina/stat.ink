35211ae3283b74592a5b01085cef08c15143b9a3
Gosin <canling0@gmail.com>
2020-09-04 18:51:23 +0100

    Finish translation of salmon-history2.php to Chinese

--- messages/zh-CN/salmon-history2.php
+++ messages/zh-CN/salmon-history2.php
@@ -4,16 +4,17 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
 
 return [
-    'Average' => '',
-    'Golden E.' => '',
-    'Points' => '',
-    'Power E.' => '',
-    'Rescued' => '',
-    'Shifts' => '',
-    'Total' => '',
+    'Average' => '平均',
+    'Golden E.' => '金蛋数',
+    'Points' => '分数',
+    'Power E.' => '蛋数',
+    'Rescued' => '援救',
+    'Shifts' => '打工次数',
+    'Total' => '总计',
 ];
