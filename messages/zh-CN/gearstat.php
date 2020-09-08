b62e101d61e062acbf1585c3658e0ed014eed4c5
AIZAWA Hina <hina@fetus.jp>
2020-09-08 14:29:09 +0900

    Fix author information (part of #747)

--- messages/zh-CN/gearstat.php
+++ messages/zh-CN/gearstat.php
@@ -4,7 +4,7 @@
  * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
- * @author Gosin <canlinng0@gmail.com>
+ * @author Gosin <canling0@gmail.com>
  */
 
 declare(strict_types=1);
