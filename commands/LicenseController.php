651d5ffd7cae5a6ea41fefeb6ec539742259685b
AIZAWA Hina <hina@fetus.jp>
2020-11-05 17:46:31 +0900

    Add license checker

---
+++ commands/LicenseController.php
@@ -0,0 +1,206 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2020 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\commands;
+
+use Exception;
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\console\Controller;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
+
+class LicenseController extends Controller
+{
+    public $defaultAction = 'check';
+
+    private array $safeLicenses = [
+        '0BSD',
+        'AFLv2.1',
+        'Apache-2.0',
+        'BSD',
+        'BSD*',
+        'BSD-2-Clause',
+        'BSD-3-Clause',
+        'ISC',
+        'MIT',
+        'MIT*',
+        'OFL-1.1',
+        'Unlicense',
+    ];
+
+    private array $safeLicenseRegexes = [
+        '/^CC-BY-[0-9.]+$/',
+        '/^CC0-[0-9.]+$/',
+        '/^LGPL/',
+    ];
+
+    public function actionCheck(): int
+    {
+        $status = 0;
+        $status |= $this->actionCheckPhp();
+        $status |= $this->actionCheckJs();
+        return $status;
+    }
+
+    public function actionCheckPhp(): int
+    {
+        return $this->check(
+            'composer',
+            vsprintf('/usr/bin/env %s --no-plugins license --format=%s', [
+                escapeshellarg(Yii::getAlias('@app/composer.phar')),
+                escapeshellarg('json'),
+            ]),
+            'dependencies',
+            function (array $json): array {
+                $results = [];
+                foreach ($json as $key => $values) {
+                    $version = $values['version'] ?? 'NONE';
+                    $package = "{$key}@${version}";
+                    $license = $values['license'] ?? null;
+                    if (is_array($license) && count($license) === 1) {
+                        $license = array_shift($license);
+                    }
+                    $results[$package] = is_string($license) ? $license : Json::encode($license);
+                }
+                return $results;
+            }
+        );
+    }
+
+    public function actionCheckJs(): int
+    {
+        return $this->check(
+            'npm',
+            vsprintf('/usr/bin/env %s %s --json', [
+                escapeshellarg('npx'),
+                escapeshellarg('license-checker-rseidelsohn'),
+            ]),
+            null,
+            function (array $json): array {
+                return array_map(
+                    function (array $values): string {
+                        $tmp = $values['licenses'] ?? null;
+                        return is_string($tmp) ? $tmp : Json::encode($tmp);
+                    },
+                    $json
+                );
+            }
+        );
+    }
+
+    protected function check(string $manager, string $cmdline, ?string $packagesSelector, ?callable $normalizer): int
+    {
+        try {
+            $jsonStr = $this->execCommand($cmdline);
+            if ($jsonStr === null) {
+                return 1;
+            }
+
+            try {
+                $json = Json::decode($jsonStr, true);
+            } catch (InvalidArgumentException $e) {
+                fwrite(STDERR, "Failed to decode a JSON: broken JSON\n");
+                return 1;
+            }
+            if (!is_array($json)) {
+                fwrite(STDERR, "Failed to decode a JSON: is not an array\n");
+                return 1;
+            }
+
+            if ($packagesSelector !== null) {
+                $json = ArrayHelper::getValue($json, $packagesSelector);
+            }
+
+            if ($normalizer !== null) {
+                $json = call_user_func($normalizer, $json);
+            }
+
+            return $this->doCheck($manager, $json);
+        } catch (Exception $e) {
+            return 1;
+        }
+    }
+
+    private function execCommand(string $cmdline): ?string
+    {
+        $lines = null;
+        $status = null;
+        @exec($cmdline, $lines, $status);
+        if ($status !== 0) {
+            fwrite(STDERR, "Failed to execute $cmdline (status={$status})\n");
+            return null;
+        }
+        return implode("\n", $lines);
+    }
+
+    private function doCheck(string $manager, array $json): int
+    {
+        $list = [];
+        foreach ($json as $package => $license) {
+            if (!$this->isSafeLicense($license)) {
+                $list[] = vsprintf("%-55s %s", [
+                    vsprintf('%s::%s', [$manager, $package]),
+                    $license,
+                ]);
+            }
+        }
+
+        if (!$list) {
+            return 0;
+        }
+
+        natcasesort($list);
+        fwrite(STDERR, join("\n", $list) . "\n");
+        return 1;
+    }
+
+    private function isSafeLicense(string $license): bool
+    {
+        // OK if known license
+        if (in_array($license, $this->safeLicenses, true)) {
+            return true;
+        }
+
+        // OK if known license (regex match)
+        foreach ($this->safeLicenseRegexes as $regex) {
+            if (preg_match($regex, $license)) {
+                return true;
+            }
+        }
+
+        // AND and OR, check recursive
+        $list = preg_split('/\s+(?:OR|AND)\s+/i', trim($license, '()'));
+        if (count($list) > 1) {
+            foreach ($list as $tmp) {
+                if ($this->isSafeLicense($tmp)) {
+                    return true;
+                }
+            }
+        }
+
+        // Process JSON encoded
+        if (substr($license, 0, 1) === '[') {
+            try {
+                $list = Json::decode($license);
+                if (is_array($list)) {
+                    foreach ($list as $tmp) {
+                        if ($this->isSafeLicense($tmp)) {
+                            return true;
+                        }
+                    }
+                }
+            } catch (Exception $e) {
+            }
+        }
+
+        return false;
+    }
+}
