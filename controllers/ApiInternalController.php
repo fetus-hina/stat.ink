a4ca0c7087475e4e610846ee8c3da8711c1b88d4
AIZAWA Hina <hina@fetus.jp>
2020-12-27 17:22:51 +0900

    Add internal API blog-entry

--- controllers/ApiInternalController.php
+++ controllers/ApiInternalController.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @copyright Copyright (C) 2015-2019 AIZAWA Hina
+ * @copyright Copyright (C) 2015-2020 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
  */
@@ -12,12 +12,12 @@ namespace app\controllers;
 
 use Yii;
 use app\actions\api\internal\ActivityAction;
+use app\actions\api\internal\BlogEntryAction;
 use app\actions\api\internal\CounterAction;
 use app\actions\api\internal\CurrentData2Action;
 use app\actions\api\internal\CurrentDataAction;
 use app\actions\api\internal\GuessTimezoneAction;
 use app\actions\api\internal\PatchBattleAction;
-use app\actions\api\internal\RecentBattlesAction;
 use app\actions\api\internal\SalmonStats2Action;
 use app\components\web\Controller;
 use yii\filters\VerbFilter;
@@ -43,12 +43,12 @@ class ApiInternalController extends Controller
     {
         return [
             'activity' => [ 'class' => ActivityAction::class ],
+            'blog-entry' => [ 'class' => BlogEntryAction::class ],
             'counter' => [ 'class' => CounterAction::class ],
             'current-data' => [ 'class' => CurrentDataAction::class ],
             'current-data2' => [ 'class' => CurrentData2Action::class ],
             'guess-timezone' => [ 'class' => GuessTimezoneAction::class ],
             'patch-battle' => [ 'class' => PatchBattleAction::class ],
-            'recent-battles' => [ 'class' => RecentBattlesAction::class ],
             'salmon-stats2' => [ 'class' => SalmonStats2Action::class ],
         ];
     }
