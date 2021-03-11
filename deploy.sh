57040224a9a229e4207c3f8802f2f8eda19c7342
AIZAWA Hina <hina@fetus.jp>
2021-03-11 19:28:06 +0900

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,40 @@
 
 set -eu
 
+function current_version() {
+    VTAGS=$(/usr/bin/env git tag -l "v*" --sort="-v:refname")
+    for VTAG in $VTAGS; do
+        if [[ $VTAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+            echo $VTAG
+            return 0
+        fi
+    done
+    echo "Could not detect current version"
+    exit 1
+}
+
 if [ $# -ne 1 ]; then
-  echo "USAGE: $0 vA.B.C"
+  echo "USAGE: $0 {vA.B.C|minor|patch}"
   exit 1
 fi
 
-if [[ $1 =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
-  git tag -s $1 -m $1
+TAG=""
+if [ $1 = "current" ]; then
+  current_version
+  exit 0
+elif [ $1 = "minor" -o $1 = "patch" ]; then
+  PREV=$(current_version)
+  TAG=$(npx semver -i $1 $PREV)
+elif [[ $1 =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  TAG=$1
+fi
+
+if [[ $TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  git tag -s $TAG -m $TAG
 else
-  echo "Invalid version tag $1. Should be like v1.2.3"
+  echo "Invalid version tag $1. Should be like v1.2.3, 'minor' or 'patch'"
   exit 1
 fi
 
-git push origin dev master $1
-ssh statink@app1.stat.ink 'pushd stat.ink && git fetch origin && git checkout composer.lock package-lock.json && touch -r composer.json composer.lock && touch -r package.json package-lock.json && git merge --ff-only origin/master && scl enable php74 make && scl enable php74 "./yii asset/up-revision" && rm -rfv runtime/Smarty/compile/*'
+git push origin dev master $TAG
+ssh statink@app1.stat.ink 'pushd stat.ink && git fetch origin && git checkout composer.lock package-lock.json && touch -r composer.json composer.lock && touch -r package.json package-lock.json && git merge --ff-only origin/master && scl enable php74 make && scl enable php74 "./yii asset/up-revision" && sudo bin/restart-supervisord.sh'
