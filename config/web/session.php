8b6b7b48dbc754fe66a5b6eac9814f7df3d309b1
AIZAWA Hina <hina@fetus.jp>
2021-03-11 20:20:26 +0900

    Update session config

--- config/web/session.php
+++ config/web/session.php
@@ -3,16 +3,19 @@
 declare(strict_types=1);
 
 use app\components\web\Session;
+use yii\web\Cookie;
 
 return [
     'class' => Session::class,
-    'timeout' => 86400,
     'cacheLimiter' => 'nocache',
     'cookieParams' => [
         'httponly' => true,
+        'samesite' => Cookie::SAME_SITE_STRICT,
         'secure' => (bool)preg_match(
             '/(?:^|\.)stat\.ink$/i',
             $_SERVER['HTTP_HOST'] ?? ''
         ),
     ],
+    'name' => YII_ENV_DEV ? 'SESSID_DEVENV' : 'PHPSESSID',
+    'timeout' => 86400,
 ];
