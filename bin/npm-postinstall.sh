96f0f2c171a275c9cdb6d7244e21487c7b09904d
AIZAWA Hina <hina@fetus.jp>
2022-03-07 03:05:54 +0900

    deps

--- bin/npm-postinstall.sh
+++ bin/npm-postinstall.sh
@@ -6,5 +6,5 @@ pushd $DIR >/dev/null 2>&1
   npx patch-package --patch-dir=data/patch/npm
   rm -rf $DIR/data/licenses-npm
   mkdir -p $DIR/data/licenses-npm
-  npx license-checker-rseidelsohn --production --files $DIR/data/licenses-npm/
+  npx license-checker-rseidelsohn --production --files $DIR/data/licenses-npm/ >/dev/null
 popd >/dev/null 2>&1
