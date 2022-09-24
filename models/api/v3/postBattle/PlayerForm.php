e0f57a7369fe328167d796c0f5ff042c2832d8f9
eli <frozenpandaman@users.noreply.github.com>
2022-09-24 05:02:53 -1000

    allow 5-digit splashtag #s

--- models/api/v3/postBattle/PlayerForm.php
+++ models/api/v3/postBattle/PlayerForm.php
@@ -55,7 +55,7 @@ final class PlayerForm extends Model
             [['me', 'disconnected'], 'in', 'range' => ['yes', 'no', true, false]],
             [['rank_in_team'], 'integer', 'min' => 1, 'max' => 4],
             [['name'], 'string', 'min' => 1, 'max' => 10],
-            [['number'], 'integer', 'min' => 0, 'max' => 9999],
+            [['number'], 'integer', 'min' => 0, 'max' => 99999],
             [['splashtag_title'], 'string', 'max' => 255],
             [['weapon'], 'string'],
             [['weapon'], KeyValidator::class,
