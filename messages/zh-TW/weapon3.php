a66b8921183da8808770bba321453bcbc1708960
AIZAWA Hina <hina@fetus.jp>
2022-09-30 00:32:12 +0900

    Update author

--- messages/zh-TW/weapon3.php
+++ messages/zh-TW/weapon3.php
@@ -4,6 +4,7 @@
  * @copyright Copyright (C) 2015-2022 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author midmiako <mayomi@baka.wang>
  */
 
 declare(strict_types=1);
