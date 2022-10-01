4af1a2b7c83a0290ca21cdabfd61160aba320e6b
AIZAWA Hina <hina@fetus.jp>
2022-10-01 22:42:54 +0900

    Add schedule3 to internal api

--- actions/api/internal/ScheduleAction.php
+++ actions/api/internal/ScheduleAction.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @copyright Copyright (C) 2015-2020 AIZAWA Hina
+ * @copyright Copyright (C) 2015-2022 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
  */
@@ -10,29 +10,19 @@ declare(strict_types=1);
 
 namespace app\actions\api\internal;
 
-use DateTime;
 use DateTimeImmutable;
 use DateTimeZone;
 use Yii;
-use app\assets\GameModeIconsAsset;
-use app\assets\Spl2WeaponAsset;
+use app\actions\api\internal\schedule\Splatoon2;
+use app\actions\api\internal\schedule\Splatoon3;
 use app\components\helpers\Battle as BattleHelper;
-use app\models\Map2;
-use app\models\SalmonSchedule2;
-use app\models\SalmonWeapon2;
-use app\models\Schedule2;
-use app\models\ScheduleMode2;
-use statink\yii2\stages\spl2\StagesAsset as Stages2Asset;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Url;
-use yii\web\ViewAction;
+use yii\base\Action;
 
-use function array_merge;
-use function strtotime;
-use function time;
-
-class ScheduleAction extends ViewAction
+final class ScheduleAction extends Action
 {
+    use Splatoon2;
+    use Splatoon3;
+
     private DateTimeImmutable $now;
     private int $currentPeriod;
 
@@ -40,7 +30,7 @@ class ScheduleAction extends ViewAction
     {
         parent::init();
 
-        $t = (int)($_SERVER['REQUEST_TIME'] ?? time());
+        $t = (int)($_SERVER['REQUEST_TIME'] ?? \time());
         $this->now = (new DateTimeImmutable())
             ->setTimestamp($t)
             ->setTimezone(new DateTimeZone('Etc/UTC'));
@@ -50,7 +40,7 @@ class ScheduleAction extends ViewAction
     public function run()
     {
         $response = Yii::$app->getResponse();
-        $response->format = 'compact-json';
+        $response->format = YII_ENV_PROD ? 'compact-json' : 'json';
 
         return [
             'time' => $this->now->getTimestamp(),
@@ -61,6 +51,7 @@ class ScheduleAction extends ViewAction
             ],
             'sources' => $this->getSources(),
             'splatoon2' => $this->getSplatoon2(),
+            'splatoon3' => $this->getSplatoon3(),
             'translations' => $this->getTranslations(),
         ];
     }
@@ -82,170 +73,10 @@ class ScheduleAction extends ViewAction
                 'url' => 'https://splatoon2.ink/',
                 'name' => 'Splatoon2.ink',
             ],
+            's3ink' => [
+                'url' => 'https://splatoon3.ink/',
+                'name' => 'Splatoon3.ink',
+            ],
         ];
-    }
-
-    private function getSplatoon2(): array
-    {
-        return array_merge(
-            $this->getSplatoon2Battles(),
-            ['salmon' => $this->getSalmon2()],
-        );
-    }
-
-    private function getSplatoon2Battles(): array
-    {
-        $am = Yii::$app->assetManager;
-        return ArrayHelper::map(
-            ScheduleMode2::find()->orderBy(['id' => SORT_ASC])->all(),
-            'key',
-            function (ScheduleMode2 $mode) use ($am): array {
-                return [
-                    'key' => $mode->key,
-                    'name' => Yii::t('app-rule2', $mode->name),
-                    'image' => $mode->key === 'regular'
-                        ? null
-                        : Url::to(
-                            $am->getAssetUrl(
-                                $am->getBundle(GameModeIconsAsset::class, true),
-                                sprintf('spl2/%s.png', $mode->key)
-                            ),
-                            true
-                        ),
-                    'source' => 's2ink',
-                    'schedules' => ArrayHelper::getColumn(
-                        Schedule2::find()
-                            ->andWhere(['mode_id' => $mode->id])
-                            ->andWhere(['>=', '{{schedule2}}.[[period]]', $this->currentPeriod])
-                            ->orderBy([
-                                '{{schedule2}}.[[period]]' => SORT_ASC,
-                            ])
-                            ->limit(10)
-                            ->with([
-                                'maps',
-                                'rule',
-                            ])
-                            ->all(),
-                        function (Schedule2 $sc) use ($am): array {
-                            return [
-                                'time' => BattleHelper::periodToRange2((int)$sc->period),
-                                'rule' => [
-                                    'key' => $sc->rule->key,
-                                    'name' => Yii::t('app-rule2', $sc->rule->name),
-                                    'short' => Yii::t('app-rule2', $sc->rule->short_name),
-                                    'icon' => Url::to(
-                                        $am->getAssetUrl(
-                                            $am->getBundle(GameModeIconsAsset::class, true),
-                                            sprintf('spl2/%s.png', $sc->rule->key)
-                                        ),
-                                        true
-                                    ),
-                                ],
-                                'maps' => ArrayHelper::getColumn(
-                                    $sc->maps,
-                                    function (Map2 $map) use ($am): array {
-                                        return [
-                                            'key' => $map->key,
-                                            'name' => Yii::t('app-map2', $map->name),
-                                            'image' => Url::to(
-                                                $am->getAssetUrl(
-                                                    $am->getBundle(Stages2Asset::class, true),
-                                                    sprintf('daytime/%s.jpg', $map->key)
-                                                ),
-                                                true
-                                            ),
-                                        ];
-                                    }
-                                ),
-                            ];
-                        }
-                    ),
-                ];
-            }
-        );
-    }
-
-    private function getSalmon2(): array
-    {
-        $am = Yii::$app->assetManager;
-        return [
-            'key' => 'salmon',
-            'name' => Yii::t('app-salmon2', 'Salmon Run'),
-            'image' => Url::to(
-                $am->getAssetUrl(
-                    $am->getBundle(GameModeIconsAsset::class, true),
-                    'spl2/salmon.png',
-                ),
-                true
-            ),
-            'source' => 's2ink',
-            'schedules' => ArrayHelper::getColumn(
-                SalmonSchedule2::find()
-                    ->andWhere([
-                        '>',
-                        '{{salmon_schedule2}}.[[end_at]]',
-                        $this->now->format(DateTime::ATOM)
-                    ])
-                    ->orderBy([
-                        '{{salmon_schedule2}}.[[end_at]]' => SORT_ASC,
-                    ])
-                    ->limit(10)
-                    ->with([
-                        'map',
-                        'weapons.weapon',
-                    ])
-                    ->all(),
-                function (SalmonSchedule2 $sc) use ($am): array {
-                    return [
-                        'time' => [
-                            strtotime($sc->start_at),
-                            strtotime($sc->end_at),
-                        ],
-                        'maps' => [[
-                            'key' => $sc->map->key,
-                            'name' => Yii::t('app-salmon-map2', $sc->map->name),
-                            'image' => Url::to(
-                                $am->getAssetUrl(
-                                    $am->getBundle(Stages2Asset::class, true),
-                                    sprintf('daytime/%s.jpg', $sc->map->key)
-                                ),
-                                true
-                            ),
-                        ]],
-                        'weapons' => $this->fillSalmonWeapon(
-                            ArrayHelper::getColumn(
-                                $sc->weapons,
-                                function (SalmonWeapon2 $info) use ($am): array {
-                                    $w = $info->weapon;
-                                    return [
-                                        'key' => $w->key,
-                                        'name' => Yii::t('app-weapon2', $w->name),
-                                        'icon' => Url::to(
-                                            $am->getAssetUrl(
-                                                $am->getBundle(Spl2WeaponAsset::class, true),
-                                                $w->key . '.png'
-                                            ),
-                                            true
-                                        ),
-                                    ];
-                                }
-                            )
-                        ),
-                    ];
-                },
-            ),
-        ];
-    }
-
-    private function fillSalmonWeapon(array $list): array
-    {
-        while (count($list) < 4) {
-            $list[] = [
-                'key' => 'random',
-                'name' => Yii::t('app-salmon2', 'Random'),
-                'icon' => null,
-            ];
-        }
-        return $list;
     }
 }
---
+++ actions/api/internal/schedule/Battle3.php
@@ -0,0 +1,171 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\actions\api\internal\schedule;
+
+use Yii;
+use app\assets\GameModeIconsAsset;
+use app\assets\Spl3StageAsset;
+use app\components\helpers\Battle as BattleHelper;
+use app\models\Lobby3;
+use app\models\Map3;
+use app\models\Rule3;
+use app\models\Schedule3;
+use app\models\ScheduleMap3;
+use yii\db\Query;
+use yii\helpers\Url;
+use yii\web\View;
+
+use const SORT_ASC;
+
+trait Battle3
+{
+    private function getBattle3(): array
+    {
+        $period = $this->currentPeriod;
+        $lobbies = $this->getLobbies3($period);
+        return \array_combine(
+            // keys
+            \array_map(
+                fn (Lobby3 $lobby): string => $lobby->key,
+                $lobbies
+            ),
+            // values
+            \array_map(
+                fn (Lobby3 $lobby): array => [
+                    'key' => $lobby->key,
+                    'name' => Yii::t('app-lobby3', $lobby->name),
+                    'image' => $this->getIconUrlForLobby3($lobby),
+                    'source' => 's3ink',
+                    'schedules' => $this->getBattleSchedules3($period, $lobby),
+                ],
+                $lobbies
+            )
+        );
+    }
+
+    // 直近に開催されるロビーの一覧を取得
+    private function getLobbies3(int $period): array
+    {
+        $availableLobbyIds = (new Query())
+            ->select('lobby_id')
+            ->from('{{%schedule3}}')
+            ->where(['between', 'period', $period, $period + 3])
+            ->groupBy(['lobby_id'])
+            ->column();
+
+        return Lobby3::find()
+            ->andWhere(['id' => $availableLobbyIds])
+            ->orderBy(['rank' => SORT_ASC])
+            ->all();
+    }
+
+    private function getBattleSchedules3(int $period, Lobby3 $lobby): array
+    {
+        $schedules = Schedule3::find()
+            ->with([
+                'rule',
+                'scheduleMap3s' => function (Query $q): void {
+                    $q->orderBy(['id' => SORT_ASC]);
+                },
+                'scheduleMap3s.map',
+            ])
+            ->andWhere(['and',
+                ['lobby_id' => $lobby->id],
+                ['between', 'period', $period, $period + 3],
+            ])
+            ->orderBy(['period' => SORT_ASC])
+            ->all();
+
+        return \array_map(
+            function (Schedule3 $schedule): array {
+                $rule = $schedule->rule;
+                return [
+                    'time' => BattleHelper::periodToRange($schedule->period),
+                    'rule' => [
+                        'key' => $rule->key,
+                        'name' => Yii::t('app-rule3', $rule->name),
+                        'short' => Yii::t('app-rule3', $rule->short_name),
+                        'icon' => $this->getIconUrlForRule3($rule),
+                    ],
+                    'maps' => \array_map(
+                        function (ScheduleMap3 $model): array {
+                            $map = $model->map;
+                            return [
+                                'key' => $map->key,
+                                'name' => Yii::t('app-map3', $map->name),
+                                'image' => $this->getImageUrlForMap3($map),
+                            ];
+                        },
+                        $schedule->scheduleMap3s
+                    ),
+                ];
+            },
+            $schedules
+        );
+    }
+
+    private function getIconUrlForLobby3(Lobby3 $lobby): ?string
+    {
+        $view = Yii::$app->view;
+        if (!$view instanceof View) {
+            return null;
+        }
+
+        $am = Yii::$app->assetManager;
+        return Url::to(
+            $am->getAssetUrl(
+                GameModeIconsAsset::register($view),
+                \vsprintf('spl3/%s.png', [
+                    $lobby->key,
+                ])
+            ),
+            true
+        );
+    }
+
+    private function getIconUrlForRule3(Rule3 $rule): ?string
+    {
+        $view = Yii::$app->view;
+        if (!$view instanceof View) {
+            return null;
+        }
+
+        $am = Yii::$app->assetManager;
+        return Url::to(
+            $am->getAssetUrl(
+                GameModeIconsAsset::register($view),
+                \vsprintf('spl3/%s.png', [
+                    $rule->key,
+                ])
+            ),
+            true
+        );
+    }
+
+    private function getImageUrlForMap3(Map3 $map): ?string
+    {
+        $view = Yii::$app->view;
+        if (!$view instanceof View) {
+            return null;
+        }
+
+        $am = Yii::$app->assetManager;
+        return Url::to(
+            $am->getAssetUrl(
+                Spl3StageAsset::register($view),
+                \vsprintf('color-normal/%s.png', [
+                    $map->key,
+                ])
+            ),
+            true
+        );
+    }
+}
