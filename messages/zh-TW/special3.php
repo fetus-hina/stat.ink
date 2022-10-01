4ab3a12026bee574b501e871a1c24515f8a2bc53
AIZAWA Hina <hina@fetus.jp>
2022-10-02 02:07:59 +0900

    Update file headers

--- messages/zh-TW/special3.php
+++ messages/zh-TW/special3.php
@@ -3,6 +3,7 @@
 /**
  * @copyright Copyright (C) 2015-2022 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author ADe <loezone@gmail.com>
  * @author AIZAWA Hina <hina@fetus.jp>
  */
 
