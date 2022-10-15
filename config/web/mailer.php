793cbecf5253ad6b85513f2c0c12f37075551c5d
AIZAWA Hina <hina@fetus.jp>
2022-10-15 16:07:54 +0900

    Use yii2-symfonymailer instead of yii2-swiftmailer (fix #1026)

--- config/web/mailer.php
+++ config/web/mailer.php
@@ -2,9 +2,15 @@
 
 declare(strict_types=1);
 
-use yii\swiftmailer\Mailer;
+use yii\symfonymailer\Mailer;
 
 return [
     'class' => Mailer::class,
     'textLayout' => '@app/views/email/layout-text',
+    'transport' => [
+        'scheme' => 'smtp',
+        'host' => 'localhost',
+        'port' => 25,
+        'dsn' => 'native://default',
+    ],
 ];
