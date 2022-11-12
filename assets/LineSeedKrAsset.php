bce1c266d9dd01e3a6e75a90fe751cf36ae8e931
AIZAWA Hina <hina@fetus.jp>
2022-11-13 08:11:46 +0900

    Add LINE Seed KR

---
+++ assets/LineSeedKrAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+final class LineSeedKrAsset extends AssetBundle
+{
+    public $css = [
+        'https://fonts.stats.ink/lineseedkr/lineseedkr.min.css',
+    ];
+}
