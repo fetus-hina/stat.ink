30abeedd2ffbb254d375d77124fa9d2276ec0204
Lukas Böttcher <github@muffl0n.de>
2022-11-13 02:09:45 +0100

    Add german translation for counter.php

--- messages/de/counter.php
+++ messages/de/counter.php
@@ -9,7 +9,7 @@
 declare(strict_types=1);
 
 return [
-    'Battles' => '',
-    'Shifts' => '',
-    'Users' => '',
+    'Battles' => 'Kämpfe',
+    'Shifts' => 'Schichten',
+    'Users' => 'Benutzer',
 ];
