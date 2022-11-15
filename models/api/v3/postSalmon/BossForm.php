358ad755034c7c688d4dff325ddcab1b182661ce
AIZAWA Hina <hina@fetus.jp>
2022-11-16 06:25:54 +0900

    Fix author (#1128)

--- models/api/v3/postSalmon/BossForm.php
+++ models/api/v3/postSalmon/BossForm.php
@@ -4,7 +4,6 @@
  * @copyright Copyright (C) 2015-2022 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
- * @author eli <frozenpandaman@users.noreply.github.com>
  */
 
 namespace app\models\api\v3\postSalmon;
