88c043ce252fe4ba08eb060a8204d7b2e1fca31f
ADe <loezone@gmail.com>
2022-11-20 21:37:42 +0800

    feat: update zh-TW translation for salmon run NW

--- messages/zh-TW/salmon3.php
+++ messages/zh-TW/salmon3.php
@@ -10,9 +10,9 @@
 declare(strict_types=1);
 
 return [
-    'Boss Salmonid' => '',
-    'King Salmonid' => '',
-    'Known Occurrence' => '',
+    'Boss Salmonid' => '巨大鮭魚',
+    'King Salmonid' => '頭目鮭魚',
+    'Known Occurrence' => '特殊狀況',
     'Salmon Run' => 'Salmon Run',
     'Time is up' => '時間到',
     'Wipe out' => '全員陣亡',
