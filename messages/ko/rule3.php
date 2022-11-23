e2e8d3b3bb037c7df47e323784248e374fd51778
AIZAWA Hina <hina@fetus.jp>
2022-11-24 08:27:13 +0900

    Regen korean translations to fix author header (#1143)

--- messages/ko/rule3.php
+++ messages/ko/rule3.php
@@ -4,23 +4,22 @@
  * @copyright Copyright (C) 2015-2022 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author cake monotone <cake.monotone@gmail.com>
  */
 
 declare(strict_types=1);
 
 return [
     'Any Mode' => '모든 룰',
-    'Ranked Modes' => '랭크 룰',
-
+    'CB' => '바지락',
     'Clam Blitz' => '랭크 바지락',
     'Rainmaker' => '랭크 피쉬 배틀',
-    'Splat Zones' => '랭크 에어리어',
-    'Tower Control' => '랭크 타워',
-    'Turf War' => '영역 배틀',
-
-    'CB' => '바지락',
+    'Ranked Modes' => '랭크 룰',
     'RM' => '피쉬 배틀',
+    'Splat Zones' => '랭크 에어리어',
     'SZ' => '에어리어',
     'TC' => '타워',
+    'Tower Control' => '랭크 타워',
+    'Turf War' => '영역 배틀',
     'TW' => '영역 배틀',
 ];
