4af60c102b0139f284d1efb77dc98673ddcac359
AIZAWA Hina <hina@fetus.jp>
2022-11-26 17:30:01 +0900

    Add /api/v3/salmon/weapon
    3. of https://github.com/fetus-hina/stat.ink/issues/1128#issuecomment-1320917823

---
+++ actions/api/v3/salmon/SalmonWeaponAction.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\actions\api\v3\salmon;
+
+use app\actions\api\v3\traits\ApiInitializerTrait;
+use app\components\formatters\api\v3\SalmonWeaponApiFormatter;
+use app\models\SalmonWeapon3;
+use yii\helpers\ArrayHelper;
+use yii\web\ViewAction;
+
+use const SORT_ASC;
+
+final class SalmonWeaponAction extends ViewAction
+{
+    use ApiInitializerTrait;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->apiInit();
+    }
+
+    /**
+     * @return array[]
+     */
+    public function run(bool $full = false): array
+    {
+        return \array_values(
+            ArrayHelper::getColumn(
+                SalmonWeapon3::find()
+                    ->with(['salmonWeapon3Aliases'])
+                    ->orderBy([
+                        '(weapon_id IS NULL)' => SORT_ASC,
+                        'key' => SORT_ASC,
+                    ])
+                    ->all(),
+                fn (SalmonWeapon3 $model): array => SalmonWeaponApiFormatter::toJson($model, $full),
+            ),
+        );
+    }
+}
