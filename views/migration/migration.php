fb348275dd84a0f75732530cba0028bde0fd1999
AIZAWA Hina <hina@fetus.jp>
2022-11-29 11:28:26 +0900

    Update migration scripts

--- views/migration/migration.php
+++ views/migration/migration.php
@@ -22,33 +22,30 @@ $inTransaction = (isset($inTransaction) && $inTransaction);
 $upFuncName = $inTransaction ? 'safeUp' : 'up';
 $downFuncName = $inTransaction ? 'safeDown' : 'down';
 
-$codeFmt = function (?string $code, int $indent): string {
-    if ($code === null || $code === '') {
-        return '';
-    }
-
-    return implode(
-        "\n",
-        array_map(
-            function (string $line) use ($indent): string {
-                return str_repeat(' ', $indent) . $line;
-            },
-            explode("\n", $code)
-        )
-    ) . "\n";
-};
+$codeFmt = fn (?string $code, int $indent): string => ($code === null || $code === '')
+    ? ''
+    : (
+        implode(
+            "\n",
+            array_map(
+                fn (string $line): string => rtrim(str_repeat(' ', $indent) . $line),
+                explode("\n", $code),
+            ),
+        ) . "\n"
+    );
 
 $uses = [
     Migration::class,
 ];
 if (isset($traits)) {
     $traits = array_unique((array)$traits);
-    $uses = array_merge($uses, array_map(
-        function (string $trait): string {
-            return ltrim($trait, '\\');
-        },
-        $traits
-    ));
+    $uses = array_merge(
+        $uses,
+        array_map(
+            fn (string $trait): string => ltrim($trait, '\\'),
+            $traits,
+        ),
+    );
 }
 sort($uses);
 $uses = array_unique($uses);
