45091348c55a1061f02a275c8e3af19d31ad68d4
AIZAWA Hina <hina@fetus.jp>
2022-11-29 18:45:56 +0900

    Add user_stat3_x_match table (#1146)

---
+++ components/helpers/userStatsV3/XStatsTrait.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\components\helpers\userStatsV3;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+use DateTimeZone;
+use LogicException;
+use PDO;
+use Yii;
+use app\models\Battle3;
+use app\models\Lobby3;
+use app\models\Rule3;
+use app\models\User;
+use app\models\UserStat3XMatch;
+use yii\db\Connection;
+use yii\db\Exception as DbException;
+use yii\db\Expression;
+use yii\db\Query;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
+
+use const SORT_ASC;
+use const SORT_DESC;
+
+trait XStatsTrait
+{
+    use AggregateFunctionsTrait;
+
+    protected static function createUserStatsX(
+        Connection $db,
+        User $user,
+        DateTimeImmutable $now,
+    ): bool {
+        try {
+            UserStat3XMatch::deleteAll(['user_id' => $user->id]);
+
+            $rows = self::createStatsAggregateQueryX($db, $user, $now)
+                ->createCommand($db)
+                ->queryAll(PDO::FETCH_ASSOC);
+
+            foreach ($rows as $row) {
+                if (!self::saveAggregatedStatsX($db, $user, $now, $row)) {
+                    $db->transaction->rollBack();
+                    return false;
+                }
+            }
+
+            return true;
+        } catch (DbException $e) {
+            Yii::error(
+                \vsprintf('Catch %s, message=%s', [
+                    \get_class($e),
+                    $e->getMessage(),
+                ]),
+                __METHOD__,
+            );
+            $db->transaction->rollBack();
+            return false;
+        }
+    }
+
+    private static function createStatsAggregateQueryX(
+        Connection $db,
+        User $user,
+        DateTimeImmutable $now,
+    ): Query {
+        $null = new Expression('NULL');
+
+        if (!$lobby = Lobby3::find()->andWhere(['key' => 'xmatch'])->limit(1)->one()) {
+            throw new LogicException();
+        }
+
+        $rules = Rule3::find()
+            ->innerJoinWith(['group'], false)
+            ->andWhere(['{{%rule_group3}}.[[key]]' => 'gachi'])
+            ->all();
+        if (!$rules) {
+            throw new LogicException();
+        }
+
+        return (new Query())
+            ->from(['b' => '{{%battle3}}'])
+            ->leftJoin(['r' => '{{%result3}}'], '{{b}}.[[result_id]] = {{r}}.[[id]]')
+            ->andWhere([
+                '{{b}}.[[is_deleted]]' => false,
+                '{{b}}.[[lobby_id]] ' => (int)$lobby->id,
+                '{{b}}.[[rule_id]]' => ArrayHelper::getColumn($rules, 'id'),
+                '{{b}}.[[user_id]]' => (int)$user->id,
+            ])
+            ->groupBy([
+                '{{b}}.[[user_id]]',
+                '{{b}}.[[rule_id]]',
+            ])
+            ->orderBy([
+                '{{b}}.[[user_id]]' => SORT_ASC,
+                '{{b}}.[[rule_id]]' => SORT_ASC,
+            ])
+            ->select([
+                'user_id' => '{{b}}.[[user_id]]',
+                'rule_id' => '{{b}}.[[rule_id]]',
+                'battles' => 'COUNT(*)',
+                'agg_battles' => self::statsSum('1'),
+                'agg_seconds' => self::statsSum(\vsprintf('(%s) - (%s)', [
+                    self::statsTimestamp('{{b}}.[[end_at]]'),
+                    self::statsTimestamp('{{b}}.[[start_at]]'),
+                ])),
+                'wins' => self::statsSum('1', ['{{r}}.[[is_win]] = TRUE']),
+                'kills' => self::statsSum('{{b}}.[[kill]]'),
+                'assists' => self::statsSum('{{b}}.[[assist]]'),
+                'deaths' => self::statsSum('{{b}}.[[death]]'),
+                'specials' => self::statsSum('{{b}}.[[special]]'),
+                'inked' => self::statsSum('{{b}}.[[inked]]'),
+                'max_inked' => self::statsMax('{{b}}.[[inked]]'),
+                'peak_x_power' => \vsprintf('GREATEST(%s, %s)', [
+                    self::statsMax('{{b}}.[[x_power_before]]'),
+                    self::statsMax('{{b}}.[[x_power_after]]'),
+                ]),
+                'peak_season' => $null,
+                'current_x_power' => $null,
+                'current_season' => $null,
+                'updated_at' => new Expression(
+                    \sprintf(
+                        '%s::TIMESTAMP(0) WITH TIME ZONE',
+                        $db->quoteValue($now->format(DateTimeInterface::ATOM)),
+                    ),
+                ),
+            ]);
+    }
+
+    private static function saveAggregatedStatsX(
+        Connection $db,
+        User $user,
+        DateTimeImmutable $now,
+        array $row,
+    ): bool {
+        $model = Yii::createObject(UserStat3XMatch::class);
+        $model->attributes = $row;
+
+        // TODO: peak_season
+
+        if ($model->save()) {
+            return true;
+        }
+
+        Yii::error('Failed to save the model, ' . Json::encode($model->getErrors()), __METHOD__);
+        return false;
+    }
+}
---
+++ models/UserStat3XMatch.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "user_stat3_x_match".
+ *
+ * @property integer $user_id
+ * @property integer $rule_id
+ * @property integer $battles
+ * @property integer $agg_battles
+ * @property integer $agg_seconds
+ * @property integer $wins
+ * @property integer $kills
+ * @property integer $assists
+ * @property integer $deaths
+ * @property integer $specials
+ * @property integer $inked
+ * @property integer $max_inked
+ * @property string $peak_x_power
+ * @property string $peak_season
+ * @property string $current_x_power
+ * @property string $current_season
+ * @property string $updated_at
+ *
+ * @property Rule3 $rule
+ * @property User $user
+ */
+class UserStat3XMatch extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'user_stat3_x_match';
+    }
+
+    public function rules()
+    {
+        return [
+            [['user_id', 'rule_id', 'battles', 'agg_battles', 'agg_seconds', 'wins', 'kills', 'assists', 'deaths', 'specials', 'inked', 'max_inked', 'updated_at'], 'required'],
+            [['user_id', 'rule_id', 'battles', 'agg_battles', 'agg_seconds', 'wins', 'kills', 'assists', 'deaths', 'specials', 'inked', 'max_inked'], 'default', 'value' => null],
+            [['user_id', 'rule_id', 'battles', 'agg_battles', 'agg_seconds', 'wins', 'kills', 'assists', 'deaths', 'specials', 'inked', 'max_inked'], 'integer'],
+            [['peak_x_power', 'current_x_power'], 'number'],
+            [['peak_season', 'current_season', 'updated_at'], 'safe'],
+            [['user_id', 'rule_id'], 'unique', 'targetAttribute' => ['user_id', 'rule_id']],
+            [['rule_id'], 'exist', 'skipOnError' => true, 'targetClass' => Rule3::class, 'targetAttribute' => ['rule_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'rule_id' => 'Rule ID',
+            'battles' => 'Battles',
+            'agg_battles' => 'Agg Battles',
+            'agg_seconds' => 'Agg Seconds',
+            'wins' => 'Wins',
+            'kills' => 'Kills',
+            'assists' => 'Assists',
+            'deaths' => 'Deaths',
+            'specials' => 'Specials',
+            'inked' => 'Inked',
+            'max_inked' => 'Max Inked',
+            'peak_x_power' => 'Peak X Power',
+            'peak_season' => 'Peak Season',
+            'current_x_power' => 'Current X Power',
+            'current_season' => 'Current Season',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    public function getRule(): ActiveQuery
+    {
+        return $this->hasOne(Rule3::class, ['id' => 'rule_id']);
+    }
+
+    public function getUser(): ActiveQuery
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+}
