c02b143025008bb7e40ec18a55534ea09dc3e078
AIZAWA Hina <hina@fetus.jp>
2022-12-16 01:09:01 +0900

    Add database/vacuum command

---
+++ commands/DatabaseController.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\commands;
+
+use app\commands\database\VacuumAction;
+
+use Yii;
+use yii\console\Controller;
+
+final class DatabaseController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'vacuum' => VacuumAction::class,
+        ];
+    }
+}
---
+++ commands/database/VacuumAction.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+namespace app\commands\database;
+
+use Yii;
+use yii\base\Action;
+use yii\console\ExitCode;
+use yii\db\Connection;
+
+use const STDERR;
+
+final class VacuumAction extends Action
+{
+    public function run(): int
+    {
+        $db = Yii::$app->db;
+        \assert($db instanceof Connection);
+
+        vfprintf(STDERR, "Vacuuming database (database=%s)\n", [
+            $this->getDatabaseName($db) ?? '[unknown]',
+        ]);
+
+        $t1 = \microtime(true);
+        $db->createCommand('VACUUM ANALYZE')->execute();
+        $t2 = \microtime(true);
+
+        vfprintf(STDERR, "Done the vacuum, took=%.3fsec\n", [
+            $t2 - $t1,
+        ]);
+
+        return ExitCode::OK;
+    }
+
+    private function getDatabaseName(Connection $db): ?string
+    {
+        return \preg_match('/^.*?dbname=([\w-]+)\b.*$/', $db->dsn, $match)
+            ? $match[1]
+            : null;
+    }
+}
