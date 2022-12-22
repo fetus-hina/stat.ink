fc411267c085b7c90de5f36717f10a9ae4d6b03b
AIZAWA Hina <hina@fetus.jp>
2022-12-23 00:25:53 +0900

    Fix display issue

--- views/entire/v3/knockout3/table.php
+++ views/entire/v3/knockout3/table.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use app\models\Knockout3;
 use app\models\Map3;
 use app\models\Rule3;
+use yii\helpers\Html;
 use yii\web\View;
 
 /**
@@ -14,6 +15,12 @@ use yii\web\View;
  * @var array<int, Map3> $maps
  * @var array<int, Rule3> $rules
  */
+
+$this->registerCss(vsprintf('.graph-container{%s}', [
+  Html::cssStyleFromArray([
+    'min-width' => sprintf('%dpx', 220 * (count($rules) + 1)),
+  ]),
+]));
 
 ?>
 <div class="table-responsive table-responsive-force">
