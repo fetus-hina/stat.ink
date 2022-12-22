c733770f0ff85aa44449115e0bf66c76ba76d4de
AIZAWA Hina <hina@fetus.jp>
2022-12-23 00:33:45 +0900

    Update translation file headers for German

--- messages/de/rank3.php
+++ messages/de/rank3.php
@@ -4,6 +4,7 @@
  * @copyright Copyright (C) 2015-2022 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Lukas Böttcher <github@muffl0n.de>
  */
 
 declare(strict_types=1);
--- messages/de/rule3.php
+++ messages/de/rule3.php
@@ -4,6 +4,8 @@
  * @copyright Copyright (C) 2015-2022 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
+ * @author Lukas <github@muffl0n.de>
+ * @author Lukas Böttcher <github@muffl0n.de>
  */
 
 declare(strict_types=1);
