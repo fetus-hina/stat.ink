ec5bd58d114c4dabde1670c5e40bc5362ac6898c
AIZAWA Hina <hina@fetus.jp>
2023-02-09 12:52:01 +0900

    Splatoon 3 2.1.1
    https://www.nintendo.co.jp/support/switch/software_support/av5ja/211.html

---
+++ migrations/m230209_034949_s3_v2_1_1.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2023 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+final class m230209_034949_s3_v2_1_1 extends Migration
+{
+    use VersionMigration;
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->upVersion3(
+            '2.1',
+            'v2.1.x',
+            '2.1.1',
+            'v2.1.1',
+            new DateTimeImmutable('2023-02-09T09:00:00+09:00'),
+        );
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->downVersion3('2.1.1', '2.1.0');
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function vacuumTables(): array
+    {
+        return [
+            '{{%splatoon_version3}}',
+            '{{%splatoon_version_group3}}',
+        ];
+    }
+}
