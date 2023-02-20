b6648cef6bdada20691338984fc57a449b541356
AIZAWA Hina <hina@fetus.jp>
2023-02-21 04:23:47 +0900

    Add Horrorboros (#1195)

--- messages/ja/salmon-boss3.php
+++ messages/ja/salmon-boss3.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @copyright Copyright (C) 2015-2023 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
  */
@@ -18,6 +18,7 @@ return [
     'Flyfish' => 'カタパッド',
     'Goldie' => 'キンシャケ',
     'Griller' => 'グリル',
+    'Horrorboros' => 'タツ',
     'Maws' => 'モグラ',
     'Mothership' => 'ハコビヤ',
     'Mudmouth' => 'ドロシャケ',
