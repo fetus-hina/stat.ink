3265a766e189eb1d4619fda66a3b495f64dc14bf
AIZAWA Hina <hina@fetus.jp>
2023-03-05 08:21:23 +0900

    Caching

--- config/web/cache.php
+++ config/web/cache.php
@@ -2,14 +2,21 @@
 
 declare(strict_types=1);
 
+use yii\caching\DummyCache;
 use yii\caching\FileCache;
 
-return [
-    'class' => FileCache::class,
+$enabled = false;
 
-    'cachePath' => '@runtime/cache-v2',
-    'defaultDuration' => 3600,
-    'dirMode' => 0755,
-    'fileMode' => 0644,
-    'gcProbability' => 0,
-];
+return $enabled || YII_ENV_PROD
+    ? [
+        'class' => FileCache::class,
+
+        'cachePath' => '@runtime/cache-v2',
+        'defaultDuration' => 3600,
+        'dirMode' => 0755,
+        'fileMode' => 0644,
+        'gcProbability' => 0,
+    ]
+    : [
+        'class' => DummyCache::class,
+    ];
