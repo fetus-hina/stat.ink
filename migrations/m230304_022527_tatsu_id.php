a24a0f0c457958c3cea951494981eb875674530e
AIZAWA Hina <hina@fetus.jp>
2023-03-04 11:29:51 +0900

    Add ID alias for `tatsu` (#1195)

---
+++ migrations/m230304_022527_tatsu_id.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2023 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+use app\components\db\Migration;
+
+final class m230304_022527_tatsu_id extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->insert('{{%salmon_king3_alias}}', [
+            'key' => '24',
+            'salmonid_id' => $this->key2id('{{%salmon_king3}}', 'tatsu'),
+        ]);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->delete('{{%salmon_king3_alias}}', [
+            'key' => '24',
+        ]);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function vacuumTables(): array
+    {
+        return [
+            '{{%salmon_king3_alias}}',
+        ];
+    }
+}
