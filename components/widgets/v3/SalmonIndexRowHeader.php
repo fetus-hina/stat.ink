bbee216dfedfd256850104a5388d1f6f27f918fb
AIZAWA Hina <hina@fetus.jp>
2023-04-10 12:15:37 +0900

    file header

--- components/widgets/v3/SalmonIndexRowHeader.php
+++ components/widgets/v3/SalmonIndexRowHeader.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @copyright Copyright (C) 2015-2023 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
 declare(strict_types=1);
 
 namespace app\components\widgets\v3;
