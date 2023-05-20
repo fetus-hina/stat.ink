41e2d28088e90a8321b050b8361d2d86ec3f6b3e
AIZAWA Hina <hina@fetus.jp>
2023-05-20 11:57:54 +0900

    Fix style

--- components/db/Migration.php
+++ components/db/Migration.php
@@ -20,9 +20,11 @@ use yii\db\Schema;
 
 use function array_keys;
 use function array_map;
+use function array_values;
 use function assert;
 use function filter_var;
 use function implode;
+use function is_array;
 use function is_int;
 use function is_string;
 use function preg_split;
