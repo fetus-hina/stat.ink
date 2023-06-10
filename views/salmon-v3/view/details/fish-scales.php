a8905c584d5c4d7942025d18e97861f61bd39205
AIZAWA Hina <hina@fetus.jp>
2023-06-11 04:49:38 +0900

    Update fish scales & medal icons

--- views/salmon-v3/view/details/fish-scales.php
+++ views/salmon-v3/view/details/fish-scales.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use app\components\widgets\Icon;
 use app\models\Salmon3;
 use yii\helpers\Html;
 use yii\web\View;
@@ -21,9 +22,18 @@ return [
     }
 
     $labels = [
-      'gold' => Yii::t('app-salmon-scale3', 'Gold'),
-      'silver' => Yii::t('app-salmon-scale3', 'Silver'),
-      'bronze' => Yii::t('app-salmon-scale3', 'Bronze'),
+      'gold' => [
+        Icon::goldScale(),
+        Yii::t('app-salmon-scale3', 'Gold'),
+      ],
+      'silver' => [
+        Icon::silverScale(),
+        Yii::t('app-salmon-scale3', 'Silver'),
+      ],
+      'bronze' => [
+        Icon::bronzeScale(),
+        Yii::t('app-salmon-scale3', 'Bronze'),
+      ],
     ];
 
     $counts = [
@@ -33,19 +43,22 @@ return [
     ];
     
     return implode(
-      ' / ',
+      '', // mb_chr(0x2003, 'UTF-8'), // em-space
       array_filter(
         array_map(
-          fn (string $key): ?string => ($counts[$key] ?? 0) < 1
-            ? null
-            : Html::tag(
-              'span',
-              vsprintf('%s: %s', [
-                Html::encode($labels[$key] ?? ''),
-                Html::encode(Yii::$app->formatter->asInteger($counts[$key] ?? 0)),
+          fn (string $key, int $count): string => Html::tag(
+            'span',
+            vsprintf('%s %s', [
+              Html::tag('span', $labels[$key][0] ?? '', [
+                'class' => 'auto-tooltip',
+                'title' => $labels[$key][1] ?? '',
               ]),
-            ),
+              Html::encode(Yii::$app->formatter->asInteger($counts[$key] ?? 0)),
+            ]),
+            ['class' => 'mr-2'],
+          ),
           array_keys($counts),
+          array_values($counts),
         ),
       ),
     );
