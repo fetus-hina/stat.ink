013bf4a76d32ec694ea416a97de4df91faf283ac
AIZAWA Hina <hina@fetus.jp>
2023-07-01 20:54:11 +0900

    Twitter integration warning

--- views/user/profile/login-with.php
+++ views/user/profile/login-with.php
@@ -13,6 +13,27 @@ use yii\web\View;
 
 ?>
 <h2><?= Html::encode(Yii::t('app', 'Log in with other services')) ?></h2>
+<div class="alert alert-warning">
+  <p>
+    <?= Html::encode(
+      Yii::t(
+        'app',
+        'Since the acquisition of Twitter by Elon Musk, the environment for third-party applications has deteriorated rapidly and extremely.',
+      ),
+    ) . "\n" ?>
+  </p>
+  <p class="mb-0">
+    <?= Html::encode(
+      Yii::t(
+        'app',
+        'The future of Twitter integration is completely uncertain, and as a {site} developer I can\'t recommend this feature at this time.',
+        [
+          'site' => Yii::$app->name,
+        ],
+      ),
+    ) . "\n" ?>
+  </p>
+</div>
 <table class="table table-striped">
   <tbody>
     <?= $this->render('login-with/twitter', compact('user')) . "\n" ?>
