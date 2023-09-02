fe61fb21f479935cf2cbebfbfcc0c25d98f8923b
AIZAWA Hina <hina@fetus.jp>
2023-09-03 03:40:03 +0900

    Salmometer stats updator

--- commands/stat/Salmon3Trait.php
+++ commands/stat/Salmon3Trait.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @copyright Copyright (C) 2015-2022 AIZAWA Hina
+ * @copyright Copyright (C) 2015-2023 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
  */
@@ -10,14 +10,17 @@ declare(strict_types=1);
 
 namespace app\commands\stat;
 
+use app\commands\stat\salmon3\SalmometerTrait;
 use app\commands\stat\salmon3\TideEventTrait;
 
 trait Salmon3Trait
 {
+    use SalmometerTrait;
     use TideEventTrait;
 
     protected function updateEntireSalmon3(): void
     {
         $this->makeStatSalmon3TideEvent();
+        $this->makeStatSalmon3Salmometer();
     }
 }
