d16c280cb6aa39ad01354792d80d36de7e84610d
AIZAWA Hina <hina@fetus.jp>
2023-09-22 13:46:17 +0900

    Update counter widget
      - It displays "--------" during loading (may)
      - Using a deterministic height at initialization may reduce browser re-rendering

--- resources/react/components/parts/counter/Counter.js
+++ resources/react/components/parts/counter/Counter.js
@@ -1,39 +1,46 @@
 import CounterItem from './CounterItem';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { STATUS_OK } from '../../../constants';
 import { connect } from 'react-redux';
 import { createUseStyles } from 'react-jss';
 
-const TYPE_ORDER = [
-  'user',
-  'battle',
-  'salmon'
-];
+const DEFAULT_DIGITS = 8;
 
 const useStyles = createUseStyles({
   root: {
     fontSize: '16px',
     lineHeight: '1.3',
-    margin: '0 0 10px',
+    margin: '0',
     textAlign: 'right'
+  },
+  initializing: {
+    minHeight: '104px'
   }
 });
 
+const isEmptyObject = (obj) => Object.keys(obj).length === 0;
+
 function Counter (props) {
   const classes = useStyles();
-  const { data } = props;
+  const { data, status } = props;
 
   const rows = createRows(data);
-  if (rows.length < 1) {
-    return null;
-  }
-
   const maxDigit = rows
-    .map(v => String(v.count).length)
-    .reduce((acc, cur) => Math.max(acc, cur), 1);
+    .map(v => v.count !== null ? String(v.count).length : DEFAULT_DIGITS)
+    .reduce((acc, cur) => Math.max(acc, cur), DEFAULT_DIGITS);
 
   return (
-    <aside className={classes.root}>
+    <aside
+      className={
+        [
+          classes.root,
+          status !== STATUS_OK && isEmptyObject(data) ? classes.initializing : null
+        ]
+          .filter(v => v !== null)
+          .join(' ')
+      }
+    >
       {rows.map(v => (
         <CounterItem
           key={v.type}
@@ -49,12 +56,14 @@ function Counter (props) {
 }
 
 Counter.propTypes = {
-  data: PropTypes.object.isRequired
+  data: PropTypes.object.isRequired,
+  status: PropTypes.string.isRequired
 };
 
 function mapStateToProps (state) {
   return {
-    data: state.counter.data
+    data: state.counter.data,
+    status: state.counter.status
   };
 }
 
@@ -64,30 +73,46 @@ function mapDispatchToProps (/* dispatch */) {
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
 
-function createRows (jsonData) {
-  const results = [];
+const numberFormat = (number) => Number(number)
+  .toLocaleString(window?.document?.documentElement?.lang ?? 'en-US');
 
-  TYPE_ORDER.map(type => {
-    // get filtered list (by type)
-    const typeData = Object.entries(jsonData)
-      .filter(
-        ([, v]) => (v.type === type)
-      )
-      .sort(
-        ([a], [b]) => a.localeCompare(b)
-      );
-    if (typeData.length > 0) {
-      const [, info] = typeData[0];
-      results.push({
-        type,
-        label: info.label,
-        popup: typeData.length > 1
-          ? typeData.map(([, v]) => String(v.count)).join(' + ') // TODO: number format
-          : null,
-        count: typeData.map(([, v]) => Number(v.count)).reduce((acc, cur) => acc + cur, 0)
-      });
+function createRows (jsonData) {
+  const results = [
+    {
+      type: 'user',
+      label: 'Users',
+      popup: null,
+      count: null
+    },
+    {
+      type: 'battle',
+      label: 'Battles',
+      popup: null,
+      count: null
+    },
+    {
+      type: 'salmon',
+      label: 'Shifts',
+      popup: null,
+      count: null
     }
-    return null;
+  ];
+
+  results.forEach(currentRow => {
+    const typeMatchedJsonItems = Object.entries(jsonData)
+      .filter(([, v]) => (v.type === currentRow.type))
+      .sort(([a], [b]) => a.localeCompare(b));
+    if (typeMatchedJsonItems.length > 0) {
+      currentRow.label = typeMatchedJsonItems[0][1].label;
+      currentRow.count = typeMatchedJsonItems
+        .map(([, v]) => Number(v.count))
+        .reduce((acc, cur) => acc + cur, 0);
+      currentRow.popup = typeMatchedJsonItems.length > 1
+        ? typeMatchedJsonItems
+          .map(([, v]) => numberFormat(v.count))
+          .join(' + ')
+        : null;
+    }
   });
 
   return results;
