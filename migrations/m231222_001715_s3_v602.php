b722bd93107aa0e136da08fbc299b06fce893be7
AIZAWA Hina <hina@fetus.jp>
2023-12-22 09:18:22 +0900

    Splatoon 3 v6.0.2

---
+++ migrations/m231222_001715_s3_v602.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2023 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+final class m231222_001715_s3_v602 extends Migration
+{
+    use VersionMigration;
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->upVersion3(
+            '6.0',
+            'v6.0.x',
+            '6.0.2',
+            'v6.0.2',
+            new DateTimeImmutable('2023-12-22T10:10:00+09:00'),
+        );
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->downVersion3('6.0.2', '6.0.1');
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function vacuumTables(): array
+    {
+        return [
+            '{{%splatoon_version3}}',
+            '{{%splatoon_version_group3}}',
+        ];
+    }
+}
