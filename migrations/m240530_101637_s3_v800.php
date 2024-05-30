3a08c27aef2471c2b996c4ce8d252e3de4a645e2
AIZAWA Hina <hina@fetus.jp>
2024-05-30 19:17:46 +0900

    Splatoon 3 v8.0.0

---
+++ migrations/m240530_101637_s3_v800.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @copyright Copyright (C) 2015-2024 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@fetus.jp>
+ */
+
+declare(strict_types=1);
+
+use app\components\db\Migration;
+use app\components\db\VersionMigration;
+
+final class m240530_101637_s3_v800 extends Migration
+{
+    use VersionMigration;
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->upVersion3(
+            '8.0',
+            'v8.0.x',
+            '8.0.0',
+            'v8.0.0',
+            new DateTimeImmutable('2024-05-31T10:10:00+09:00'),
+        );
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->downVersion3('8.0.0', '7.2.0');
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function vacuumTables(): array
+    {
+        return [
+            '{{%splatoon_version3}}',
+            '{{%splatoon_version_group3}}',
+        ];
+    }
+}
