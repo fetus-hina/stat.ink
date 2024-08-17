39a29cd409e2d487ee94a13bffad4d21e10f8d15
Ant <150183507+Ant-Spl@users.noreply.github.com>
2024-08-16 22:24:14 -0300

    api and alert

--- messages/pt-BR/alert.php
+++ messages/pt-BR/alert.php
@@ -13,7 +13,7 @@ return [
     'Incorrect data will be registered if you do not use the latest version.' => 'Dados incorretos serão registados se você não usar a versão mais recente.',
     'Please update your client software and use always latest version.' => 'Por favor atualize seu client software e sempre use a versão mais recente.',
     'Sorry for inconvenience.' => 'Pedimos desculpas pela incoveniência.',
-    'Term: {startDate} - {endDate}' => 'Termo: {startDate} - {endDate}',
+    'Term: {startDate} - {endDate}' => 'Duração: {startDate} - {endDate}',
     'We\'ll perform maintenance on the schedule below:' => 'Nós faremos uma manuntenção seguindo o cronograma abaixo:',
     'We won\'t implement automatic importing to {appName} for security reasons.' => 'Nós não vamos implementar importação automática para o {appName} por razões de segurança.',
     'You can import automatically from SplatNet, use these apps: (USE AT YOUR OWN RISK)' => 'Você pode importar automaticamente do SplatNet, use esses aplicativos: (USE POR SUA CONTA E RISCO)',
