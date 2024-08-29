e032954a66cf549516032d86d2c392a8da338137
Hitomi <119537233+Hitomi11212@users.noreply.github.com>
2024-08-29 12:14:20 +0200

    Update salmon-event2.php

--- messages/fr/salmon-event2.php
+++ messages/fr/salmon-event2.php
@@ -10,10 +10,10 @@ declare(strict_types=1);
 
 return [
     'Cohock Charge' => 'Charges de Sumoches',
-    'Event' => '',
+    'Event' => 'Évènement',
     'Fog' => 'Brouillard',
     'Goldie Seeking' => 'Chasse aux Dorax',
-    'Known Occurrence' => '',
+    'Known Occurrence' => 'Événement connu',
     'Rush' => 'Ruées',
     'The Griller' => 'Barbeurks',
     'The Mothership' => 'Vaisseaux mères',
