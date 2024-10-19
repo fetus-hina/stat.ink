bb8b5af9c7ca2b41c2c05d9c08123bb5672bf608
AIZAWA Hina <hina@fetus.jp>
2024-10-19 14:27:07 +0900

    Regenerate zh-CN messages

--- messages/zh-CN/tz.php
+++ messages/zh-CN/tz.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @copyright Copyright (C) 2015-2023 AIZAWA Hina
+ * @copyright Copyright (C) 2015-2024 AIZAWA Hina
  * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
  * @author AIZAWA Hina <hina@fetus.jp>
  * @author Yifan Liu <yifanliu00@gmail.com>
@@ -238,4 +238,3 @@ return [
     'Zambia' => '赞比亚',
     'Zimbabwe' => '津巴布韦',
 ];
-
