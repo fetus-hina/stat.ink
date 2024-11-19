334de3cbb2b0be081a5c3a3c636884f89292ca36
AIZAWA Hina <hina@fetus.jp>
2024-11-19 11:54:19 +0900

    cron: switch update-geoip to systemd timer

--- schedule/schedule.php
+++ schedule/schedule.php
@@ -8,7 +8,6 @@ use omnilight\scheduling\Schedule;
  * @var Schedule $schedule
  */
 
-require __DIR__ . '/schedule/geoip.php';
 require __DIR__ . '/schedule/salmon2.php';
 require __DIR__ . '/schedule/salmon3.php';
 require __DIR__ . '/schedule/schedule.php';
