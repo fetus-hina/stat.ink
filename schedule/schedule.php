42e70390b0cd201bc145d8fe9b6aef0af7dc9a81
AIZAWA Hina <hina@fetus.jp>
2024-11-21 12:08:29 +0900

    cron: switch stat/update-entire-salmon{2,3} to systemd timer

--- schedule/schedule.php
+++ schedule/schedule.php
@@ -8,6 +8,4 @@ use omnilight\scheduling\Schedule;
  * @var Schedule $schedule
  */
 
-require __DIR__ . '/schedule/salmon2.php';
-require __DIR__ . '/schedule/salmon3.php';
 require __DIR__ . '/schedule/user-json.php';
